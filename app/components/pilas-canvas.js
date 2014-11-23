26588005f5d2f6cf8f69995c884c80468115a0fc
Hugo Ruscitti <hugoruscitti@gmail.com>
2014-11-23 03:45:50 -0300

    Creando el componente Canvas.

---
+++ app/components/pilas-canvas.js
@@ -0,0 +1,16 @@
+import Ember from 'ember';
+
+export default Ember.Component.extend({
+  iniciarPilas: function() {
+    var canvas_element = this.$().find('canvas')[0];
+
+    pilas = new Pilas();
+    pilas.iniciar({ancho: 320, alto: 240, canvas: canvas_element, data_path: 'libs/data'});
+
+    pilas.onready = function() {
+    }
+
+    pilas.ejecutar();
+
+  }.on('didInsertElement'),
+});
