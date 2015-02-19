29187464381df6af99ad503573d562a7cd85ae43
Alf Sanzo <alfredo.sanzo@gmail.com>
2015-02-19 19:36:50 -0300

    Agregando gitignore para laburar con Eclipse tranquilo

--- ejerciciosPilas/src/primerEjercicio.js
+++ ejerciciosPilas/src/primerEjercicio.js
@@ -1,17 +1,17 @@
-pilas = new Pilas();
-pilas.iniciar({ancho: 320, alto: 240, data_path: 'data'});
-
-pilas.onready = function() {
-    var fondo = new pilas.fondos.Tarde();
-    var rodri = new pilas.actores.Maton();
-    
-    rodri.x = -150;
-    rodri.y = -100;
-
-    rodri.caminar_derecha();
-    rodri.hacer(pilas.comportamientos.Saltando);
-    rodri.caminar_derecha();
-
-}
-
-pilas.ejecutar();
+	pilas = new Pilas();
+	pilas.iniciar({ancho: 320, alto: 240, data_path: 'data'});
+	
+	pilas.onready = function() {
+	    var fondo = new pilas.fondos.Tarde();
+	    var rodri = new pilas.actores.Maton();
+	    
+	    rodri.x = -150;
+	    rodri.y = -100;
+	
+	    rodri.caminar_derecha();
+	    rodri.hacer(pilas.comportamientos.Saltando);
+	    rodri.caminar_derecha();
+	
+	};
+	
+	pilas.ejecutar();
