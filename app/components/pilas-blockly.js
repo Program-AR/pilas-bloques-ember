a36834cd902c969c7fe0d162307a4318165b107d
sawady.faso@gmail.com <sawady.faso@gmail.com>
2015-02-20 21:22:49 -0300

    Separacion entre una actividad y el editor

--- app/components/pilas-blockly.js
+++ app/components/pilas-blockly.js
@@ -2,187 +2,6 @@ import Ember from 'ember';
 
 var Blockly = window.Blockly;
 
-/*
-   Modelo que permite definir el lenguaje
-   de una actividad
-*/
-
-var BloqueCategoria = Ember.Object.extend({
-   init: function() {
-      this.set('bloques', []);
-   },
-
-   agregar_simple: function(blockType) {
-     this.get('bloques').pushObject(Bloque.create({
-       tipo: blockType
-     }));
-   },
-
-   agregar_con_params: function(blockType, params) {
-    var nuevo_bloque = Bloque.create({
-       tipo: blockType
-    });
-
-    nuevo_bloque.agregar(params);
-
-    this.get('bloques').pushObject(nuevo_bloque);
-   },
-
-   borrar: function(blockType) {
-     this.get('bloques').forEach(function (item) {
-       if(item.get('tipo') === blockType) {
-           this.get('bloques').removeObject(item);
-       }
-     });
-   },
-
-   build: function() {
-     var str_category = '';
-
-     str_category += '<category name="x">\n'.replace("x", this.get('nombre'));
-
-     this.get('bloques').forEach(function(item) {
-         str_category += item.build();
-     });
-
-     str_category += '</category>\n';
-
-     return str_category;
-   }
-});
-
-var BloqueCategoriaVariables = Ember.Object.extend({
-    build: function() {
-        return '<category name="Variables" custom="VARIABLE"></category>';
-    }
-});
-
-var BloqueCategoriaSubtareas = Ember.Object.extend({
-    build: function() {
-        return '<category name="Subtareas" custom="PROCEDURE"></category>';
-    }
-});
-
-var Bloque = Ember.Object.extend({
-   init: function() {
-      this.set('parametros', []);
-   },
-
-   agregar: function(params) {
-      var yo = this;
-      params.forEach( function(item) {
-         yo.get('parametros').pushObject(item);
-      });
-   },
-
-   build: function() {
-     var str_block = '';
-     str_block += '<block type="TIPO">'.replace("TIPO", this.get('tipo'));
-
-     this.get('parametros').forEach(function(item) {
-         str_block += item.build();
-     });
-
-     str_block += '</block>';
-     return str_block;
-   }
-});
-
-var ParamCampo = Ember.Object.extend({
-   build: function() {
-     var str_block = '';
-     str_block += '<field name="NOMBRE">'.replace("NOMBRE", this.get('nombre_valor'));
-     str_block += this.get('valor');
-     str_block += '</field>';
-     return str_block;
-   }
-});
-
-var ParamValor = Ember.Object.extend({
-   build: function() {
-     var str_block = '';
-     str_block += '<value name="NOMBRE">'.replace("NOMBRE", this.get('nombre_param'));
-
-     str_block += '<block type="TIPO">'.replace("TIPO", this.get('tipo_bloque'));
-
-     str_block += '<field name="TIPO">'.replace("TIPO", this.get('nombre_valor'));
-     str_block += this.get('valor');
-     str_block += '</field>';
-
-     str_block += '</block>';
-
-     str_block += '</value>';
-
-     return str_block;
-   }
-});
-
-var Lenguaje = Ember.Object.extend({
-   build: function() {
-     var str_toolbox = '';
-
-     str_toolbox += '<xml>';
-
-     this.get('categorias').forEach(function(item) {
-         str_toolbox += item.build();
-     });
-
-     str_toolbox += '</xml>';
-
-     return str_toolbox;
-   }
-});
-
-/*
-   Bloques que son parte de la actividad "Alien"
-*/
-
-var alien_primitivas = BloqueCategoria.create({
-   nombre: 'Alien'
-});
-
-alien_primitivas.agregar_simple('alien-ir_derecha');
-alien_primitivas.agregar_simple('alien-ir_izquierda');
-alien_primitivas.agregar_simple('alien-ir_arriba');
-alien_primitivas.agregar_simple('alien-ir_abajo');
-alien_primitivas.agregar_simple('alien-recoger');
-alien_primitivas.agregar_simple('choca_con_tuerca');
-
-var alien_control = BloqueCategoria.create({
-   nombre: 'Control'
-});
-
-alien_control.agregar_con_params('repetir', [
-  ParamValor.create({
-    nombre_param: "count",
-    tipo_bloque: "math_number",
-    nombre_valor: "NUM",
-    valor: "10"
-  })
-]);
-
-alien_control.agregar_simple('si');
-alien_control.agregar_simple('sino');
-alien_control.agregar_simple('hasta');
-
-var alien_expresiones = BloqueCategoria.create({
-   nombre: 'Expresiones'
-});
-
-alien_expresiones.agregar_simple('math_number');
-alien_expresiones.agregar_simple('math_arithmetic');
-alien_expresiones.agregar_simple('logic_boolean');
-alien_expresiones.agregar_simple('logic_compare');
-alien_expresiones.agregar_simple('logic_operation');
-alien_expresiones.agregar_simple('logic_negate');
-
-var alien_variables = new BloqueCategoriaVariables();
-var alien_subtareas = new BloqueCategoriaSubtareas();
-
-var alien_lenguaje = Lenguaje.create({
-   categorias: [alien_primitivas, alien_control, alien_expresiones, alien_variables, alien_subtareas]
-});
-
 export default Ember.Component.extend({
   ejecutando: false,
 
@@ -190,6 +9,7 @@ export default Ember.Component.extend({
     window.forzar_redimensionado();
     this.sendAction('redimensionar');
   },
+
   actions: {
     ejecutar: function() {
       window.LoopTrap = 1000;
@@ -251,29 +71,13 @@ export default Ember.Component.extend({
     main_program_block_def.initSvg();
     Blockly.getMainWorkspace().render();
 
-    this.cargar_escenario(this.escenario);
+    this.cargar_lenguaje();
     this.cargar_codigo_desde_el_modelo();
   }.on('didInsertElement'),
 
-  definir_bloques: function(leng) {
-    Blockly.updateToolbox(leng);
+  cargar_lenguaje: function() {
+    Blockly.updateToolbox(this.get('actividad').getLenguaje());
   },
-
-  cargar_escenario: function(escenario) {
-    var escenarios = {};
-
-    escenarios.alien_laberinto = function() {
-      this.definir_bloques(alien_lenguaje.build());
-    }.bind(this);
-
-    if (escenario in escenarios) {
-      escenarios[escenario].call(this);
-    }
-    else {
-      throw new Error("No se puede cargar el escenario {{ESCENARIO}}, al parecer no está declarado en pilas-blockly.js".replace("{{ESCENARIO}}", escenario));
-    }
-  },
-
 
   cargar_codigo_desde_el_modelo: function() {
     if (this.get('model')) {
--- app/controllers/editor.js
+++ app/controllers/editor.js
@@ -2,16 +2,241 @@ import Ember from 'ember';
 
 var Bootstrap = window.Bootstrap;
 
-var Actividad = Ember.Object.extend({
+/*
+ * Representa un bloque
+ * para el lenguaje de la actividad
+ */
+var Bloque = Ember.Object.extend({
+  build: function() {
+    var str_block = '';
+    str_block += '<block type="TIPO">'.replace('TIPO', this.get('tipo'));
+
+    this.get('parametros').forEach(function(item) {
+       str_block += item.build();
+    });
+
+    str_block += '</block>';
+    return str_block;
+  }
+});
+
+/*
+ * Representa el valor
+ * de un campo string de un bloque
+ */
+var ParamCampo = Ember.Object.extend({
+   build: function() {
+     var str_block = '';
+     str_block += '<field name="NOMBRE">'.replace('NOMBRE', this.get('nombre_valor'));
+     str_block += this.get('valor');
+     str_block += '</field>';
+     return str_block;
+   }
+});
+
+/*
+ * Representa un valor mas complejo
+ * de un campo de un bloque
+ */
+var ParamValor = Ember.Object.extend({
+   build: function() {
+     var str_block = '';
+     str_block += '<value name="NOMBRE">'.replace('NOMBRE', this.get('nombre_param'));
+
+     str_block += '<block type="TIPO">'.replace('TIPO', this.get('tipo_bloque'));
+
+     str_block += '<field name="TIPO">'.replace('TIPO', this.get('nombre_valor'));
+     str_block += this.get('valor');
+     str_block += '</field>';
+
+     str_block += '</block>';
+
+     str_block += '</value>';
+
+     return str_block;
+   }
+});
+
+var Lenguaje = Ember.Object.extend({
+
+  init: function() {
+    var bloques = {};
+    this.get('categorias').forEach(function(i) {
+      bloques[i] = [];
+    });
+    this.set('bloques', bloques);
+  },
+
+  bloque: function(categoria, nombre, params) {
+    var nuevo_bloque = Bloque.create({
+      tipo: nombre,
+      parametros: params || []
+    });
+
+    this.get('bloques')[categoria].pushObject(nuevo_bloque);
+  },
+
+  build: function() {
+    var str_toolbox = '';
+
+    str_toolbox += '<xml>';
+
+    this.get('categorias').forEach(function(item) {
+      if(item === 'Subtareas') {
+        str_toolbox += this._build_subtareas();
+      } else if (item === 'Variables') {
+        str_toolbox += this._build_variables();
+      } else {
+        str_toolbox += this._build_categoria(item);
+      }
+    }.bind(this));
+
+    str_toolbox += '</xml>';
+
+    return str_toolbox;
+  },
+
+  _build_categoria: function(categoria) {
+   var str_category = '';
+
+   str_category += '<category name="x">\n'.replace('x', categoria);
+
+   this.get('bloques')[categoria].forEach(function(b) {
+       str_category += b.build();
+   });
+
+   str_category += '</category>\n';
+
+   return str_category;
+  },
+
+
+  _build_subtareas: function() {
+    return '<category name="Subtareas" custom="PROCEDURE"></category>';
+  },
+
+  _build_variables: function() {
+    return '<category name="Variables" custom="VARIABLE"></category>';
+  }
 
 });
 
+/**
+  Modelo de actividad
+**/
+var Actividad = Ember.Object.extend({
+  init: function() {
+    this.set('puedeDuplicar', true);
+    this.set('puedeDesactivar', false);
+    this.set('puedeComentar', false);
+  }
+});
+
+var ActividadAlien = Actividad.extend({
+
+  init: function() {
+    this.set('nombre', 'El alien y las tuercas');
+    this.set('enunciado', 'Define un programa para que el alien junte todas las tuercas');
+    this.set('tuercas_recolectadas', 0);
+  },
+
+  coord_grilla: function(fila, columna) {
+    var columnas = [-175, -105, -35, 35, 105, 175];
+    var filas = [140, 60, -20, -100, -180];
+
+    return {x: columnas[columna-1], y: filas[fila-1]};
+  },
+
+  iniciarEscena: function() {
+    pilas.reiniciar();
+
+    var fondo = new pilas.fondos.Laberinto1();
+    var alien = new pilas.actores.Alien(-175, -180);
+
+    window.alien = alien;
+    window.fondo = fondo;
+
+    var actividad = this;
+
+    alien.cuando_busca_recoger = function() {
+      var actores = pilas.obtener_actores_en(alien.x, alien.y + 20, 'Tuerca');
+
+      if (actores.length > 0) {
+        var mensaje = '';
+        actores[0].eliminar();
+        var restantes = pilas.obtener_actores_con_etiqueta('Tuerca').length;
+
+        actividad.incrementProperty('tuercas_recolectadas');
+
+        if (restantes > 0) {
+          mensaje = 'genial, aún quedan: ' + restantes;
+        } else {
+          mensaje = '¡Nivel completado!';
+        }
+
+        //alien.decir(mensaje);
+        //console.log(mensaje);
+      }
+    };
+
+    var posicion = this.coord_grilla(1, 1);
+    new pilas.actores.Tuerca(posicion.x, posicion.y);
+
+    posicion = this.coord_grilla(2, 2);
+    new pilas.actores.Tuerca(posicion.x, posicion.y);
+
+    posicion = this.coord_grilla(3, 3);
+    new pilas.actores.Tuerca(posicion.x, posicion.y);
+
+    posicion = this.coord_grilla(4, 4);
+    new pilas.actores.Tuerca(posicion.x, posicion.y);
+
+    posicion = this.coord_grilla(5, 5);
+    new pilas.actores.Tuerca(posicion.x, posicion.y);
+  },
+
+  getLenguaje: function() {
+
+    var leng = Lenguaje.create({
+       categorias: ['Acciones', 'Sensores', 'Control', 'Expresiones', 'Variables', 'Subtareas']
+    });
+
+    leng.bloque('Acciones', 'alien-ir_derecha');
+    leng.bloque('Acciones', 'alien-ir_izquierda');
+    leng.bloque('Acciones', 'alien-ir_arriba');
+    leng.bloque('Acciones', 'alien-ir_abajo');
+    leng.bloque('Acciones', 'alien-recoger');
+    leng.bloque('Sensores', 'choca_con_tuerca');
+
+    leng.bloque('Control', 'repetir', [
+      ParamValor.create({
+        nombre_param: 'count',
+        tipo_bloque: 'math_number',
+        nombre_valor: 'NUM',
+        valor: '10'
+      })
+    ]);
+
+    leng.bloque('Control', 'si');
+    leng.bloque('Control', 'sino');
+    leng.bloque('Control', 'hasta');
+
+    leng.bloque('Expresiones', 'math_number');
+    leng.bloque('Expresiones', 'math_arithmetic');
+    leng.bloque('Expresiones', 'logic_boolean');
+    leng.bloque('Expresiones', 'logic_compare');
+    leng.bloque('Expresiones', 'logic_operation');
+    leng.bloque('Expresiones', 'logic_negate');
+
+    return leng.build();
+
+  }
+});
+
 export default Ember.Controller.extend({
-  actividad_actual: {},
-  nombre_escenario: "alien_laberinto",
-  tuercas_recolectadas: 0,
-  nombre_al_guardar: "mi juego",
-  tmp_codigo_xml: "",
+  actividad: ActividadAlien.create(),
+  nombre_al_guardar: 'mi actividad',
+  tmp_codigo_xml: '',
 
 
   inyectarRedimensionado: function() {
@@ -74,8 +299,8 @@ export default Ember.Controller.extend({
     },
 
     guardarEnGaleria: function() {
-      var imagen = document.getElementById("canvas");
-      var imagen_data = imagen.toDataURL("image/png");
+      var imagen = document.getElementById('canvas');
+      var imagen_data = imagen.toDataURL('image/png');
 
       var juego = this.get('model');
 
@@ -95,73 +320,7 @@ export default Ember.Controller.extend({
     },
 
     reiniciar: function() {
-      var controller = this;
-
-      var escenarios = {};
-      var nombre_escenario = this.get('nombre_escenario');
-
-      this.set('tuercas_recolectadas', 0);
-
-      escenarios.alien_laberinto = function() {
-        pilas.reiniciar();
-
-        function convertir_posicion_a_coordenada(fila, columna) {
-          var columnas = [-175, -105, -35, 35, 105, 175];
-          var filas = [140, 60, -20, -100, -180];
-
-          return {x: columnas[columna-1], y: filas[fila-1]};
-        }
-
-          var fondo = new pilas.fondos.Laberinto1();
-          var alien = new pilas.actores.Alien(-175, -180);
-
-          window.alien = alien;
-          window.fondo = fondo;
-
-          alien.cuando_busca_recoger = function() {
-            var actores = pilas.obtener_actores_en(alien.x, alien.y + 20, 'Tuerca');
-
-            if (actores.length > 0) {
-              var mensaje = "";
-              actores[0].eliminar();
-              var restantes = pilas.obtener_actores_con_etiqueta("Tuerca").length;
-
-              controller.incrementProperty('tuercas_recolectadas');
-
-
-              if (restantes > 0) {
-                mensaje = "genial, aún quedan: " + restantes;
-              } else {
-                mensaje = "¡Nivel completado!";
-              }
-
-              //alien.decir(mensaje);
-              //console.log(mensaje);
-            }
-          };
-
-          var posicion = convertir_posicion_a_coordenada(1, 1);
-          new pilas.actores.Tuerca(posicion.x, posicion.y);
-
-          posicion = convertir_posicion_a_coordenada(2, 2);
-          new pilas.actores.Tuerca(posicion.x, posicion.y);
-
-          posicion = convertir_posicion_a_coordenada(3, 3);
-          new pilas.actores.Tuerca(posicion.x, posicion.y);
-
-          posicion = convertir_posicion_a_coordenada(4, 4);
-          new pilas.actores.Tuerca(posicion.x, posicion.y);
-
-          posicion = convertir_posicion_a_coordenada(5, 5);
-          new pilas.actores.Tuerca(posicion.x, posicion.y);
-
-      };
-
-      if (nombre_escenario in escenarios) {
-        escenarios[nombre_escenario].call(this);
-      } else {
-        throw new Error("No se puede cargar el escenario {{ESCENARIO}}, al parecer no está declarado en pilas-canvas.js".replace("{{ESCENARIO}}", nombre_escenario));
-      }
+      this.get('actividad').iniciarEscena();
     }
   }
 });
