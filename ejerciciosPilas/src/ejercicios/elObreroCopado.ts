18b08667fc96975108b6bbd746d724282729efa1
asanzo <alfredo.sanzo@gmail.com>
2015-02-23 21:17:27 -0300

    Prueba con reference path

--- ejerciciosPilas/src/ejercicios/elObreroCopado.ts
+++ ejerciciosPilas/src/ejercicios/elObreroCopado.ts
@@ -1,3 +1,5 @@
+/// <reference path="../../../pilasweb/src/escenas.ts"/>
+
 /**
  * @class ElObreroCopado
  * Es el ejercicio inicial
@@ -11,8 +13,8 @@ class ElObreroCopado extends Base {
         pilas = pila;
     }
     
-    iniciar(receptor) {
-        super.iniciar(receptor);
+    iniciar() {
+        super.iniciar();
         this.fondo = new pilas.fondos.Fondo();
         fondo.imagen = 'fondos/fondoObrero.png';
         new pilas.actores.Mono();
