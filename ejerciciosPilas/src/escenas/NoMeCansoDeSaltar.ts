85141085d058f0fbefb1db5c3d4f5b6a508235ce
Charly <carlosiguaran@FS-E412X.(none)>
2015-03-11 15:22:53 -0300

    escena no me canso de saltar

---
+++ ejerciciosPilas/src/escenas/NoMeCansoDeSaltar.ts
@@ -0,0 +1,28 @@
+/// <reference path = "../../dependencias/pilasweb.d.ts" />
+// <reference path = "../../comportamientos/SaltarHablando.ts" />
+
+
+/**
+ * @class NoMeCansoDeSaltar
+ * 
+ * Objetivos: Introducir Repetición
+ * Enunciado: Repetir salto.
+ */
+ class NoMeCansoDeSaltar extends ElObreroCopado{
+ 	saltosRestantes;
+ 	iniciar() {
+        this.fondo = new Fondo('fondos/fondoObrero.png',0,0);
+        this.obrero = new Obrero(160,-100);
+        this.obrero.aprender(AvisaAlSalirDePantalla,{});
+        this.saltosRestantes=30;
+    }
+
+    saltarHablando(){
+        this.obrero.hacer_luego(SaltarHablando)
+    }
+    fraseAlSaltar(){
+    	this.saltosRestantes--;
+    	return this.saltosRestantes.toString();
+    }
+}
+ 
