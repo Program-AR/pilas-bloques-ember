de52540966174bc3ab119da5e7f76fc23d95710e
Charly <carlosiguaran@FS-E412X.(none)>
2015-03-17 12:36:35 -0300

    saco un archivo que esta de mas

--- ejerciciosPilas/src/comportamientos/RecogerEstrella.ts
+++
@@ -1,9 +0,0 @@
-class RecogerEstrella extends Recoger {
- paso;
-    imagenAnterior;  
-	iniciar(receptor) {
-        super.iniciar(receptor);
-        pilas.escena_actual().intentaronRecoger();
-    }
-}
-
