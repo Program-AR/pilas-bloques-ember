2d4bd08099c01a0d4f8f55ac3964e31853dd1de0
Alf Sanzo <alfredo.sanzo@gmail.com>
2015-03-20 13:20:12 -0300

    Refactor sobre ComportamientoAnimado para que ahora puedan hacerse cosas no solo al iniciar y terminar la animación, sino mientras.

--- ejerciciosPilas/src/comportamientos/ComportamientoAnimado.ts
+++ ejerciciosPilas/src/comportamientos/ComportamientoAnimado.ts
@@ -2,13 +2,34 @@
 
 
 /**
- * New typescript file
+ * @class ComportamientoAnimado
+ * Esta clase está pensada para ser usada de superclase,
+ * si es que se desea construir un comportamiento que se anime.
+ * 
+ * @example
+ * Puede usarse directamente de esta manera:
+ *      actor.hacer_luego(ComportamientoAnimado,{nombreAnimacion: 'correr'});
+ * De esta manera el actor se animará sin hacer nada.
+ * 
+ * @example
+ * Otra manera de usarlo es así:
+ *      actor.hacer_luego(Explotar);
+ *
+ * Donde Explotar es una subclase y tiene definidos los siguientes métodos:
+ *      nombreAnimacion(){ 
+ *			return 'explosion'
+ *		};
+ *      doActualizar(){
+ *			if (this.terminoAnimacion) {
+ *				this.receptor.eliminar();
+ *				return true;
+ *			}
+ *		}
  */
 
 class ComportamientoAnimado extends Comportamiento {
-	animParado = 'parado';
+	_terminoAnimacion = true;
 	
-
 	iniciar(receptor){
 		super.iniciar(receptor);
 		this.receptor.cargarAnimacion(this.nombreAnimacion());
@@ -16,20 +37,37 @@ class ComportamientoAnimado extends Comportamiento {
 	}
 	
 	actualizar(){
-		if(this.receptor.avanzarAnimacion()){
-			this.receptor.cargarAnimacion(this.animParado);
-			this.alFinalizarAnimacion();
+		this._terminoAnimacion = this.receptor.avanzarAnimacion();
+		if(this.doActualizar()){
+			this.receptor.cargarAnimacion(this.nombreAnimacionParado());
 			return true;
 		}
 		return false;
 	}
 	
+	get terminoAnimacion(){
+		return this._terminoAnimacion;
+	}
+	
+	/* Redefinir si corresponde animar el comportamiento. */
 	nombreAnimacion(){
-		return this.animParado;
+		return this.argumentos.nombreAnimacion || this.nombreAnimacionParado();
 	}
-	alFinalizarAnimacion(){
+	
+	/* Redefinir si corresponde */
+	nombreAnimacionParado(){
+		return this.argumentos.nombreAnimacionParado || 'parado';
 	}
+	
+	/* Redefinir si corresponde */
 	alIniciarAnimacion(){
-		
+	}
+	
+	/** Redefinir siempre. 
+	 *  Redefinir sólo este, no el actualizar original.
+	 *  Es lo que hace efectivamente el comportamiento, además de animar.
+	 *  Debe retornar true cuando corresponda terminar el comportamiento */
+	doActualizar(){
+		return this.terminoAnimacion;
 	}
 } 
--- ejerciciosPilas/src/comportamientos/RecogerTipoEspecifico.ts
+++ ejerciciosPilas/src/comportamientos/RecogerTipoEspecifico.ts
@@ -1,18 +1,22 @@
+/// <reference path = "ComportamientoAnimado.ts"/>
+	
 class RecogerTipoEspecifico extends ComportamientoAnimado {
 	nombreAnimacion(){
 		return 'recoger';
 	}
-   alFinalizarAnimacion(){
-        if (this.receptor.tocandoTipo(this.argumentos['tipoEspecifico'])) {
-        	var objetosEscena=pilas.escena_actual().objetos;
-            var objetos2 = objetosEscena.filter(objeto => objeto.colisiona_con(this.receptor));
-            var index = objetosEscena.indexOf(objetos2[0]);
-            pilas.escena_actual().eliminarObjeto(index);
-        } else {
-            this.receptor.decir(this.argumentos['mensajeError']);
-        }
+   doActualizar(){
+   		if(this.terminoAnimacion){
+	        if (this.receptor.tocandoTipo(this.argumentos['tipoEspecifico'])) {
+	        	var objetosEscena=pilas.escena_actual().objetos;
+	            var objetos2 = objetosEscena.filter(objeto => objeto.colisiona_con(this.receptor));
+	            var index = objetosEscena.indexOf(objetos2[0]);
+	            pilas.escena_actual().eliminarObjeto(index);
+	        } else {
+	            this.receptor.decir(this.argumentos['mensajeError']);
+	        }
+    	}
+    	return true;
     }
-    
   
 }
 
