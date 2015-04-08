6ad18ee092e6754c3df6907cc767ce4902fd844c
unknown <hschinca@herlap.sadosky.local>
2015-04-08 17:07:20 -0300

    Modificado setCuadricula. Avances en elMarcianoEnElDesierto, falta terminar.

---
+++ ejerciciosPilas/src/actores/Marciano.ts
@@ -0,0 +1,12 @@
+/// <reference path="ActorAnimado.ts"/>
+
+class Marciano extends ActorAnimado {
+    constructor(x, y) {
+        super(x, y, {grilla: 'rpg/maton.png', cantColumnas:12, cantFilas: 1});
+
+        this.definirAnimacion("correr",[3,4],15);
+        this.definirAnimacion("parado",[5],5);
+        this.definirAnimacion("recoger",[4,6,4],5);
+        this.detener_animacion();
+    }
+}
---
+++ ejerciciosPilas/src/escenas/ElMarcianoEnElDesierto.ts
@@ -0,0 +1,78 @@
+/// <reference path = "../../dependencias/pilasweb.d.ts"/>
+
+/**
+ * @class ElMarcianoEnElDesierto
+ * 
+ * Objetivos: Ejercitarse en el uso de programas para la resolución de problemas.
+ * Enunciado: Comer todas las manzanas del tablero.
+ */
+class ElMarcianoEnElDesierto extends Base {
+    fondo;
+    personaje;
+    cuadricula;
+    manzanas;
+
+    iniciar() {
+        this.fondo = new Fondo('fondos/nubes.png',0,0);
+        var cantidadFilas=4;
+        var cantidadColumnas=5;
+
+        this.cuadricula = new Cuadricula(0,0,cantidadFilas,cantidadColumnas,{},
+            {grilla: 'casillaLightbot.png', 
+            cantColumnas: 5, 
+            ancho: 50, 
+            alto: 50})
+
+        this.manzanas=[];
+        var objeto= new ManzanaAnimada(0,0);
+        objeto.setCuadricula(this.cuadricula,0,0);
+        this.manzanas.push(objeto);
+
+        var objeto= new ManzanaAnimada(0,0);
+        objeto.setCuadricula(this.cuadricula,0,2);
+        this.manzanas.push(objeto);
+
+        var objeto= new ManzanaAnimada(0,0);
+        objeto.setCuadricula(this.cuadricula,0,4);
+        this.manzanas.push(objeto);
+
+        var objeto= new ManzanaAnimada(0,0);
+        objeto.setCuadricula(this.cuadricula,1,4);
+        this.manzanas.push(objeto);
+
+        var objeto= new ManzanaAnimada(0,0);
+        objeto.setCuadricula(this.cuadricula,2,4);
+        this.manzanas.push(objeto);
+
+        var objeto= new ManzanaAnimada(0,0);
+        objeto.setCuadricula(this.cuadricula,3,2);
+        this.manzanas.push(objeto);
+
+        var objeto= new ManzanaAnimada(0,0);
+        objeto.setCuadricula(this.cuadricula,3,1);
+        this.manzanas.push(objeto);
+
+        this.personaje = new Marciano(0,0);
+        this.personaje.setCuadricula(this.cuadricula,cantidadFilas-1,0);
+    }
+
+/*************** Métodos para que se cuelgue blockly ****************/
+/****** Deben tener sólo una línea, que sea un "hacer_luego" ********/
+/****** El nombre debe ser el que tendrá el bloque en blockly *******/
+
+    avanzar() {
+        this.personaje.hacer_luego(MoverACasillaDerecha);
+    }
+    
+    retroceder() {
+        this.personaje.hacer_luego(CaminaDerecha,{pasos: 2});
+    }
+    
+    martillar() {
+        this.personaje.hacer_luego(Animar,this.personaje.argumentosMartillar());
+    }
+    
+    saltar() {
+        this.personaje.hacer_luego(Saltar);
+    }
+}
