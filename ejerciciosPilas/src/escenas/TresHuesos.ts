b16d8b04cbc97c88f13adf515c321aae1e97944b
hschinca <hschinca@fundacionsadosky.org.ar>
2015-05-12 14:57:15 -0300

    TresHuesos andando.

--- ejerciciosPilas/src/escenas/TresHuesos.ts
+++ ejerciciosPilas/src/escenas/TresHuesos.ts
@@ -14,17 +14,15 @@ class TresHuesos extends Base {
     personaje;
     cuadricula;
     objetos = [];
-        
+
     iniciar() {
         this.fondo = new Fondo('fondos.nubes.png',0,0);
-        //this.robot.izquierda = pilas.izquierda();
 
         this.cuadricula = new Cuadricula(0,0,1,4,
             {alto: 70},
             {grilla: 'casillaLightbot.png', 
             cantColumnas: 5});
-        
-/*
+
         //se cargan los huesos
         var hayAlMenosUno = false;
         for(var i = 0; i < 3; i++) {
@@ -43,56 +41,22 @@ class TresHuesos extends Base {
             }
             this.agregarHueso(columna);
         }
-*/
-
-        var objeto= new Hueso(0,0);
-        objeto.setCuadricula(this.cuadricula,0,0);
-        this.objetos.push(objeto);
-
-        var objeto= new Hueso(0,0);
-        objeto.setCuadricula(this.cuadricula,0,1);
-        this.objetos.push(objeto);
-
-        var objeto= new Hueso(0,0);
-        objeto.setCuadricula(this.cuadricula,0,2);
-        this.objetos.push(objeto);
-
-        var objeto= new Hueso(0,0);
-        objeto.setCuadricula(this.cuadricula,0,3);
-        this.objetos.push(objeto);
 
         // se crea el personaje
         this.personaje = new PerroCohete(0,0);
         this.cuadricula.agregarActor(this.personaje,0,0);
     }
-/*
+
     agregarHueso(columna) {
         var objeto = new Hueso(0,0);
         this.cuadricula.agregarActor(objeto,0,columna);
-        //objeto.y -= 30;
         this.objetos.push(objeto);
     }
-*/
-/*
-    intentaronRecoger() {
-        if (this.tocandoHueso()) {
-            var objeto = this.objetos.filter(objeto => objeto.colisiona_con(this.personaje))[0];
-            var index = this.objetos.indexOf(objeto);
-            this.objetos.splice(index, 1);
-            objeto.eliminar();
-        } else {
-            this.personaje.decir("¡No hay hueso para comer!");
-        }
-    }
-*/
+
     comerHueso() {
         this.personaje.hacer_luego(RecogerPorEtiqueta,{'etiqueta' : 'Hueso', 'mensajeError' : 'No hay un hueso aqui' });
     }
-/*
-    tocandoHueso() {
-        return this.objetos.some(objeto => objeto.colisiona_con(this.personaje));
-    }
-*/
+
     avanzar() {
         this.personaje.hacer_luego(MoverACasillaDerecha);
     }
