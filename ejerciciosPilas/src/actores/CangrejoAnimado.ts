ac5f8f98f0b82be5034cb6eb30dfb1fc000206e3
charly <ciguaran@dc.uba.ar>
2015-05-18 12:14:05 -0300

    agrego actores globoAnimado y cangrejo con sus imagenes

---
+++ ejerciciosPilas/src/actores/CangrejoAnimado.ts
@@ -0,0 +1,9 @@
+/// <reference path="ActorAnimado.ts"/>
+
+class CangrejoAnimado extends ActorAnimado {
+    constructor(x, y) {
+        super(x, y, {grilla: 'cangrejo.png', cantColumnas:1, cantFilas: 1});
+        
+
+    }
+}
---
+++ ejerciciosPilas/src/actores/GloboAnimado.ts
@@ -0,0 +1,9 @@
+/// <reference path="ActorAnimado.ts"/>
+
+class GloboAnimado extends ActorAnimado {
+    constructor(x, y) {
+        super(x, y, {grilla: 'globoAnimado.png', cantColumnas:1, cantFilas: 1});
+
+
+    }
+}
