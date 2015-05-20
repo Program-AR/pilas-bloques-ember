c4de9dedaf01e671634b1f0b734167552948b193
charly <ciguaran@dc.uba.ar>
2015-05-20 15:32:22 -0300

    imagenes, actor y visor para Alimentado a los pces

---
+++ ejerciciosPilas/src/actores/BuzoAnimado.ts
@@ -0,0 +1,9 @@
+/// <reference path="ActorAnimado.ts"/>
+
+class BuzoAnimado extends ActorAnimado {
+    constructor(x, y) {
+        super(x, y, {grilla: 'buzo.png', cantColumnas:1, cantFilas: 1});
+
+
+    }
+}
