52801048e0ccb5d7c48dd1b843270bb8f7989c45
charly <ciguaran@dc.uba.ar>
2015-08-26 11:44:57 -0300

    actividad del alien refactor

--- app/actividades/actividadAlien.js
+++ app/actividades/actividadAlien.js
@@ -1,210 +1,51 @@
 import bloques from 'pilas-engine-bloques/actividades/bloques';
+import direcciones from 'pilas-engine-bloques/actividades/direccionesCuadricula';
+
 var {Accion, Sensor, Repetir,Si,Procedimiento} = bloques;
+var {IrDerecha, IrIzquierda, IrArriba, IrAbajo} = direcciones;
 
 
-var EscenaAlien = (function (_super) {
-    __extends(EscenaAlien, _super);     // jshint ignore:line
-    function EscenaAlien() {            // jshint ignore:line
-      _super.apply(this, arguments);
-    }
-
-    EscenaAlien.prototype.coord_grilla = function(fila, columna) {
-      var columnas = [-175, -105, -35, 35, 105, 175];
-      var filas = [140, 60, -20, -100, -180];
-
-      return {x: columnas[columna-1], y: filas[fila-1]};
-    };
-
-    EscenaAlien.prototype.iniciar = function() {
-
-      new pilas.fondos.Laberinto1();
-      var alien = new pilas.actores.Alien(-175, -180);
-
-      this.automata = alien;
-
-      // metodo para ver si choca con tuerca
-      alien.choca_con_tuerca = function() {
-        var actores = pilas.obtener_actores_en(alien.x, alien.y + 20, 'Tuerca');
-        return actores.length > 0;
-      };
-
-      alien.cuando_busca_recoger = function() {
-        var actores = pilas.obtener_actores_en(alien.x, alien.y + 20, 'Tuerca');
-        if (actores.length > 0) {
-          var mensaje = '';
-          actores[0].eliminar();
-          var restantes = pilas.obtener_actores_con_etiqueta('Tuerca').length;
-
-          if (restantes > 0) {
-            mensaje = 'genial, aún quedan: ' + restantes;
-          } else {
-            mensaje = '¡Nivel completado!';
-          }
-
-          // alien.decir(mensaje);
-        }
-      };
-
-      var posicion = this.coord_grilla(1, 1);
-      new pilas.actores.Tuerca(posicion.x, posicion.y);
-
-      posicion = this.coord_grilla(2, 2);
-      new pilas.actores.Tuerca(posicion.x, posicion.y);
-
-      posicion = this.coord_grilla(3, 3);
-      new pilas.actores.Tuerca(posicion.x, posicion.y);
-
-      posicion = this.coord_grilla(4, 4);
-      new pilas.actores.Tuerca(posicion.x, posicion.y);
-
-      posicion = this.coord_grilla(5, 5);
-      new pilas.actores.Tuerca(posicion.x, posicion.y);
-    };
-
-    return EscenaAlien;
-})(Base);
-
-
-var IrDerecha = Accion.extend({
-
-  init() {
+var LevantarTuerca = Accion.extend({
+  init: function() {
     this._super();
-    this.set('id', 'ir_derecha');
+    this.set('id', 'LevantaTuerca');
   },
 
-  block_init(block) {
+
+  block_init: function(block) {
     this._super(block);
     block.appendDummyInput()
-         .appendField(this.obtener_icono('derecha.png'))
-         .appendField('ir derecha');
+          .appendField('Levantar ')
+         .appendField(this.obtener_icono('../libs/data/tuerca.png'));
   },
 
-  nombre_comportamiento() {
-    return 'MoverHaciaDerecha';
+  nombre_comportamiento: function() {
+    return 'RecogerPorEtiqueta';
   },
 
-  argumentos() {
-    return '{cantidad: 68, tiempo: 1}';
+  argumentos: function() {
+    return '{\'etiqueta\' : \'TuercaAnimada\',  \'mensajeError\' : \'No hay una tuerca aqui\' }';
   }
-
-});
-
-var IrIzquierda = Accion.extend({
-
-  init() {
-    this._super();
-    this.set('id', 'ir_izquierda');
-  },
-
-  block_init(block) {
-    this._super(block);
-    block.appendDummyInput()
-         .appendField(this.obtener_icono('izquierda.png'))
-         .appendField('ir izquierda');
-  },
-
-  nombre_comportamiento() {
-    return 'MoverHaciaIzquierda';
-  },
-
-  argumentos() {
-    return '{cantidad: 68, tiempo: 1}';
-  }
-
-});
-
-
-var IrArriba = Accion.extend({
-
-  init() {
-    this._super();
-    this.set('id', 'ir_arriba');
-  },
-
-  block_init(block) {
-    this._super(block);
-    block.appendDummyInput()
-         .appendField(this.obtener_icono('arriba.png'))
-         .appendField('ir arriba');
-  },
-
-  nombre_comportamiento() {
-    return 'MoverHaciaArriba';
-  },
-
-  argumentos() {
-    return '{cantidad: 80, tiempo: 1}';
-  }
-
-});
-
-
-var IrAbajo = Accion.extend({
-
-  init() {
-    this._super();
-    this.set('id', 'ir_abajo');
-  },
-
-  block_init(block) {
-    this._super(block);
-    block.appendDummyInput()
-         .appendField(this.obtener_icono('abajo.png'))
-         .appendField('ir abajo');
-  },
-
-  nombre_comportamiento() {
-    return 'MoverHaciaAbajo';
-  },
-
-  argumentos() {
-    return '{cantidad: 80, tiempo: 1}';
-  }
-
-});
-
-var Recoger = Accion.extend({
-
-  init() {
-    this._super();
-    this.set('id', 'recoger');
-  },
-
-  block_init(block) {
-    this._super(block);
-    block.appendDummyInput()
-        .appendField('recoger')
-        .appendField(new Blockly.FieldImage('libs/data/tuerca.png', 16, 16, 'tuerca'));
-  },
-
-  nombre_comportamiento() {
-    return 'Recoger';
-  },
-
-  argumentos() {
-    return '{tiempo: 1}';
-  }
-
 });
 
 
 
-
-var ChocaConTuerca = Sensor.extend({
+var TocandoTuerca = Sensor.extend({
   init() {
     this._super();
-    this.set('id', 'choca_con_tuerca');
+    this.set('id', 'tocandoTuerca');
   },
 
   block_init(block) {
     this._super(block);
     block.appendDummyInput()
-         .appendField('choca con')
-         .appendField(new Blockly.FieldImage('libs/data/tuerca.png', 15, 15, 'tuerca'));
+         .appendField('¿tocando')
+         .appendField(this.obtener_icono('../libs/data/tuerca.png'))
+         .appendField('?');
   },
 
   nombre_sensor() {
-    return 'choca_con_tuerca()';
+    return 'tocando(\'TuercaAnimada\')';
   }
 });
 
@@ -213,7 +54,7 @@ var actividadAlien = {
   nombre: 'El alien y las tuercas',
   enunciado: 'Define un programa para que el alien junte todas las tuercas',
 
-  escena: EscenaAlien,
+  escena: AlienLevantaTuercas,
   puedeComentar: false,
   puedeDesactivar: false,
   puedeDuplicar: false,
@@ -224,8 +65,8 @@ var actividadAlien = {
 
   control: [Repetir,Si],
   expresiones: [],
-  acciones: [IrDerecha, IrIzquierda, IrArriba, IrAbajo, Recoger],
-  sensores: [ChocaConTuerca]
+  acciones: [IrDerecha, IrIzquierda, IrArriba, IrAbajo, LevantarTuerca],
+  sensores: [TocandoTuerca]
 };
 
 export default actividadAlien;
