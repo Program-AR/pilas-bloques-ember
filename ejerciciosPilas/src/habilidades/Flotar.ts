bd20e2367a9c8e90e92b3731eda7f75fad74a263
charly <ciguaran@dc.uba.ar>
2015-09-02 10:43:16 -0300

    limpieza codigo

--- ejerciciosPilas/src/habilidades/Flotar.ts
+++ ejerciciosPilas/src/habilidades/Flotar.ts
@@ -14,55 +14,12 @@ class Flotar extends Habilidad {
         this.desvio = argumentos["Desvio"];
     }
 
-    iniciar(){
-
-    }
-
     actualizar() {
       this.contador += 0.025;
       this.contador = this.contador % 256;
       //Esto es para evitar overflow.
       console.log(this.contador)
       this.receptor.y = this.altura_original + Math.sin(this.contador) * this.desvio
-
-      //this.receptor.sombra.x = this.receptor.x;
-      //this.receptor.sombra.y = this.receptor.altura_original - 20;
-//      this.receptor.sombra.z = this.receptor.z + 1;
     }
 
-
-
-
-
-
-
-
-
-
-  /*
-    class Tuerca extends Actor {
-      y_original;
-      contador;
-      sombra;
-
-      constructor(x, y) {
-
-
-
-        this.y_original = y + 20;
-        this.x = x + 10;
-        this.contador = Math.random() * 3;
-        this.sombra = new pilas.actores.Sombra();
-        this.sombra.escala = 0.25;
-      }
-
-      actualizar() {
-        this.contador += 0.025;
-        this.y = this.y_original + Math.sin(this.contador) * 5;
-        this.sombra.x = this.x;
-        this.sombra.y = this.y_original - 20;
-        this.sombra.z = this.z + 1;
-      }
-    }
-*/
 }
