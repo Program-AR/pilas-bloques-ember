046f5d7c5e775ca82d9e643cb0768947824ea0b1
charly <ciguaran@dc.uba.ar>
2015-09-22 12:25:13 -0300

    finalizando los movimientos en cuadricula

--- ejerciciosPilas/test/cuadricula_test.js
+++ ejerciciosPilas/test/cuadricula_test.js
@@ -26,6 +26,7 @@ testPilas('Set Ancho',3, function(assert) {
       assert.equal(this.cuadricula.ancho,100);
       assert.equal(this.cuadricula.casilla(0,0).ancho,20);
       assert.equal(this.cuadricula.casilla(0,0).alto,100);
+      done();
        /*Modifica el ancho de las casillas pero no el alto*/
 });
 
@@ -34,6 +35,7 @@ testPilas('Set Alto ',3, function(assert) {
       assert.equal(this.cuadricula.alto,600);
       assert.equal(this.cuadricula.casilla(0,0).ancho,40);
       assert.equal(this.cuadricula.casilla(0,0).alto,200);
+      done();
 });
 
 
@@ -48,6 +50,7 @@ testPilas('Colisionan',6, function(assert) {
       assert.equal(this.cuadricula.colisionan(this.actor3,this.actor1),false);
       assert.equal(this.cuadricula.colisionan(this.actor3,this.actor2),false);
       assert.equal(this.cuadricula.colisionan(this.actor2,this.actor3),false);
+      done();
 
 });
 
@@ -59,6 +62,7 @@ testPilas('Casilla ',7, function(assert) {
       assert.equal(this.cuadricula.casilla(0,1).nroColumna,1);
       assert.equal(this.cuadricula.casilla(1,1).nroColumna,1);
       assert.equal(this.cuadricula.casilla(1,1).nroFila,1);
+      done();
 });
 
 testPilas('Mover a casilla derecha ',3, function(assert) {
@@ -70,26 +74,41 @@ testPilas('Mover a casilla derecha ',3, function(assert) {
         assert.equal(this.actor1.y, y, 'Me mantuve en altura');
         assert.equal(this.actor1.casillaActual(), this.cuadricula.casilla(0,1), 'Mi casillaActual es la correspondiente');
       });
-      
+
 });
 
-
-
-/*
-testPilas('Mover a casilla izquierda ',2, function(assert) {
-
+testPilas('Mover a casilla izquierda ',3, function(assert) {
       this.cuadricula.agregarActor(this.actor1,0,1);
-      var x= this.actor1.x;
-      var y= this.actor1.y;
-      this.actor1.hacer_luego(MoverACasillaIzquierda);
-      assert.equal(this.actor1.x,x-this.cuadricula.casilla(0,0).ancho);
-      assert.equal(this.actor1.casillaActual(),this.cuadricula.casilla(0,0));
+      var x = this.actor1.x;
+      var y = this.actor1.y;
+      hacerLuegoConCallback(this.actor1, MoverACasillaIzquierda, {}, function(){
+        assert.equal(this.actor1.x, x - this.cuadricula.casilla(0,0).ancho, 'Movi a la izquierda bien');
+        assert.equal(this.actor1.y, y, 'Me mantuve en altura');
+        assert.equal(this.actor1.casillaActual(), this.cuadricula.casilla(0,0), 'Mi casillaActual es la correspondiente');
+      });
+
 });
-*/
-/*
-testPilas('Constructor',0, function(assert) {
-      this.cuadricula.setAlto(600);
-      assert.equal(this.cuadricula.alto,600);
-      assert.equal(this.cuadricula.casilla(0,0).ancho,40);
-      assert.equal(this.cuadricula.casilla(0,0).alto,200);
-});*/
+
+
+testPilas('Mover a casilla abajo ',3, function(assert) {
+      this.cuadricula.agregarActor(this.actor1,1,0);
+      var x = this.actor1.x;
+      var y = this.actor1.y;
+      hacerLuegoConCallback(this.actor1, MoverACasillaAbajo, {}, function(){
+        assert.equal(this.actor1.y, y - this.cuadricula.casilla(0,0).alto, 'Movi abajo bien');
+        assert.equal(this.actor1.x, x, 'Me mantuve en eje x');
+        assert.equal(this.actor1.casillaActual(), this.cuadricula.casilla(2,0), 'Mi casillaActual es la correspondiente');
+      });
+
+});
+
+testPilas('Mover a casilla Arriba ',3, function(assert) {
+      this.cuadricula.agregarActor(this.actor1,1,0);
+      var x = this.actor1.x;
+      var y = this.actor1.y;
+      hacerLuegoConCallback(this.actor1, MoverACasillaArriba, {}, function(){
+        assert.equal(this.actor1.y, y - this.cuadricula.casilla(0,0).alto, 'Movi arriba bien');
+        assert.equal(this.actor1.x, x, 'Me mantuve en eje x');
+        assert.equal(this.actor1.casillaActual(), this.cuadricula.casilla(0,0), 'Mi casillaActual es la correspondiente');
+      });
+});
