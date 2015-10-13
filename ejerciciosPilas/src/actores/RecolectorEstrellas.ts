98bd093d112e5509c6cdc595b3d081563d2f86ab
charly <ciguaran@dc.uba.ar>
2015-10-13 12:32:55 -0300

    ajustes a los personajes

--- ejerciciosPilas/src/actores/RecolectorEstrellas.ts
+++ ejerciciosPilas/src/actores/RecolectorEstrellas.ts
@@ -2,11 +2,12 @@
 
 class RecolectorEstrellas extends ActorAnimado {
     constructor(x, y) {
-        super(x, y, {grilla: 'recolectorAnimado.png', cantColumnas:1, cantFilas: 7});
+        super(x, y, {grilla: 'recolectorAnimado.png', cantColumnas:14, cantFilas: 1});
+        this.definirAnimacion("serAnimado",[0,1,2],15);
+        this.definirAnimacion("parado",[0,1,2],15);
+        this.definirAnimacion("correr",[3,4,5,6,7],5);
+        this.definirAnimacion("recoger",[7,8,9,10,11,12,13,7],60);
 
-        this.definirAnimacion("correr",[4,5,6,5],15);
-        this.definirAnimacion("parado",[4],5);
-        this.definirAnimacion("recoger",[4],5);
 
     }
 }
