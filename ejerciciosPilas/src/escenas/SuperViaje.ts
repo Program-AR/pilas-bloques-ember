a2205389be65a08218b7261565e5c8465222da5c
ciguaran@dc.uba.ar <ciguaran@dc.uba.ar>
2015-10-19 10:44:32 -0300

    super viaje refactor

--- ejerciciosPilas/src/escenas/SuperViaje.ts
+++ ejerciciosPilas/src/escenas/SuperViaje.ts
@@ -2,19 +2,19 @@
 /// <reference path = "../actores/Cuadricula.ts"/>
 /// <reference path = "../actores/PerroCohete.ts"/>
 /// <reference path = "../actores/Hueso.ts"/>
-/// <reference path = "../comportamientos/movimientosEnCuadricula.ts"/>
+/// <reference path = "../comportamientos/MovimientosEnCuadricula.ts"/>
 
 
 /**
  * @class SuperViaje
- * 
+ *
  */
 class SuperViaje extends Base {
     fondo;
     personaje;
     totalKM = 10;
     restantesKM;
-        
+
     iniciar() {
         this.fondo = new Fondo('fondos.nubes.png',0,0);
         this.personaje = new PerroCohete(0,0);
@@ -25,7 +25,7 @@ class SuperViaje extends Base {
         if (this.restantesKM == 0) {
             this.personaje.decir("¡Llegué!");
             return;
-        } 
+        }
 
         if (this.restantesKM == 1) {
             this.personaje.decir("¡Faltan 1 kilometro!");
