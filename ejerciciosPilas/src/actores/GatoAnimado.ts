a633505d74f9ec57c71e86289f809f155ba90495
ciguaran@dc.uba.ar <ciguaran@dc.uba.ar>
2015-11-16 11:32:03 -0300

    bugfix gato cerrando los ojos

--- ejerciciosPilas/src/actores/GatoAnimado.ts
+++ ejerciciosPilas/src/actores/GatoAnimado.ts
@@ -11,8 +11,7 @@ class GatoAnimado extends ActorAnimado {
         this.definirAnimacion('saludando',[15,16,16,17,18,19,19,18,17,16,16,16,16,17,18,19,19],5);
         this.definirAnimacion('acostado',[4,6,4],5);
         this.definirAnimacion('abrirOjos',[43,44,45,46],5);
-        
-        this.definirAnimacion('cerrarOjos',[36,37,38,39],5);
+        this.definirAnimacion('cerrarOjos',[38,38,38,39,39,39],5);
         this.definirAnimacion('correr',[22,23,24,25,26],6);
         this.definirAnimacion('volver',[28,29,30,31,32,33,34,35],6);
 
