8271cf24cf67dbf4e9dcd6ab9292d4e3807dd799
Hugo Ruscitti <hugoruscitti@gmail.com>
2015-11-23 14:39:26 -0300

    creando el branch gh-pages.

--- app/models/preferencium.js
+++
@@ -1,6 +0,0 @@
-import DS from 'ember-data';
-
-export default DS.Model.extend({
-  nombre: DS.attr('string'),
-  tipo: DS.attr('string'),
-});
--- app/routes/solucion/ver.js
+++
@@ -1,8 +0,0 @@
-import Ember from 'ember';
-
-export default Ember.Route.extend({
-  model() {
-    console.error("Volviendo a la vista index porque no hay un modelo para cargar.");
-    return this.transitionTo('solucion.index');
-  }
-});
