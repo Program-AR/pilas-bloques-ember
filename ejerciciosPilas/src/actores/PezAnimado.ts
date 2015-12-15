eb8fec9a9f0cd5dc2286789438c65cddb82dd9b7
ciguaran@dc.uba.ar <ciguaran@dc.uba.ar>
2015-12-15 11:06:21 -0300

    pez animado refactor

--- ejerciciosPilas/src/actores/PezAnimado.ts
+++ ejerciciosPilas/src/actores/PezAnimado.ts
@@ -3,14 +3,19 @@
 class PezAnimado extends ActorAnimado {
     constructor(x, y) {
       if(Math.random()<0.5){
-        super(x, y, {grilla: 'pez1.png', cantColumnas:1, cantFilas: 1});
+        super(x, y, {grilla: 'pez1.png', cantColumnas:4, cantFilas: 1});
+
+
       }else{
         if(Math.random()<0.5){
-          super(x, y, {grilla: 'pez2.png', cantColumnas:1, cantFilas: 1});
+          super(x, y, {grilla: 'pez2.png', cantColumnas:4, cantFilas: 1});
         }else{
-          super(x, y, {grilla: 'pez2.png', cantColumnas:1, cantFilas: 1});
+          super(x, y, {grilla: 'pez2.png', cantColumnas:4, cantFilas: 1});
         }
       }
+      this.definirAnimacion("parado",[0,1,2,3],6);
+      this.definirAnimacion("recoger",[0,1,2,3,],6);
+    
 
     }
 }
