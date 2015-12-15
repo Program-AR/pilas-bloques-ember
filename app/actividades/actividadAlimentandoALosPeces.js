bd6c235b0065f3762e2a710eacd4183cf5dd7513
ciguaran@dc.uba.ar <ciguaran@dc.uba.ar>
2015-12-15 12:09:10 -0300

    alimentando a los peces completa

--- app/actividades/actividadAlimentandoALosPeces.js
+++ app/actividades/actividadAlimentandoALosPeces.js
@@ -1,11 +1,67 @@
 /* globals AlimentandoALosPeces */
 import bloques from 'pilas-engine-bloques/actividades/bloques';
 import direcciones from 'pilas-engine-bloques/actividades/direccionesCuadricula';
-var {Si, Repetir,Hasta, Procedimiento,Funcion} = bloques;
+var {Si, Repetir,Hasta, Procedimiento,Funcion,Accion} = bloques;
 var {IrDerecha,IrIzquierda,IrAbajo,IrArriba} = direcciones;
 
-//'icono.alimento_pez.png'
 
+
+var AlimentarPez = Accion.extend({
+      init() {
+        this._super();
+        this.set('id', 'AlimentarPez');
+      },
+
+
+      block_init(block) {
+        this._super(block);
+        block.appendDummyInput()
+             .appendField('alimentar pez');
+
+;
+      },
+
+      nombre_comportamiento() {
+        return 'RecogerPorEtiqueta';
+      },
+
+
+      argumentos() {
+        return '{\'etiqueta\' : \'PezAnimado\', \'mensajeError\' : \'No hay un pez aqui\', \'idComportamiento\' : \'alimentarPez\' }';
+
+
+
+
+      }
+    });
+
+
+    var AgarrarComida = Accion.extend({
+          init() {
+            this._super();
+            this.set('id', 'AgarrarComida');
+          },
+
+
+          block_init(block) {
+            this._super(block);
+            block.appendDummyInput()
+                 .appendField('agarrar ')
+                 .appendField(this.obtener_icono('../libs/data/icono.alimento_pez.png'));
+;
+          },
+
+          nombre_comportamiento() {
+            return 'RecogerPorEtiqueta';
+          },
+
+
+          argumentos() {
+            return   '{\'etiqueta\' : \'AlimentoAnimado\',\'mensajeError\' : \'No hay una alimento aqui\', \'idComportamiento\' : \'recogerComida\' }';
+
+
+          }
+        });
 var actividadAlimentandoALosPeces = {
   nombre: 'Alimentando a los peces',
   enunciado:'..',
@@ -22,7 +78,7 @@ var actividadAlimentandoALosPeces = {
 
   control: [Si,Repetir,Hasta],
   expresiones: [Funcion],
-  acciones: [IrDerecha,IrIzquierda,IrAbajo,IrArriba],
+  acciones: [IrDerecha,IrIzquierda,IrAbajo,IrArriba,AlimentarPez,AgarrarComida],
   sensores: [],
 };
 
