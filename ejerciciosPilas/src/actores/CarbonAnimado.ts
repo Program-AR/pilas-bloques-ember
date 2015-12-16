f0bf2a43dd1b3c41b70108c1a190af9c3029f4af
ciguaran@dc.uba.ar <ciguaran@dc.uba.ar>
2015-12-16 12:53:55 -0300

    reparando la nave

--- ejerciciosPilas/src/actores/CarbonAnimado.ts
+++ ejerciciosPilas/src/actores/CarbonAnimado.ts
@@ -2,7 +2,7 @@
 
 class CarbonAnimado extends ActorAnimado {
     constructor(x, y) {
-        super(x, y, {grilla: 'carbon_animado.png', cantColumnas:1, cantFilas: 1});
+        super(x, y, {grilla: 'carbon_animado.png', cantColumnas:3, cantFilas: 1});
 
 
 
--- ejerciciosPilas/src/escenas/ReparandoLaNave.ts
+++ ejerciciosPilas/src/escenas/ReparandoLaNave.ts
@@ -18,37 +18,31 @@ class ReparandoLaNave extends EscenaActividad {
       var cantidadFilas=4
       var cantidadColumnas=5
       this.cuadricula = new Cuadricula(0,0,cantidadFilas,cantidadColumnas,
-          {},
+          {ancho:323,alto:261},
           {grilla: 'invisible.png',
-          cantColumnas: 5, alto:50, ancho:50});
+          cantColumnas: 1});
       this.automata = new MarcianoVerdeAnimado(0,0);
       this.nave= new NaveAnimada(0,0);
       this.cuadricula.agregarActor(this.nave,cantidadFilas-1,0);
-      this.cuadricula.agregarActor(this.automata,cantidadFilas-1,0);
+      this.cuadricula.agregarActorEnPerspectiva(this.automata,cantidadFilas-1,0);
       this.cuadricula.agregarActor(new HierroAnimado(0,0),0,0);
       this.cuadricula.agregarActor(new CarbonAnimado(0,0),0,cantidadColumnas-1);
-      this.tableroHierro = new Tablero(150,220,"Hierro");
-      this.tableroCarbon = new Tablero(150,230,"Carbon");
+
+      this.tableroCarbon= new Tablero(150,220,{texto:"Hierro",separacionX:30,valorInicial:0,imagen:'placacontar.png'});
+      this.tableroHierro = new Tablero(150,190,{texto:"Carbon",separacionX:30,valorInicial:0,imagen:'placacontar.png'});
       this.cantidadCarbon= new ObservadoConDisminuir(3);
       this.cantidadHierro= new ObservadoConDisminuir(3);
       this.cantidadCarbon.registrarObservador(this.tableroCarbon);
       this.cantidadHierro.registrarObservador(this.tableroHierro);
-
       var builder= new BuilderStatePattern('estoy00');
       this.definirTransiciones(builder);
       this.estado=builder.estadoInicial();
       this.secuenciaCaminata = new Secuencia({'secuencia':[ new CaminaArriba({})]})
-
       this.secuenciaCaminata.iniciar(this.automata);
-
-       this.condicion = () => { return this.automataPrincipal().y > pilas.arriba+10; }
-
+       this.condicion = () => { return this.automata.y > pilas.arriba+10; }
+       this.automata.escala=0.75;
 
  }
-
-  automataPrincipal(){
-    return this.automata;
-  }
 
   private definirTransiciones(builder){
     //modelo estoyCH como cantidad de carbon y de hierro ya depositados,
@@ -57,11 +51,6 @@ class ReparandoLaNave extends EscenaActividad {
     //Estados donde no tengo nada en la mano.
     for(var hierro=0;hierro<=3;hierro++){
         for(var carbon=0;carbon<=3;carbon++){
-            console.log("Agregando estados")
-            console.log(('estoy'+hierro)+carbon)
-            console.log((('estoy'+hierro)+carbon)+'carbon')
-            console.log((('estoy'+hierro)+carbon)+'hierro')
-
             builder.agregarEstado(('estoy'+hierro)+carbon)
             builder.agregarEstado((('estoy'+hierro)+carbon)+'carbon')
             builder.agregarEstado((('estoy'+hierro)+carbon)+'hierro')
@@ -71,9 +60,6 @@ class ReparandoLaNave extends EscenaActividad {
             for(var carbon=0;carbon<=3;carbon++){
               builder.agregarError('estoy'+hierro+carbon,'depositar','No tengo nada en la mano')
             if(hierro!=3){
-                console.log("Transición")
-                console.log((('estoy'+hierro)+carbon)+'hierro')
-                console.log((('estoy'+(hierro+1))+carbon))
                 builder.agregarTransicion((('estoy'+hierro)+carbon)+'hierro',('estoy'+(hierro+1))+carbon,'depositar')
                 builder.agregarTransicion((('estoy'+hierro)+carbon),'estoy'+(hierro)+carbon+'hierro','tomarHierro')
 
@@ -87,22 +73,6 @@ class ReparandoLaNave extends EscenaActividad {
 
 
 }
-
-
-
-  moverDerecha(){
-      this.automata.hacer_luego(MoverACasillaDerecha);
-  }
-  moverIzquierda(){
-      this.automata.hacer_luego(MoverACasillaIzquierda);
-  }
-  moverArriba(){
-      this.automata.hacer_luego(MoverACasillaArriba);
-  }
-  moverAbajo(){
-      this.automata.hacer_luego(MoverACasillaAbajo);
-  }
-
   tomarHierro(){
         this.automata.hacer_luego(TomarYContarPorEtiqueta,{'etiqueta':'HierroAnimado','mensajeError':'No hay hierro aquí','dondeReflejarValor': this.cantidadHierro,'idComportamiento' : 'tomarHierro'})
   }
