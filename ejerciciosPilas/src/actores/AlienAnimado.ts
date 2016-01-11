1777c54ee22b315044e5674008e1b0c63bb5364f
Alf Sanzo <alfredo.sanzo@gmail.com>
2016-01-11 14:46:01 -0300

    cambio animaciones buzo y alien

--- ejerciciosPilas/src/actores/AlienAnimado.ts
+++ ejerciciosPilas/src/actores/AlienAnimado.ts
@@ -5,7 +5,7 @@ class AlienAnimado extends ActorAnimado {
         super(x, y, {grilla: 'alien.png', cantColumnas:14});
         this.definirAnimacion("parado", new Cuadros(13).repetirVeces(50).concat([12, 13, 11, 12, 11, 13]).concat(new Cuadros(13).repetirVeces(30)).concat([9,9,9,9,8,8,8,8,8,8,8,8,8,8]), 4, true);
     	this.definirAnimacion("hablar", [12, 13, 11, 12, 11, 13], 15);
-    	this.definirAnimacion("recoger", [12, 10, 10, 10, 10, 12], 5);
+    	this.definirAnimacion("recoger", [12, 10, 10, 12], 6);
     	this.definirAnimacion("correr", [0, 1, 2, 3, 4, 3, 2, 1], 20);
         this.definirAnimacion("apretar",[12,6,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,6,12,13],3);
         this.definirAnimacion("SerAnimado", [0, 1, 2, 3, 4, 3, 2, 1], 20);
