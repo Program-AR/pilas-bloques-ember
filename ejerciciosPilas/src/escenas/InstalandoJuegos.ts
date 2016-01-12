f1e80963fad095070d3e19cef73799a1b4de2459
ciguaran@dc.uba.ar <ciguaran@dc.uba.ar>
2016-01-12 11:52:37 -0300

    bugfix instalando juegos

--- ejerciciosPilas/src/escenas/InstalandoJuegos.ts
+++ ejerciciosPilas/src/escenas/InstalandoJuegos.ts
@@ -34,36 +34,30 @@ class InstalandoJuegos extends EscenaActividad {
     builder.agregarEstadosPrefijados('escritoC',1,3);
     builder.agregarEstadosPrefijados('juegoInstalado',1,3);
     builder.agregarEstadosPrefijados('maquinaApagada',1,3);
-    builder.agregarTransicionesIteradas('maquinaApagada','prendido','prender',1,3,1,3)
-    builder.agregarTransicionesIteradas('prendido','escritoA','escribirA',1,3,1,3)
-    builder.agregarTransicionesIteradas('escritoA','escritoB','escribirB',1,3,1,3)
-    builder.agregarTransicionesIteradas('escritoB','escritoC','escribirC',1,3,1,3)
-    builder.agregarTransicionesIteradas('escritoC','juegoInstalado','instalar',1,3,1,3)
-    builder.agregarTransicionesIteradas('juegoInstalado','maquinaApagada','apagar',1,3,1,3)
+    builder.agregarTransicionesIteradas('prendido','escritoA','escribirA',1,3,1,3);
+    builder.agregarTransicionesIteradas('escritoA','escritoB','escribirB',1,3,1,3);
+    builder.agregarTransicionesIteradas('escritoB','escritoC','escribirC',1,3,1,3);
+    builder.agregarTransicionesIteradas('escritoC','juegoInstalado','instalar',1,3,1,3);
+    builder.agregarTransicionesIteradas('juegoInstalado','maquinaApagada','apagar',1,3,1,3);
     builder.agregarTransicion('inicial','prendido1','prender');
     builder.agregarTransicion('maquinaApagada1','prendido2','prender');
     builder.agregarTransicion('maquinaApagada2','prendido3','prender');
-
-    //builder.agregarError('inicial','prender','Para prender una compu, hay que estar frente a ella')
-    //No es necesario modelarlo, porque se encarga el comportamiento colision
-    builder.agregarError('inicial','instalar','Primero hay que prender la computadora')
-    builder.agregarError('inicial','escribirA','Primero hay que prender la computadora')
-    builder.agregarError('inicial','escribirB','Primero hay que prender la computadora')
-    builder.agregarError('inicial','escribirC','Primero hay que prender la computadora')
-
-    builder.agregarErrorAVariosEstadosDeSalida('maquinaApagada','instalar','Primero hay que prender la computadora',1,3)
-    builder.agregarErrorAVariosEstadosDeSalida('maquinaApagada','escribirC','Primero hay que prender la computadora',1,3)
-    builder.agregarErrorAVariosEstadosDeSalida('maquinaApagada','escribirA','Primero hay que prender la computadora',1,3)
-    builder.agregarErrorAVariosEstadosDeSalida('maquinaApagada','escribirB','Primero hay que prender la computadora',1,3)
-
-    builder.agregarErrorAVariosEstadosDeSalida('prendido','escribirC','Esa no es la clave correcta',1,3)
-    builder.agregarErrorAVariosEstadosDeSalida('prendido','escribirB','Esa no es la clave correcta',1,3)
-
-    builder.agregarErrorAVariosEstadosDeSalida('escritoA','escribirC','Esa no es la clave correcta',1,3)
-    builder.agregarErrorAVariosEstadosDeSalida('escritoA','escribirA','Esa no es la clave correcta',1,3)
-    builder.agregarErrorAVariosEstadosDeSalida('escritoB','escribirB','Esa no es la clave correcta',1,3)
-    builder.agregarErrorAVariosEstadosDeSalida('escritoB','escribirA','Esa no es la clave correcta',1,3)
-
+    builder.agregarError('inicial','instalar','Primero hay que prender la computadora');
+    builder.agregarError('inicial','escribirA','Primero hay que prender la computadora');
+    builder.agregarError('inicial','escribirB','Primero hay que prender la computadora');
+    builder.agregarError('inicial','escribirC','Primero hay que prender la computadora');
+    builder.agregarError('inicial','apagar','Primero hay que prender la computadora');
+    builder.agregarErrorAVariosEstadosDeSalida('maquinaApagada','instalar','Primero hay que prender la computadora',1,3);
+    builder.agregarErrorAVariosEstadosDeSalida('maquinaApagada','escribirC','Primero hay que prender la computadora',1,3);
+    builder.agregarErrorAVariosEstadosDeSalida('maquinaApagada','escribirA','Primero hay que prender la computadora',1,3);
+    builder.agregarErrorAVariosEstadosDeSalida('maquinaApagada','escribirB','Primero hay que prender la computadora',1,3);
+    builder.agregarErrorAVariosEstadosDeSalida('maquinaApagada','apagar','Primero hay que prender la computadora',1,3);
+    builder.agregarErrorAVariosEstadosDeSalida('prendido','escribirC','Esa no es la clave correcta',1,3);
+    builder.agregarErrorAVariosEstadosDeSalida('prendido','escribirB','Esa no es la clave correcta',1,3);
+    builder.agregarErrorAVariosEstadosDeSalida('escritoA','escribirC','Esa no es la clave correcta',1,3);
+    builder.agregarErrorAVariosEstadosDeSalida('escritoA','escribirA','Esa no es la clave correcta',1,3);
+    builder.agregarErrorAVariosEstadosDeSalida('escritoB','escribirB','Esa no es la clave correcta',1,3);
+    builder.agregarErrorAVariosEstadosDeSalida('escritoB','escribirA','Esa no es la clave correcta',1,3);
     this.estado=builder.estadoInicial();
   }
 
@@ -73,18 +67,20 @@ class InstalandoJuegos extends EscenaActividad {
      this.automata.escala=1;
      this.automata.y=-75;
      this.automata.x=-170;
-
    }
 
   siguienteCompu(){
     this.automata.hacer_luego(MoverACasillaDerecha)
   }
+
   prenderCompu(){
     this.automata.hacer_luego(PrenderPorEtiqueta,{'etiqueta' : 'CompuAnimada',  'mensajeError' : 'No hay una compu aqui', 'idComportamiento' : 'prender' });
   }
+
   apagarCompu(){
     this.automata.hacer_luego(ApagarPorEtiqueta,{'etiqueta' : 'CompuAnimada',  'mensajeError' : 'No hay una compu aqui', 'idComportamiento' : 'apagar' });
   }
+
   instalarJuego(){
     this.automata.hacer_luego(InstalarPorEtiqueta,{'etiqueta' : 'CompuAnimada',  'mensajeError' : 'No hay una compu aqui', 'idComportamiento' : 'instalar'})
   }
@@ -92,16 +88,17 @@ class InstalandoJuegos extends EscenaActividad {
   escribirC(){
     this.automata.hacer_luego(EscribirEnCompuAnimada,{'etiqueta' : 'CompuAnimada',  'mensajeError' : 'No hay una compu aqui', 'idComportamiento' : 'escribirC'})
   }
+
   escribirB(){
     this.automata.hacer_luego(EscribirEnCompuAnimada,{'etiqueta' : 'CompuAnimada',  'mensajeError' : 'No hay una compu aqui', 'idComportamiento' : 'escribirB'})
   }
+
   escribirA(){
     this.automata.hacer_luego(EscribirEnCompuAnimada,{'etiqueta' : 'CompuAnimada',  'mensajeError' : 'No hay una compu aqui', 'idComportamiento' : 'escribirA'})
     }
 
 
-
-  }
+}
 
 
 
@@ -117,7 +114,7 @@ class ApagarPorEtiqueta extends ComportamientoColision {
 
 class InstalarPorEtiqueta extends ComportamientoColision {
     metodo(objetoColision){
-        objetoColision.hacer_luego(ComportamientoAnimado, { nombreAnimacion: "instalado", mantenerAnimacion: true }); 
+        objetoColision.hacer_luego(ComportamientoAnimado, { nombreAnimacion: "instalado", mantenerAnimacion: true });
     }
 }
 
