fbc57461721e39db35f1becbffc406ff4471225d
Alf Sanzo <alfredo.sanzo@gmail.com>
2016-01-14 17:21:52 -0300

    modifico animaciones e imagenes mar encantado

--- ejerciciosPilas/src/actores/CofreAnimado.ts
+++ ejerciciosPilas/src/actores/CofreAnimado.ts
@@ -3,8 +3,8 @@
 class CofreAnimado extends ActorAnimado {
     constructor(x, y) {
         super(x, y, {grilla: 'cofreAnimado.png', cantColumnas:4});
-		this.definirAnimacion("abrir", [0,1,2,3], 6);
-        this.definirAnimacion("parado", new Cuadros(0).repetirVeces(50).concat([1,2,1]), 4, true);
-        this.definirAnimacion("abierto", new Cuadros(3).repetirVeces(50).concat([2,1,2]), 4);
+		this.definirAnimacion("abrir", [0,1,2,3], 3);
+        this.definirAnimacion("parado", [0,0,0,0,0,0,0,0,0,0,0,0,2], 1, true);
+        this.definirAnimacion("abierto", [3], 4);
     }
 }
