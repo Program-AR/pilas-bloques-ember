286622e964148050dcb9dc8e616975f6a7b03f1b
Alf Sanzo <alfredo.sanzo@gmail.com>
2016-01-14 18:59:59 -0300

    Masajeadas animaciones del gato y del recolector de estrellas

--- ejerciciosPilas/src/escenas/ElRecolectorDeEstrellas.ts
+++ ejerciciosPilas/src/escenas/ElRecolectorDeEstrellas.ts
@@ -1,4 +1,6 @@
 /// <reference path = "EscenaActividad.ts" />
+/// <reference path = "../actores/RecolectorEstrellas.ts" />
+/// <reference path = "../habilidades/Flotar.ts" />
 
 class ElRecolectorDeEstrellas extends EscenaActividad {
     fondo;
@@ -7,19 +9,22 @@ class ElRecolectorDeEstrellas extends EscenaActividad {
     objetos;
     estado;
     iniciar() {
-        this.estado=new SinEstado(function(escena){return escena.cantidadObjetosConEtiqueta('EstrellaAnimada')==0;})
-        this.fondo = new Fondo('fondo.recolector.png',0,0);
+        this.estado = new SinEstado(function(escena) { return escena.cantidadObjetosConEtiqueta('EstrellaAnimada') == 0; })
+        this.fondo = new Fondo('fondo.recolector.png', 0, 0);
         //this.recolector.izquierda = pilas.izquierda();
-        var cantidadFilas=4
-        var cantidadColumnas=5
+        var cantidadFilas = 4
+        var cantidadColumnas = 5
 
-        this.cuadricula = new Cuadricula(0,-20,cantidadFilas,cantidadColumnas,
-            {alto: 400},
-            {grilla: 'invisible.png',
-            cantColumnas: 1})
+        this.cuadricula = new Cuadricula(0, -20, cantidadFilas, cantidadColumnas,
+            { alto: 400 },
+            {
+                grilla: 'invisible.png',
+                cantColumnas: 1
+            })
 
-        this.automata = new RecolectorEstrellas(0,0);
-        this.cuadricula.agregarActorEnPerspectiva(this.automata,cantidadFilas-1,0);
+        this.automata = new RecolectorEstrellas(0, 0);
+        this.cuadricula.agregarActorEnPerspectiva(this.automata, cantidadFilas - 1, 0);
+        this.automata.aprender(Flotar, {});
         // La posición inicial pretende respectar el ejemplo
 
         this.objetos=[];
@@ -27,6 +32,7 @@ class ElRecolectorDeEstrellas extends EscenaActividad {
             for(var columna=1;columna<cantidadColumnas;columna++){
                 var objeto= new EstrellaAnimada(0,0);
                 this.cuadricula.agregarActor(objeto,fila,columna);
+                objeto.escala *= 0.7;
                 this.objetos.push(objeto)
             }
         }
