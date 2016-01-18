b059a302050e48f715f8bea8282f951d308de997
Alf Sanzo <alfredo.sanzo@gmail.com>
2016-01-18 11:52:15 -0300

    arreglo animación tito

--- ejerciciosPilas/src/actores/Tito.ts
+++ ejerciciosPilas/src/actores/Tito.ts
@@ -2,11 +2,11 @@
 
 class Tito extends ActorAnimado {
     constructor(x, y) {
-        super(x, y, {grilla: 'tito.png', cantColumnas:6, cantFilas: 1});
+        super(x, y, {grilla: 'tito.png', cantColumnas:8, cantFilas: 1});
         
-        this.definirAnimacion("correr",[1,2,3,4,4,4,4,4,4,4,3,2,5],12);
-        this.definirAnimacion("parado",[0,1,2],2, true);
-        this.definirAnimacion("recoger", [0,2,1,1,1,2,0], 9);
+        this.definirAnimacion("correr",[3,4,5,6,6,6,6,6,6,5,4,8],12);
+        this.definirAnimacion("parado",[0,1,2,3,4],4, true);
+        this.definirAnimacion("recoger", [0,1,2,2,2,2,2,2,3,4], 9);
 
     }
 } 
