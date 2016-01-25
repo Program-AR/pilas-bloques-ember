e6de035fe5156793b222979afb38ba286ec04a79
Alf Sanzo <alfredo.sanzo@gmail.com>
2016-01-25 16:39:34 -0300

    arreglando tamaño laberinto largo

--- ejerciciosPilas/src/escenas/LaberintoLargo.ts
+++ ejerciciosPilas/src/escenas/LaberintoLargo.ts
@@ -10,9 +10,10 @@ class LaberintoLargo extends EscenaActividad {
     iniciar() {
         this.estado=undefined;
         this.fondo = new Fondo('fondo.laberinto.largo.png',0,0);
-        this.cuadricula = new CuadriculaParaRaton(0,0,10,10,{'alto':400,'ancho':300},{'->':'casillaDerecha.png','<-':'casillaIzquierda.png','v':'casillaAbajo.png','^':'casillaArriba.png'}).dameCamino();
+        this.cuadricula = new CuadriculaParaRaton(0,0,8,8,{'alto':440,'ancho':400},{'->':'casillaDerecha.png','<-':'casillaIzquierda.png','v':'casillaAbajo.png','^':'casillaArriba.png'}).dameCamino();
         this.automata = new RatonAnimado(0,0);
         this.cuadricula.agregarActorEnPerspectiva(this.automata,0,0);
+        this.automata.escala *= 1.6;
     }
 
     estaResueltoElProblema(){
