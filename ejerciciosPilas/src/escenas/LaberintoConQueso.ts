86492ff0a513ba2b31f1e5479aaa4662e6f5ac3f
charly <ciiguaran@gmail.com>
2016-01-26 17:38:17 -0300

    escena laberinto con queso refactor

--- ejerciciosPilas/src/escenas/LaberintoConQueso.ts
+++ ejerciciosPilas/src/escenas/LaberintoConQueso.ts
@@ -5,34 +5,17 @@
 /// <reference path = "../comportamientos/RecogerPorEtiqueta.ts" />}
 /// <reference path="Camino.ts"/>
 
-class LaberintoConQueso extends EscenaActividad {
-    estado;
-    automata;    cuadricula;    queso; secuenciaCaminata; condicion;
+class LaberintoConQueso extends LaberintoLargo {
+  
     iniciar() {
-        this.estado=undefined;
-        this.cuadricula = new CuadriculaParaRaton(0,0,10,10,{'alto':400,'ancho':300},{'->':'casillaDerecha.png','<-':'casillaIzquierda.png','v':'casillaAbajo.png','^':'casillaArriba.png'}).dameCamino();
+        super.iniciar();
         this.cuadricula.completarConObjetosRandom([QuesoAnimado]);
-        this.automata=new RatonAnimado(0,0)
-        this.cuadricula.agregarActor(this.automata,0,0);
+    }
+     dameOpcionesCuadricula(){
+      return {'alto':440,'ancho':400};
     }
 
-    valorCondicion(argumentos){
-      return argumentos.receptor.y > 250;
-    }
-
-    personajePrincipal(){
-      return this.automata;
-    }
-
-    moverDerecha(){
-      this.automata.hacer_luego(MoverACasillaDerecha);
-    }
-
-    moverAbajo(){
-      this.automata.hacer_luego(MoverACasillaAbajo);
-    }
-
-    ComerQueso(){
-      this.automata.hacer_luego(RecogerPorEtiqueta,{'etiqueta' : 'QuesoAnimado',  'mensajeError' : 'No hay queso aqui' });
-    }
+    nombreFondo(){
+      return 'fondo.laberinto.queso.png';
+    }    
 }
