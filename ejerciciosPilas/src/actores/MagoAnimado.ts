6fb69eb689d7e0d30530c623e55f42d0ce90ae9a
Alf Sanzo <alfredo.sanzo@gmail.com>
2016-02-10 20:46:26 -0300

    fixes animaciones q cuelgan todo

--- ejerciciosPilas/src/actores/MagoAnimado.ts
+++ ejerciciosPilas/src/actores/MagoAnimado.ts
@@ -4,7 +4,7 @@ class MagoAnimado extends ActorAnimado {
     constructor(x, y) {
         super(x, y, {grilla: 'mago.png', cantColumnas:4, cantFilas:2});
         this.definirAnimacion("parado", new Cuadros(1).repetirVeces(16).concat([2,2,2,2,2]), 2, true);
-        this.definirAnimacion("darEspada", new Cuadros([1, 3, 4, 5, 5, 6, 6, 7, 7]).repetirVeces(1).concat(new Cuadros(0).repetirVeces(99999)), 6);
+        this.definirAnimacion("darEspada", new Cuadros([1, 3, 4, 5, 5, 6, 6, 7, 7]).repetirVeces(1).concat(new Cuadros(0).repetirVeces(999)), 6);
         this.definirAnimacion("paradoConSombrero", [0], 12);
     }
 } 
