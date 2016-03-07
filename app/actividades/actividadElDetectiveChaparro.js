cf40f386014a58f454955253c93772d3a59de704
hschinca <hschinca@users.noreply.github.com>
2016-03-07 19:00:43 -0300

    Enunciado + "Sabías qué"

--- app/actividades/actividadElDetectiveChaparro.js
+++ app/actividades/actividadElDetectiveChaparro.js
@@ -31,8 +31,8 @@ var EsCulpable = AccionBuilder.buildSensor({
 export default {
   nombre: 'El detective Chaparro',
   id: 'ElDetectiveChaparro',
-  enunciado: 'TODO',
-  consignaInicial: 'TODO',
+  enunciado: 'El detective debe descubrir al culpable de un crimen, quitándole el disfraz que lo camufla. Comenzando por el primero de la izquierda, ¡interrogá a cada uno de los sospechosos hasta encontrar al culpable!',
+  consignaInicial: 'El bloque "Repetir hasta que" nos permite terminar el programa cuando encontramos al culpable sin tener que interrogar a todos los sospechosos de la fila.',
   
   // la escena proviene de ejerciciosPilas
   escena: ElDetectiveChaparro,  // jshint ignore:line
