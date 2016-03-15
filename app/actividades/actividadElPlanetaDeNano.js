d7880b27524e41faf983fe58d953f7bb722b5f7a
hschinca <hschinca@users.noreply.github.com>
2016-03-15 16:27:27 -0300

    Update actividadElPlanetaDeNano.js
    Enunciado + "Sabías qué..."

--- app/actividades/actividadElPlanetaDeNano.js
+++ app/actividades/actividadElPlanetaDeNano.js
@@ -21,6 +21,7 @@ var VolverAlBordeIzquierdo = AccionBuilder.build({
 export default {
   nombre: 'El planeta de Nano',
   enunciado: 'Ayudá a Nano a recoger todas sus frutas. ¡Cuidado! No se puede bajar...',
+  consignaInicial: 'A los procedimientos se les pueden agregar parámetros para que resulten más generales. Por ejemplo, si creamos los procedimientos "Avanzar 2 casillas", "Avanzar 3 casillas" y "Avanzar 4 casillas", podemos reemplazar a los 3 por un solo procedimiento que reciba como parámetro la cantidad de casillas que queremos avanzar: "Avanzar [cantidad] casillas".',
   id: 'ElPlanetaDeNano',
   // la escena proviene de ejerciciosPilas
   escena: ElPlanetaDeNano, // jshint ignore:line
