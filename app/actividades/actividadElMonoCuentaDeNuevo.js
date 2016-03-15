f8ca2953fde97af0176b08e27ede9f2b18337614
hschinca <hschinca@users.noreply.github.com>
2016-03-15 15:17:00 -0300

    Update actividadElMonoCuentaDeNuevo.js
    Enunciado + Pista + "Sabías qué..."

--- app/actividades/actividadElMonoCuentaDeNuevo.js
+++ app/actividades/actividadElMonoCuentaDeNuevo.js
@@ -25,8 +25,8 @@ var LargoFilaActual = VariableLocalGet.extend({
 export default {
   nombre: 'El mono cuenta de nuevo',
   id: 'ElMonoCuentaDeNuevo',
-  enunciado: 'COMPLETAR',
-  consignaInicial: 'COMPLETAR.',
+  enunciado: 'El mono tiene que contar otra vez las frutas, ¡pero ahora no puede verificar si ya llegó al final de una columna! Pista: mirá en la categoría "Variables" si hay algo que te pueda servir.',
+  consignaInicial: 'Una variable nos permite guardar información que puede cambiar en cada ejecución del programa, incluso en una misma ejecución. Por ejemplo, el largo de cada columna varía dependiendo en qué columna esté parado el mono.',
 
   // la escena proviene de ejerciciosPilas
   escena: ElMonoCuentaDeNuevo,  // jshint ignore:line
