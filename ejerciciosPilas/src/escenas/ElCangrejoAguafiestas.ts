179ffc56439e0ed1f31e8eeb72c4c6c7412f2375
Alf Sanzo <alfredo.sanzo@gmail.com>
2016-05-04 13:04:02 -0300

    fix imagen casilla cangrejo y proporciones cangrejo

--- ejerciciosPilas/src/escenas/ElCangrejoAguafiestas.ts
+++ ejerciciosPilas/src/escenas/ElCangrejoAguafiestas.ts
@@ -21,21 +21,26 @@
           ['T','T','T','T','T','T'],
           ['T','F','F','F','F','T'],
           ['T','T','T','T','T','T']]
-        this.cuadricula = new CuadriculaEsparsa(0,0,{alto: 400, ancho:400},{grilla:'casilla.cangrejo_aguafiestas.png'},matriz)
+        this.cuadricula = new CuadriculaEsparsa(0,15,{alto: 360, ancho:400},{grilla:'casilla.cangrejo_aguafiestas.png'},matriz)
         this.completarConGlobos();
         this.automata = new CangrejoAnimado(0,0);
+        this.automata.escala *= 1.2;
         this.cuadricula.agregarActor(this.automata,0,0);
       }
 
     private completarConGlobos(){
-      this.cuadricula.casillas.forEach(c => this.agregarGlobo(c.nroFila,c.nroColumna));
+      this.cuadricula.casillas.forEach(c => {if(!c.esEsquina()) this.agregarGlobo(c.nroFila,c.nroColumna)});
     }
 
     private agregarGlobo(fila,col){
       var globo = new GloboAnimado();
       this.cuadricula.agregarActor(globo,fila,col,false);
-      globo.y += 30;
+      globo.y += 20;
       globo.escala *= 0.8;
       globo.aprender(Flotar,{Desvio:5});
+    }
+
+    estaResueltoElProblema(){
+      return this.contarActoresConEtiqueta('GloboAnimado') === 1; // porque el programa termina antes de que se elimine el último globo
     }
 }
