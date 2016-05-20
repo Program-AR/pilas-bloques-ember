5e0cc022c6df7d7884975566964c1a5615d8235c
teresalberto <terealberto@gmail.com>
2016-05-20 14:03:44 -0300

    Edité el enunciado

--- app/actividades/actividadTitoCuadrado.js
+++ app/actividades/actividadTitoCuadrado.js
@@ -7,7 +7,7 @@ import {Numero,OpComparacion,OpAritmetica,Texto} from 'pilas-engine-bloques/acti
 export default {
   nombre: 'Tito cuadrado',
   id: 'TitoCuadrado',
-  enunciado: 'Tito debe encender todas las luces del cuadrado. Tené en cuenta que las casillas de la esquina nunca se prenden y que el tamaño del cuadrado no varía de una ejecución a la otra.',
+  enunciado: 'Tito debe encender todas las luces del cuadrado, en cada ejecución distribuidas de una manera diferente. Tené en cuenta que las casillas de la esquina nunca se prenden y que el tamaño del cuadrado no varía de una ejecución a la otra.',
 
   escena: TitoCuadrado,  // jshint ignore:line
   puedeComentar: false,
