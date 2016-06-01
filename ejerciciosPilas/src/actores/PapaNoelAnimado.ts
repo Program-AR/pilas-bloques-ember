1998cb6732893aca9e19769b2781e82445655395
Alf Sanzo <alfredo.sanzo@gmail.com>
2016-06-01 11:23:13 -0300

    Arreglo animaciones papa noel

--- ejerciciosPilas/src/actores/PapaNoelAnimado.ts
+++ ejerciciosPilas/src/actores/PapaNoelAnimado.ts
@@ -3,12 +3,10 @@
 class PapaNoelAnimado extends ActorAnimado {
     constructor(x, y) {
         super(x, y, {grilla: 'papaNoel.png', cantColumnas:11});
-        this.definirAnimacion('correr',[0,1,2,3,2,1],6);
+        this.definirAnimacion('correr',[0,1,2,3,4,5,6],6);
         this.definirAnimacion('parado',
-          new Cuadros([0,6,5]).repetirVeces(1).
-          concat(new Cuadros([5]).repetirVeces(40).
-          concat([5,6,0]).
-          concat(new Cuadros([0]).repetirVeces(40))),6,true);
+          new Cuadros([0]).repetirVeces(40).
+          concat(new Cuadros([1]).repetirVeces(40)),6,true);
         this.definirAnimacion('recoger',[7,8,9,10,11],6);
         this.definirAnimacion('depositar',[11,10,9,8,7],6);
     }
