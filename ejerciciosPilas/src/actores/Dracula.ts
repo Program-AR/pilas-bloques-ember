58dcc12b57349c6318fb1471edde52795ac0901f
Alf Sanzo <alfredo.sanzo@gmail.com>
2016-06-08 16:02:21 -0300

    cambio animacion drácula

--- ejerciciosPilas/src/actores/Dracula.ts
+++ ejerciciosPilas/src/actores/Dracula.ts
@@ -2,9 +2,9 @@
 
 class Dracula extends ActorAnimado {
     constructor(x=0, y=0) {
-        super(x, y, {grilla: 'dracula.png', cantColumnas:14});
-        this.definirAnimacion("bailando", [8,9,10,11,12,13,12,11,10,9], 6);
+        super(x, y, {grilla: 'dracula.png', cantColumnas:15});
+        this.definirAnimacion("bailando", [9,10,11,12,13,14,13,12,11,10], 6);
         this.definirAnimacion("parado", [0], 12, true);
-        this.definirAnimacion("aparecer", [0,1,2,3,4,5,6,7], 6);
+        this.definirAnimacion("aparecer", [0,1,2,3,4,5,6,7,8], 6);
     }
 }
