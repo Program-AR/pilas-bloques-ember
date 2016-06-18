6685169af63186fa22936241e4014325035b6de6
Hugo Ruscitti <hugoruscitti@gmail.com>
2016-06-18 15:15:50 -0300

    Comenzando a serializar el código de la solución #102

--- app/controllers/desafio.js
+++ app/controllers/desafio.js
@@ -2,9 +2,12 @@ import Ember from 'ember';
 
 export default Ember.Controller.extend({
   pilas: Ember.inject.service(),
+  queryParams: ['codigo'],
+  codigo: '',
 
   actions: {
     cuandoCargaPilas(/*pilas*/) {
+      this.set('codigo', 'demo');
       //console.log("SE CARGO PILAS !!!");
     }
   }
