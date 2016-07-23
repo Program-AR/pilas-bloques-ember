0997744cdcb7aaf1baa41c1db3482f17aa12e8c0
Hugo Ruscitti <hugoruscitti@gmail.com>
2016-07-22 21:33:00 -0300

    Haciendo que el modo depuración mantenga los paneles persistentes en la URL.

--- app/controllers/desafio.js
+++ app/controllers/desafio.js
@@ -2,7 +2,7 @@ import Ember from 'ember';
 
 export default Ember.Controller.extend({
   pilas: Ember.inject.service(),
-  queryParams: ['codigo', 'debug', 'panelCanvasVisible'],
+  queryParams: ['codigo', 'debug', 'panelCanvasVisible', 'panelBlocklyVisible', 'panelCodigoVisible'],
   codigo: null,
   debug: false,
   panelCanvasVisible: true,
