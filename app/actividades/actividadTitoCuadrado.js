c50a26a3e1765df6dc655e1a5c1fcd67d2640913
Hugo Ruscitti <hugoruscitti@gmail.com>
2016-08-06 00:16:54 -0300

    Migrando bloques para mover en forma genérica en una dirección.

--- app/actividades/actividadTitoCuadrado.js
+++ app/actividades/actividadTitoCuadrado.js
@@ -1,8 +1,85 @@
-import {Repetir, Si, Sino, Hasta, Procedimiento} from 'pilas-engine-bloques/actividades/bloques';
+import {Repetir, Bloque, Si, Sino, Hasta, Procedimiento, Accion} from 'pilas-engine-bloques/actividades/bloques';
 import {IrAbajo, IrIzquierda, IrDerecha, IrArriba} from 'pilas-engine-bloques/actividades/direccionesCuadricula';
-import {EncenderLuz,TocandoLuz} from 'pilas-engine-bloques/actividades/bloquesTito';
-import {Numero,OpComparacion,OpAritmetica,Texto} from 'pilas-engine-bloques/actividades/expresiones';
+import {EncenderLuz, TocandoLuz} from 'pilas-engine-bloques/actividades/bloquesTito';
+import {OpComparacion, OpAritmetica} from 'pilas-engine-bloques/actividades/expresiones';
+import {Valores} from 'pilas-engine-bloques/actividades/categorias';
 
+var MoverA = Accion.extend({
+
+  init() {
+    this._super();
+    this.set('id', 'MoverA');
+  },
+
+  block_init(block) {
+    this._super(block);
+    block.appendValueInput('direccion')
+    .setCheck('Number')
+    .appendField('Mover a');
+  },
+
+  block_javascript(block) {
+    var direccionDesdeParametro = this.obtenerDireccionDesdeParametro(block);
+    var comportamiento = this.obtenerComportamiento(direccionDesdeParametro);
+
+    if (!comportamiento) {
+      console.error(direccionDesdeParametro);
+      alert("No puedo ir en esa dirección...");
+      return ``;
+    }
+
+    let funcion = "function() {return {};}";
+    return `programa.llamada_proc_primitivo(${comportamiento}, ${funcion});`;
+  },
+
+  obtenerComportamiento(direccionDesdeParametro) {
+    let mapaDirecciones = {
+      derecha: "MoverACasillaDerecha",
+      izquierda: "MoverACasillaIzquierda",
+      arriba: "MoverACasillaArriba",
+      abajo: "MoverACasillaAbajo",
+    };
+
+    if (direccionDesdeParametro in mapaDirecciones) {
+      return mapaDirecciones[direccionDesdeParametro];
+    }
+
+    return null;
+  },
+
+  obtenerDireccionDesdeParametro(block) {
+    let tipo = Blockly.JavaScript.ORDER_ATOMIC;
+    return Blockly.JavaScript.valueToCode(block, 'direccion', tipo);
+  },
+
+});
+
+
+var LaDerecha = Bloque.extend({
+  _categoria: Valores,
+
+  init() {
+    this._super();
+    this.set('id', 'LaDerecha');
+  },
+
+  block_init(block) {
+    this._super(block);
+
+    block.setColour(Blockly.Blocks.sensores.COLOUR);
+    block.setInputsInline(true);
+    block.setOutput(true);
+
+    block.appendDummyInput()
+    .appendField(this.obtener_icono('derecha.png'))
+    .appendField('la derecha');
+  },
+
+  block_javascript() {
+    return ['derecha', Blockly.JavaScript.ORDER_ATOMIC];
+  },
+
+});
 
 export default {
   nombre: 'Tito cuadrado',
@@ -14,6 +91,7 @@ export default {
   puedeDesactivar: false,
   puedeDuplicar: false,
 
-  bloques: [Procedimiento, Repetir, Si, Sino, Hasta, TocandoLuz, EncenderLuz,
-    IrAbajo,IrArriba,IrIzquierda,IrDerecha,Numero,OpComparacion,OpAritmetica,Texto],
+  bloques: [MoverA, LaDerecha, Procedimiento, Repetir, Si, Sino, Hasta,
+            TocandoLuz, EncenderLuz, IrAbajo, IrArriba, IrIzquierda, IrDerecha,
+            OpComparacion, OpAritmetica],
 };
