c97bf2722a6a8a5d994a8030bd350c052c7689df
Hugo Ruscitti <hugoruscitti@gmail.com>
2016-08-26 20:05:15 -0300

    Arreglos menores y actualizando blockly.

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -14,32 +14,6 @@ app.import('bower_components/bootstrap/dist/css/bootstrap.css');
 app.import('vendor/prism/prism.css');
 app.import('vendor/prism/prism.js');
 
-//app.import('vendor/pilasweb.js');
-
-
-//app.import('bower_components/bootstrap/dist/js/bootstrap.js');
-
-/*
-app.import('bower_components/bootstrap/dist/fonts/glyphicons-halflings-regular.eot', {
-    destDir: 'fonts'
-});
-app.import('bower_components/bootstrap/dist/fonts/glyphicons-halflings-regular.ttf', {
-    destDir: 'fonts'
-});
-app.import('bower_components/bootstrap/dist/fonts/glyphicons-halflings-regular.svg', {
-    destDir: 'fonts'
-});
-app.import('bower_components/bootstrap/dist/fonts/glyphicons-halflings-regular.woff', {
-    destDir: 'fonts'
-});
-app.import('bower_components/bootstrap/dist/fonts/glyphicons-halflings-regular.woff2', {
-    destDir: 'fonts'
-});
-app.import('bower_components/bootstrap/dist/css/bootstrap.css.map', {
-    destDir: 'assets'
-});
-*/
-
     app.import("vendor/libs/blockly/blockly_compressed.js");
     app.import("vendor/libs/blockly/blocks_compressed.js");
     app.import("vendor/libs/blockly/javascript_compressed.js");
