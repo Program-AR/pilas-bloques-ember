fc64f460ffc5f7b4c732e88c10e11199a52da17b
Hugo Ruscitti <hugoruscitti@gmail.com>
2016-10-07 12:05:54 -0300

    Modificando script de deploy a surge por branchs refs #231

--- config/environment.js
+++ config/environment.js
@@ -60,7 +60,8 @@ module.exports = function(environment) {
   }
 
   if (environment === 'production') {
-    ENV['cursoBackendURL'] = 'http://api.pilasbloques.program.ar';
+    // ENV['cursoBackendURL'] = 'http://api.pilasbloques.program.ar';
+    ENV['cursoBackendURL'] = 'http://testing-pilas-bloques-api.enjambrelab.com.ar';
 
 
     ENV['ember-cli-mirage'] = {
