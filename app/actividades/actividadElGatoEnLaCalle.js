c966e57cc820d749eed51ec426833206ef2cd348
Alf Sanzo <alfredo.sanzo@gmail.com>
2016-11-07 13:27:11 -0300

    comenzando a agregar FSM al gato. sin testear, ya terminado. #187

--- app/actividades/actividadElGatoEnLaCalle.js
+++ app/actividades/actividadElGatoEnLaCalle.js
@@ -6,56 +6,56 @@ var Saludar = AccionBuilder.build({
   descripcion: 'Saludar',
   icono: 'icono.saludar.png',
   comportamiento: 'ComportamientoAnimado',
-  argumentos: '{nombreAnimacion: "saludando"}',
+  argumentos: '{nombreAnimacion: "saludando", idTransicion: "saludar"}',
 });
 
 var AbrirOjos = AccionBuilder.build({
   descripcion: 'Abrir ojos',
   icono: 'icono.abrirOjos.png',
   comportamiento: 'AnimarSiNoEstoyYa',
-  argumentos: '{nombreAnimacion: "abrirOjos", valorEstar: "con los ojos abiertos", descripcionEstar: "estadoOjos", nombreAnimacionSiguiente: "parado", arrancoAsi:true}',
+  argumentos: '{nombreAnimacion: "abrirOjos", valorEstar: "con los ojos abiertos", descripcionEstar: "estadoOjos", nombreAnimacionSiguiente: "parado", arrancoAsi:true, idTransicion: "abrirOjos"}',
 });
 
 var CerrarOjos = AccionBuilder.build({
   descripcion: 'Cerrar ojos',
   icono: 'icono.cerrarOjos.png',
   comportamiento: 'AnimarSiNoEstoyYa',
-  argumentos: '{nombreAnimacion: "cerrarOjos", valorEstar: "con los ojos cerrados", descripcionEstar: "estadoOjos", nombreAnimacionSiguiente: "ojosCerrados"}',
+  argumentos: '{nombreAnimacion: "cerrarOjos", valorEstar: "con los ojos cerrados", descripcionEstar: "estadoOjos", nombreAnimacionSiguiente: "ojosCerrados", idTransicion: "cerrarOjos"}',
 });
 
 var Acostarse = AccionBuilder.build({
   descripcion: 'Acostarse',
   icono: 'icono.acostarse.png',
   comportamiento: 'ModificarRotacionYAltura',
-  argumentos: '{alturaIr: -180 ,rotacionIr: 90, nombreAnimacion:"acostado", valorEstar: "acostado", descripcionEstar: "posicionCuerpo"}',
+  argumentos: '{alturaIr: -180 ,rotacionIr: 90, nombreAnimacion:"acostado", valorEstar: "acostado", descripcionEstar: "posicionCuerpo", idTransicion: "acostarse"}',
 });
 
 var Pararse = AccionBuilder.build({
   descripcion: 'Pararse',
   icono: 'icono.pararse.png',
   comportamiento: 'ModificarRotacionYAltura',
-  argumentos: '{alturaIr: -150 ,rotacionIr: 0, nombreAnimacion:"acostado", valorEstar: "parado", descripcionEstar: "posicionCuerpo", arrancoAsi:true}',
+  argumentos: '{alturaIr: -150 ,rotacionIr: 0, nombreAnimacion:"acostado", valorEstar: "parado", descripcionEstar: "posicionCuerpo", arrancoAsi:true, idTransicion: "levantarse"}',
 });
 
 var Volver = AccionBuilder.build({
   descripcion: 'Volver',
   icono: '../../iconos/izquierda.png',
   comportamiento: 'MovimientoAnimado',
-  argumentos: '{direccion: new Direct(-1,0), distancia: 50}',
+  argumentos: '{direccion: new Direct(-1,0), distancia: 50, idTransicion: "volver"}',
 });
 
 var Avanzar = AccionBuilder.build({
   descripcion: 'Avanzar',
   icono: '../../iconos/derecha.png',
   comportamiento: 'MovimientoAnimado',
-  argumentos: '{direccion: new Direct(1,0), distancia: 50}',
+  argumentos: '{direccion: new Direct(1,0), distancia: 50, idTransicion: "avanzar"}',
 });
 
 var Soniar = AccionBuilder.build({
   descripcion: 'Soñar',
   icono: 'icono.soniar.png',
   comportamiento: 'Pensar',
-  argumentos: '{mensaje: "ZZzzZzZ...", hayQueAnimar: false}',
+  argumentos: '{mensaje: "ZZzzZzZ...", hayQueAnimar: false, idTransicion: "soniar"}',
 });
 
 export default {
