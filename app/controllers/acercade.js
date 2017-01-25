3eb602dc403625cd07b28ca559957b063ac01348
Alf Sanzo <alfredo.sanzo@gmail.com>
2017-01-25 14:47:30 -0300

    pequeña corrección y limpieza de link

--- app/controllers/acercade.js
+++ app/controllers/acercade.js
@@ -4,11 +4,4 @@ export default Ember.Controller.extend({
   notificador: Ember.inject.service(),
   hayActualizacion: Ember.computed.alias('notificador.hayActualizacion'),
   versionMasReciente: Ember.computed.alias('notificador.versionActual'),
-
-  actions: {
-    visitarWeb() {
-      var url = "http://bloques.pilas-engine.com.ar";
-      window.open(url);
-    },
-  }
 });
