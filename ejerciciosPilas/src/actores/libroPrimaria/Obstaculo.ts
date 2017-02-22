cf14d7356060bcd2a839bf0a0efce178138cf964
Alf Sanzo <alfredo.sanzo@gmail.com>
2017-02-22 15:26:47 -0300

    obstaculo genérico

---
+++ ejerciciosPilas/src/actores/libroPrimaria/Obstaculo.ts
@@ -0,0 +1,15 @@
+/// <reference path="../ActorAnimado.ts"/>
+
+/**
+ * El objetivo es que cualquier obstáculo de una cuadrícula tenga la misma forma
+*/
+class Obstaculo extends ActorAnimado {
+		constructor(imagenes) {
+			super(0, 0, {grilla: this.randomDe(imagenes), cantColumnas:4, cantFilas: 1});
+			this.definirAnimacion("parado",new Cuadros(0).repetirVeces(100).concat([1,2,3,3,2,1]),6,true);
+		}
+
+		randomDe(lista){
+				 return lista[Math.floor(Math.random()*lista.length)];
+		}
+}
