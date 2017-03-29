55a278c7ab5167c4b8b118277d9e79b587363161
Alf Sanzo <alfredo.sanzo@gmail.com>
2017-03-29 15:41:01 -0300

    el commit anterior me olvide algo

--- tests/integration/components/pilas-blockly-test.js
+++ tests/integration/components/pilas-blockly-test.js
@@ -1,4 +1,4 @@
-import { moduleForComponent, test } from 'ember-qunit';
+import { moduleForComponent, skip } from 'ember-qunit';
 import hbs from 'htmlbars-inline-precompile';
 
 moduleForComponent('pilas-blockly', 'Integration | Component | pilas blockly', {
