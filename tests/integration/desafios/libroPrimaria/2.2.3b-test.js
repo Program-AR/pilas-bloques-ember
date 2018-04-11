6859d1d6dd9233996745c0ef95f73a75da8fce29
Franco Frizzo <francofrizzo@gmail.com>
2018-04-11 15:14:51 -0300

    Agregando test de solución incorrecta a desafío 2.2.3b Tandil

--- tests/integration/desafios/libroPrimaria/2.2.3b-test.js
+++ tests/integration/desafios/libroPrimaria/2.2.3b-test.js
@@ -5,6 +5,32 @@ let nombre = "2.2.3b";
 moduloActividad(nombre);
 
 actividadTest(nombre, {
+  descripcionAdicional: "La solución provista no resuelve el problema",
+  solucion: `<xml xmlns="http://www.w3.org/1999/xhtml">
+  <block type="al_empezar_a_ejecutar" id="1" deletable="false" movable="false" editable="false" x="15" y="15">
+  <statement name="program">
+    <block type="MoverACasillaIzquierda">
+    <next>
+    <block type="MoverACasillaArriba">
+    <next>
+    <block type="MoverACasillaArriba">
+    <next>
+    <block type="ComerChurrasco">
+    </block>
+    </next>
+    </block>
+    </next>
+    </block>
+    </next>
+    </block>
+  </statement>
+  </block>
+</xml>`,
+  errorEsperado: "Hay un obstáculo para arriba",
+});
+
+actividadTest(nombre, {
+  descripcionAdicional: "La solución corregida resuelve el problema",
 	solucion: `<xml xmlns="http://www.w3.org/1999/xhtml">
     <block type="al_empezar_a_ejecutar" id="1" deletable="false" movable="false" editable="false" x="15" y="15">
     <statement name="program">
