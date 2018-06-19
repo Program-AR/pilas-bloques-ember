9e0e19b39253be36b9a45cb5011fa46de38b4890
Alfredo Sanzo <alfredo.sanzo@gmail.com>
2018-06-19 20:27:24 -0300

    cambio splash screen, actualizo imágenes, toco acerca de

--- mirage/fixtures/libros.js
+++ mirage/fixtures/libros.js
@@ -2,15 +2,15 @@ export default [
   {
     id: 1,
     nombre: 'programar',
-    titulo: 'Curso completo Program.AR',
-    descripcion: 'Todos los desafíos de Program.AR',
+    titulo: 'Segundo Ciclo',
+    descripcion: 'Desafíos del cuaderno para docentes "Actividades para aprender a Program.AR"  Para 2° ciclo de primaria en adelante.',
     modoLecturaSimple: false, // modo de lectura para niños pequeños.
   },
   {
     id: 2,
     nombre: 'primer-ciclo',
-    titulo: 'Curso primer ciclo primaria',
-    descripcion: 'Desafíos pensados para primer ciclo.',
+    titulo: 'Primer Ciclo',
+    descripcion: 'Desafíos del manual para docentes "Ciencias de la Computación para el aula, 1° ciclo de primaria"',
     modoLecturaSimple: true,
   },
 ];
