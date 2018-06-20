1d78f6a7905c1b9ce4dd66f78440f3c3c49c8e63
Alfredo Sanzo <alfredo.sanzo@gmail.com>
2018-06-20 09:54:21 -0300

    cambiando los grupos de ID de libro

--- mirage/fixtures/grupos.js
+++ mirage/fixtures/grupos.js
@@ -1,118 +1,118 @@
 export default [
   {
     id: 1,
-    libroId: 1,
+    libroId: 2,
     titulo: 'Autómatas, comandos, procedimientos y repetición',
   },
   {
     id: 2,
-    libroId: 1,
+    libroId: 2,
     titulo: 'Alternativa condicional',
   },
   {
     id: 3,
-    libroId: 1,
+    libroId: 2,
     titulo: 'Repetición condicional',
   },
   {
     id: 4,
-    libroId: 1,
+    libroId: 2,
     titulo: 'Sensores Numéricos',
   },
   {
     id: 5,
-    libroId: 1,
+    libroId: 2,
     titulo: 'Parametrización de soluciones',
   },
 
   {
     id: 'manual2cPrimaria2',
-    libroId: 2,
+    libroId: 1,
     titulo: 'Capítulo 2: Programando en la computadora'
   },
   {
     id: 'manual2cPrimaria2.1.2',
-    libroId: 2,
+    libroId: 1,
     titulo: 'A trabajar con la computadora'
   },
   {
     id: 'manual2cPrimaria2.1.3',
-    libroId: 2,
+    libroId: 1,
     titulo: 'Coty empieza a dibujar'
   },
   {
     id: 'manual2cPrimaria2.1.4',
-    libroId: 2,
+    libroId: 1,
     titulo: 'Lita prepara una rica ensalada'
   },
   {
     id: 'manual2cPrimaria2.2.2',
-    libroId: 2,
+    libroId: 1,
     titulo: 'La ensalada secreta'
   },
   {
     id: 'manual2cPrimaria2.2.3',
-    libroId: 2,
+    libroId: 1,
     titulo: '¡Problemas para comer!'
   },
   {
     id: 'manual2cPrimaria2.I',
-    libroId: 2,
+    libroId: 1,
     titulo: 'Las palabras de Toto'
   },
   {
     id: 'manual2cPrimaria3',
-    libroId: 2,
+    libroId: 1,
     titulo: 'Capítulo 3: Repetición'
   },
   {
     id: 'manual2cPrimaria3.1.3',
-    libroId: 2,
+    libroId: 1,
     titulo: 'Más churrascos para Duba'
   },
   {
     id: 'manual2cPrimaria3.1.4',
-    libroId: 2,
+    libroId: 1,
     titulo: 'Las líneas de Coty'
   },
   {
     id: 'manual2cPrimaria3.2.3',
-    libroId: 2,
+    libroId: 1,
     titulo: 'Corregimos programas'
   },
   {
     id: 'manual2cPrimaria3.I',
-    libroId: 2,
+    libroId: 1,
     titulo: 'Lita y Toto se preparan para el picnic'
   },
   {
     id: 'manual2cPrimaria4',
-    libroId: 2,
+    libroId: 1,
     titulo: 'Capítulo 4: Alternativa condicional'
   },
   {
     id: 'manual2cPrimaria4.1.3',
-    libroId: 2,
+    libroId: 1,
     titulo: 'Duba todavía tiene hambre'
   },
   {
     id: 'manual2cPrimaria4.1.4',
-    libroId: 2,
+    libroId: 1,
     titulo: '¿Qué encontrará Lita?'
   },
   {
     id: 'manual2cPrimaria4.2.1',
-    libroId: 2,
+    libroId: 1,
     titulo: 'Duba, más hambrienta que nunca'
   },
   {
     id: 'manual2cPrimaria4.I',
-    libroId: 2,
+    libroId: 1,
     titulo: 'Agente Secreto Topotopo'
   },
   {
 		id: 'manual2cPrimariaOtros',
-		libroId: 2,
+		libroId: 1,
 		titulo: 'Otros',
 	}
 ];
