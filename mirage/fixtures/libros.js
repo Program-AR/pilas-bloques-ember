706a0f0e15b6b289a5cd53394cb627367d404e85
Alfredo Sanzo <alfredo.sanzo@gmail.com>
2018-06-19 23:36:42 -0300

    cambios a los id de los libros y al header

--- mirage/fixtures/libros.js
+++ mirage/fixtures/libros.js
@@ -1,16 +1,16 @@
 export default [
   {
     id: 1,
-    nombre: 'programar',
-    titulo: 'Segundo Ciclo',
-    descripcion: 'Desafíos del cuaderno para docentes "Actividades para aprender a Program.AR"  Para 2° ciclo de primaria en adelante.',
-    modoLecturaSimple: false, // modo de lectura para niños pequeños.
+    nombre: 'primer-ciclo',
+    titulo: 'Desafíos del Primer Ciclo',
+    descripcion: 'Desafíos del manual para docentes "Ciencias de la Computación para el aula, 1° ciclo de primaria"',
+    modoLecturaSimple: true,
   },
   {
     id: 2,
-    nombre: 'primer-ciclo',
-    titulo: 'Primer Ciclo',
-    descripcion: 'Desafíos del manual para docentes "Ciencias de la Computación para el aula, 1° ciclo de primaria"',
-    modoLecturaSimple: true,
+    nombre: 'programar',
+    titulo: 'Desafíos del Segundo Ciclo',
+    descripcion: 'Desafíos del cuaderno para docentes "Actividades para aprender a Program.AR"  Para 2° ciclo de primaria en adelante.',
+    modoLecturaSimple: false, // modo de lectura para niños pequeños.
   },
 ];
