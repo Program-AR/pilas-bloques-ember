cd0ff8dfcc6bf4b39fa85f5e6d9688fd5afbf8dd
Franco Frizzo <francofrizzo@gmail.com>
2018-06-28 17:31:45 -0300

    Arreglos en algoritmo de exportación de SVG

--- vendor/utilidades_de_depuracion.js
+++ vendor/utilidades_de_depuracion.js
@@ -31,22 +31,39 @@ function utils_obtener_svg() {
     a.click();
   }
 
-  workspace = Blockly.getMainWorkspace();
-  aleph = workspace.svgBlockCanvas_.cloneNode(true);
+  var workspace = Blockly.getMainWorkspace();
+  var aleph = workspace.svgBlockCanvas_.cloneNode(true);
   aleph.removeAttribute("width");
   aleph.removeAttribute("height");
+  aleph.removeAttribute("transform");
+  
+  var cssContent = Blockly.Css.CONTENT.join('').replace(/font-family: sans-serif/g, "font-weight:bold;font-family: Arial");
+  var css = '<defs><style type="text/css" xmlns="http://www.w3.org/1999/xhtml"><![CDATA[' + cssContent + ']]></style></defs>';
+  
   if (aleph.children[0] !== undefined) {
-    aleph.removeAttribute("transform");
     aleph.children[0].removeAttribute("transform");
     aleph.children[0].children[0].removeAttribute("transform");
-    aleph.querySelectorAll(".blocklyPathLight").forEach(function(elem) {elem.setAttribute("fill", "none")});
-    var linkElm = document.createElementNS("http://www.w3.org/1999/xhtml", "style");
-    linkElm.textContent = Blockly.Css.CONTENT.join('') + '\n\n';
-    aleph.insertBefore(linkElm, aleph.firstChild);
-    var bbox = document.getElementsByClassName("blocklyBlockCanvas")[0].getBBox();
-    var xml = new XMLSerializer().serializeToString(aleph);
-    xml = '<svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="'+bbox.width+'" height="'+bbox.height+'" viewBox="0 0 '+bbox.width+' '+bbox.height+'"><rect width="100%" height="100%" fill="white"></rect>'+xml+'</svg>';
-
-    descargar(xml, 'bloques.svg', 'image/svg+xml');
   }
+  
+  aleph.querySelectorAll(".blocklyPathLight").forEach(function(elem) {elem.setAttribute("fill", "none")});
+  if (document.getElementsByClassName("aplicar-modo-lectura-simple").length > 0) {
+    aleph.querySelectorAll(".blocklyText").forEach(function (elem) { elem.textContent = elem.textContent.toUpperCase() });
+  }
+
+  // aleph.querySelectorAll("image").forEach(function (img) {
+  //   console.log(img);
+  //   var obj = document.createElement("object");
+  //   obj.setAttribute("data", img.href.baseVal);
+  //   document.body.appendChild(obj);
+  //   obj.onload = function() {
+  //     var xml = new XMLSerializer().serializeToString(obj.contentDocument)
+  //     img.setAttribute("href", "data:image/svg+xml;base64," + btoa(xml));
+  //     obj.remove();
+  //   };
+  // });
+
+  var bbox = document.getElementsByClassName("blocklyBlockCanvas")[0].getBBox();
+  var xmlContent = new XMLSerializer().serializeToString(aleph);
+  xml = '<svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="'+bbox.width+'" height="'+bbox.height+'" viewBox="0 0 '+bbox.width+' '+bbox.height+'">'+css+'<rect width="100%" height="100%" fill="white"></rect>'+xmlContent+'</svg>';
+  descargar(xml, 'bloques.svg', 'image/svg+xml');
 }
