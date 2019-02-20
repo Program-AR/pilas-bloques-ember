30e4aaafb7eb41e4f09d0f5bbde66ea1629420e0
PalumboN <nahuel.palumbo@gmail.com>
2019-02-20 14:01:07 -0300

    Fix pilas-blockly-test

--- tests/integration/components/pilas-blockly-test.js
+++ tests/integration/components/pilas-blockly-test.js
@@ -1,28 +1,43 @@
-import { moduleForComponent, skip } from 'ember-qunit';
+import { moduleForComponent, test, skip } from 'ember-qunit';
 import hbs from 'htmlbars-inline-precompile';
 
 moduleForComponent('pilas-blockly', 'Integration | Component | pilas blockly', {
   integration: true
 });
 
-skip('it renders', function(assert) {
-
-  this.set('pilas', {
-    on: function() {
-    }
-  });
-
-  this.set('bloques', ['controls_if']);
-
-  /* Cuando el componente está listo para ser usado. */
-  this.render(hbs`{{pilas-blockly cargando=false pilas=pilas bloques=bloques}}`);
-  assert.ok(this.$().text().indexOf("Ejecutar") > -1, "Tiene el botón ejecutar visible");
-  assert.ok(this.$().text().indexOf("Compartir") === -1, 'Ya no existe un botón para compartir por twitter por omisión');
-
-  assert.ok(this.$().text().indexOf("Abrir") > -1, 'Existe un botón para cargar una solución');
-  assert.ok(this.$().text().indexOf("Guardar") > -1, 'Existe un botón para guardar una solución');
-
-  /* Cuando el componente está cargando */
+test('Cuando el componente está cargando', function(assert) {
   this.render(hbs`{{pilas-blockly cargando=true pilas=pilas bloques=bloques}}`);
-  assert.equal(this.$().text().indexOf("Ejecutar") > -1, true, 'Cuando carga, solamente está el botón Ejecutar (deshabilitado)');
+
+  assert.equal(this.$("button").length, 0, 'No muestra ningún botón');
+  assert.ok(existeTexto("cargando"), "Solo muestra el texto 'cargando'");
 });
+
+test('Cuando el componente está listo para ser usado', function(assert) {
+  this.render(hbs`{{pilas-blockly cargando=false pilas=pilas bloques=bloques}}`);
+  
+  assert.ok(existeBoton("Ejecutar"), "Tiene el botón ejecutar visible");
+  assert.notOk(existeBoton("Compartir"), 'Ya no existe un botón para compartir por twitter por omisión');
+
+  assert.ok(existeBoton("Abrir"), 'Existe un botón para cargar una solución');
+  assert.ok(existeBoton("Guardar"), 'Existe un botón para guardar una solución');
+});
+
+skip('Cuando ejecuta un ejercicio', function(assert) {
+  this.render(hbs`{{pilas-blockly cargando=false pilas=pilas bloques=bloques}}`);
+  this.send("ejecutar");
+
+  assert.ok(existeBoton("Reiniciar"), "Tiene el botón reiniciar visible");
+});
+
+function existeElementoConTexto(elemento, texto) {
+  return this.$(elemento).text().includes(texto);
+}
+
+function existeBoton(texto) {
+  return existeElementoConTexto("button", texto);
+}
+
+function existeTexto(texto) {
+  return existeElementoConTexto("p", texto);
+}
+
