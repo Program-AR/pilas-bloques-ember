c649f088753d4164a11b07a89d94806502db703f
Lucas Alan Silvestri <silvestrilucasalan@gmail.com>
2019-02-26 23:47:46 -0300

    Charco provoca un comportamiento en quien lo toque

--- ejerciciosPilas/src/actores/libroPrimaria/Charco.ts
+++ ejerciciosPilas/src/actores/libroPrimaria/Charco.ts
@@ -1,7 +1,20 @@
-/// <reference path="../ActorAnimado.ts"/>
-class Charco extends ActorAnimado {
+/// <reference path="../ActorColisionable.ts"/>
+/// <reference path="../../comportamientos/Hundir.ts"/>
+
+class Charco extends ActorColisionable {
+
     constructor() {
         super(0, 0, { grilla: 'actor.charco.png' });
         this.definirAnimacion("parado", [0], 6, true);
     }
+
+    provocarEnAutomata() {
+        return [Hundir];
+    };
+
+
+    provocarEnMi() {
+        return [];
+    };
+
 }
