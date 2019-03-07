64fe6186e46ba9f53f09cafbe84c5bb1a2ca983a
PalumboN <nahuel.palumbo@gmail.com>
2019-03-07 17:13:32 -0300

    Testing component pilas-blockly

--- tests/unit/components/pilas-blockly-test.js
+++ tests/unit/components/pilas-blockly-test.js
@@ -1,5 +1,6 @@
+import Ember from 'ember';
 import { moduleFor, test } from 'ember-qunit';
-import { pilasMock, interpreterFactoryMock } from '../../helpers/mocks';
+import { pilasMock, interpreterFactoryMock, actividadMock } from '../../helpers/mocks';
 
 moduleFor('component:pilas-blockly', 'Unit | Components | pilas-blockly', {
   // Specify the other units that are required for this test.
@@ -8,28 +9,69 @@ moduleFor('component:pilas-blockly', 'Unit | Components | pilas-blockly', {
     this.register('service:interpreterFactory', interpreterFactoryMock);
     let ctrl = this.subject();
     ctrl.pilas = pilasMock;
+    ctrl.set('modelActividad', actividadMock)
   }
 });
 
-test('Al ejecutar settea flags y ejecuta el intérprete', function(assert) {
+test('Al ejecutar se encuentra ejecutando y ejecuta el intérprete', function(assert) {
   let ctrl = this.subject();
-  
   ctrl.send('ejecutar');
 
   assert.ok(ctrl.get('ejecutando'));
   assert.notOk(ctrl.get('pausadoEnBreakpoint'));
-  //TODO: testing sobre la ejecución (async)
+  //TODO: testing sobre interprete.run() (spy)
 });
 
+test('Ejecutar paso a paso bloquea la ejecución', function(assert) {
+  let ctrl = this.subject();
+  ctrl.send('ejecutar', true);
+  //TODO: Test 'step'
+  
+  Ember.run.later(() => {
+    //TODO: testing sobre interprete.run() (spy)
+    assert.ok(ctrl.get('pausadoEnBreakpoint'));
+  });
+});
+
+test('Luego de ejecutar termina de ejecutar', function(assert) {
+  let ctrl = this.subject();
+  ctrl.send('ejecutar');
+
+  Ember.run.later(() => {
+    assert.notOk(ctrl.get('ejecutando'));
+    assert.ok(ctrl.get('terminoDeEjecutar'));
+    assert.notOk(ctrl.get('highlightedBlock'));
+  });
+});
+
+test('Al resolver el problema muestra el fin del desafío', function(assert) {
+  let ctrl = this.subject();
+  ctrl.set('debeMostrarFinDeDesafio', true);
+  ctrl.send('ejecutar');
+
+  Ember.run.later(() => {
+    assert.ok(ctrl.get('mostrarDialogoFinDesafio'));
+  });
+});
 
 test('Al reiniciar settea flags y reinicia la escena de pilas', function(assert) {
   let ctrl = this.subject();
-  
   ctrl.send('reiniciar');
 
   assert.notOk(ctrl.get('highlightedBlock'));
   assert.notOk(ctrl.get('ejecutando'));
   assert.notOk(ctrl.get('terminoDeEjecutar'));
   assert.notOk(ctrl.get('errorDeActividad'));
-  //TODO: testing sobre mock pilas.reiniciarEscenaCompleta()
+  //TODO: testing sobre mock pilas.reiniciarEscenaCompleta() (spy)
+});
+
+
+test('Al guardar solución hace una descarga con la solución', function(assert) {
+  let ctrl = this.subject();
+  ctrl.descargar = (text, name, type) => { 
+    assert.equal(text, JSON.stringify({version:1, actividad:"Actividad_Mock", solucion:""}));
+    assert.equal(name, 'Actividad_Mock.spbq');
+    assert.equal(type, 'application/octet-stream');
+  };
+  ctrl.send('guardarSolucion');
 });
