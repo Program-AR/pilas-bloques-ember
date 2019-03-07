12906261f814c170f8287ee26e602252cff0d6d3
PalumboN <nahuel.palumbo@gmail.com>
2019-03-07 14:35:27 -0300

    Tests básicos del componente pilas-blockly

--- tests/unit/components/pilas-blockly-test.js
+++ tests/unit/components/pilas-blockly-test.js
@@ -6,14 +6,30 @@ moduleFor('component:pilas-blockly', 'Unit | Components | pilas-blockly', {
   // needs: ['controller:foo']
   setup() {
     this.register('service:interpreterFactory', interpreterFactoryMock);
+    let ctrl = this.subject();
+    ctrl.pilas = pilasMock;
   }
 });
 
-test('Al ejecutar se está ejecutando', function(assert) {
+test('Al ejecutar settea flags y ejecuta el intérprete', function(assert) {
   let ctrl = this.subject();
-  ctrl.pilas = pilasMock;
   
-  ctrl.send("ejecutar");
-  
+  ctrl.send('ejecutar');
+
   assert.ok(ctrl.get('ejecutando'));
+  assert.notOk(ctrl.get('pausadoEnBreakpoint'));
+  //TODO: testing sobre la ejecución (async)
+});
+
+
+test('Al reiniciar settea flags y reinicia la escena de pilas', function(assert) {
+  let ctrl = this.subject();
+  
+  ctrl.send('reiniciar');
+
+  assert.notOk(ctrl.get('highlightedBlock'));
+  assert.notOk(ctrl.get('ejecutando'));
+  assert.notOk(ctrl.get('terminoDeEjecutar'));
+  assert.notOk(ctrl.get('errorDeActividad'));
+  //TODO: testing sobre mock pilas.reiniciarEscenaCompleta()
 });
