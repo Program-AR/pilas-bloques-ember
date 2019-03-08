26d90514f821eafb159295ace9f9272a68bb2032
PalumboN <nahuel.palumbo@gmail.com>
2019-03-08 17:26:48 -0300

    Testing: toto escritor debe terminar en la última casilla

--- tests/integration/desafios/libroPrimaria/TotoEscritor-test.js
+++ tests/integration/desafios/libroPrimaria/TotoEscritor-test.js
@@ -82,6 +82,32 @@ actividadTest("5.I1a", {
 actividadTest("5.I1b", {
   descripcionAdicional: '5.I1b: Se puede resolver',
   solucion: `<xml xmlns="http://www.w3.org/1999/xhtml">
+    <block type="al_empezar_a_ejecutar" deletable="false" movable="false" editable="false" x="382" y="15">
+      <statement name="program">
+        <block type="repetir">
+          <value name="count">
+            <block type="math_number">
+              <field name="NUM">7</field>
+            </block>
+          </value>
+          <statement name="block">
+            <block type="MoverACasillaDerecha">
+              <next>
+                <block type="EscribirTextoDadoEnOtraCuadricula">
+                  <field name="texto">x</field>
+                </block>
+              </next>
+            </block>
+          </statement>
+        </block>
+      </statement>
+    </block>
+  </xml>`
+});
+
+actividadTest("5.I1b", {
+  descripcionAdicional: '5.I1b: Solamente escribir sin moverse NO resuelve el desafio',
+  solucion: `<xml xmlns="http://www.w3.org/1999/xhtml">
     <variables></variables>
     <block type="al_empezar_a_ejecutar" deletable="false" movable="false" editable="false" x="382" y="15">
       <statement name="program">
@@ -99,7 +125,8 @@ actividadTest("5.I1b", {
         </block>
       </statement>
     </block>
-  </xml>`
+  </xml>`,
+  resuelveDesafio: false
 });
 
 actividadTest("5.I1c", {
