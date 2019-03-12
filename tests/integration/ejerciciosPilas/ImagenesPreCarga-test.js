498eed7f4ee52aceccbcc94d4036c0d89d401fe2
PalumboN <nahuel.palumbo@gmail.com>
2019-03-12 14:25:15 -0300

    Más actores en las escenas de los tests

--- tests/integration/ejerciciosPilas/ImagenesPreCarga-test.js
+++ tests/integration/ejerciciosPilas/ImagenesPreCarga-test.js
@@ -6,7 +6,7 @@ moduloEjerciciosPilas('Imagenes Pre-carga');
 
 
 test('EscenaDuba', function(assert) {
-  let escena = `new EscenaDuba("[O]")`;
+  let escena = `new EscenaDuba("[O,-,A,P,*]")`;
   return createPilasTest(this, escena, (pilas, resolve, pilasService) => {
     let imagenes = pilasService.imagenesParaPrecargar(escena);
     let imagenesEsperadas = ["fondo.duba.png", "actor.duba.png", "actor.churrasco.png", "flechaEscenarioAleatorio.png", "casillas.duba.png", "obstaculo.duba1.png", "obstaculo.duba2.png", "obstaculo.duba3.png", "obstaculo.duba4.png"];
@@ -16,7 +16,7 @@ test('EscenaDuba', function(assert) {
 });
 
 test('EscenaLita', function(assert) {
-  let escena = `new EscenaLita("[O]")`;
+  let escena = `new EscenaLita("[O,-,A,T,L,E]")`;
   return createPilasTest(this, escena, (pilas, resolve, pilasService) => {
     let imagenes = pilasService.imagenesParaPrecargar(escena);
     let imagenesEsperadas = ["fondo.lita.png", "actor.lita.png", "actor.ensaladera.png", "actor.tomate.png", "actor.lechuga.png", "flechaEscenarioAleatorio.png", "casillas.lita.png", "obstaculo.lita1.png", "obstaculo.lita2.png", "obstaculo.lita3.png", "obstaculo.lita4.png"];
