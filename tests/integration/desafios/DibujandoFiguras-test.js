4eea428b8d31c247fe0c1339e8904adb6d93c4cb
PalumboN <nahuel.palumbo@gmail.com>
2019-04-01 16:50:07 -0300

    Se puede rotar 0 grados

--- tests/integration/desafios/DibujandoFiguras-test.js
+++ tests/integration/desafios/DibujandoFiguras-test.js
@@ -253,7 +253,6 @@ actividadTest('DibujoLibre', {
   resuelveDesafio: false,
 });
 
-
 actividadTest('DibujoLibre', {
   descripcionAdicional: 'No se puede dividir por cero.',
   solucion: `<xml xmlns="http://www.w3.org/1999/xhtml">
@@ -281,5 +280,24 @@ actividadTest('DibujoLibre', {
     </block>
   </xml>`,
   errorEsperado: "No se puede dividir por 0",
+  resuelveDesafio: false,
+});
+
+actividadTest('DibujoLibre', {
+  descripcionAdicional: 'Se puede girar 0 grados.',
+  solucion: `<xml xmlns="http://www.w3.org/1999/xhtml">
+    <variables></variables>
+    <block type="al_empezar_a_ejecutar" deletable="false" movable="false" editable="false" x="15" y="15">
+      <statement name="program">
+        <block type="GirarGrados">
+          <value name="grados">
+            <block type="math_number">
+              <field name="NUM">0</field>
+            </block>
+          </value>
+        </block>
+      </statement>
+    </block>
+  </xml>`,
   resuelveDesafio: false,
 });
