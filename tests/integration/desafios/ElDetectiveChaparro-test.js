4e87762607bc7de8e4f119b05272a765dc06ad61
Lucas Alan Silvestri <silvestrilucasalan@gmail.com>
2019-04-09 11:19:16 -0300

    Corregida identacion e referencia a bloques del test detective chaparro

--- tests/integration/desafios/ElDetectiveChaparro-test.js
+++ tests/integration/desafios/ElDetectiveChaparro-test.js
@@ -5,7 +5,33 @@ const nombre = "ElDetectiveChaparro";
 moduloActividad(nombre);
 
 actividadTest(nombre, {
-	solucion: '<xml xmlns="http://www.w3.org/1999/xhtml"><block type="al_empezar_a_ejecutar" id="2" deletable="false" movable="false" editable="false" x="0" y="0"><statement name="program"><block type="Primersospechoso" id="3"><next><block type="Sacardisfraz" id="8"><next><block type="hasta" id="4" inline="true"><value name="condition"><block type="Descubralculpable" id="5"></block></value><statement name="block"><block type="Siguientesospechoso" id="7"><next><block type="Sacardisfraz" id="6"></block></next></block></statement></block></next></block></next></block></statement></block></xml>',
+  solucion: `<?xml version="1.0" encoding="UTF-8"?>
+  <xml xmlns="http://www.w3.org/1999/xhtml">
+     <block type="al_empezar_a_ejecutar" id="2" deletable="false" movable="false" editable="false" x="0" y="0">
+        <statement name="program">
+           <block type="PrimerSospechoso" id="3">
+              <next>
+                 <block type="SacarDisfraz" id="8">
+                    <next>
+                       <block type="Hasta" id="4" inline="true">
+                          <value name="condition">
+                             <block type="EsCulpable" id="5" />
+                          </value>
+                          <statement name="block">
+                             <block type="SiguienteSospechoso" id="7">
+                                <next>
+                                   <block type="SacarDisfraz" id="6" />
+                                </next>
+                             </block>
+                          </statement>
+                       </block>
+                    </next>
+                 </block>
+              </next>
+           </block>
+        </statement>
+     </block>
+  </xml>`,
 });
 
 actividadTest(nombre, {
