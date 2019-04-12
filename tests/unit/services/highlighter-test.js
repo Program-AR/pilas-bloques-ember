d6e960a0628cec36880bfe7ef43c7950b4e5be03
PalumboN <nahuel.palumbo@gmail.com>
2019-04-12 17:30:33 -0300

    Testing repetition highlight

--- tests/unit/services/highlighter-test.js
+++ tests/unit/services/highlighter-test.js
@@ -1,10 +1,16 @@
-import { moduleFor, test } from 'ember-qunit';
+import { moduleFor, test, only } from 'ember-qunit';
 
 
 moduleFor('service:highlighter', 'Unit | Service | highlighter', { 
     needs: ['service:blocksGallery', 'service:blockly'],
     setup() {
+        var highlighter = this.subject()
+        let workspace = new Blockly.Workspace()
+        workspace.highlightBlock = function() { } //Mocking (?)
+        highlighter.workspace = workspace
+        
         this.container.lookup('service:blocksGallery').start()
+        highlighter.clear()
     }
 });
 
@@ -24,6 +30,20 @@ let linealProgram = `
 </block>
 `
 
+test('Should not highlight program block', function(assert) {
+    var highlighter = this.subject()
+    loadProgramAndSendSteps(highlighter, 1, linealProgram)
+    assertHighlight(assert, highlighter, [])
+});
+
+
+test('On lineal program should highlight only current block', function(assert) {
+    var highlighter = this.subject()
+    loadProgramAndSendSteps(highlighter, 3, linealProgram)
+    assertHighlight(assert, highlighter, ['MoverACasillaIzquierda'])
+});
+
+
 let repetitionProgram = `
 <block type="al_empezar_a_ejecutar" deletable="false" movable="false" editable="false" x="269" y="15">
     <statement name="program">
@@ -32,7 +52,7 @@ let repetitionProgram = `
         <block type="repetir">
             <value name="count">
             <block type="math_number">
-                <field name="NUM">2</field>
+                <field name="NUM">1</field>
             </block>
             </value>
             <statement name="block">
@@ -52,35 +72,52 @@ let repetitionProgram = `
 </block>
 `
 
-test('Should not highlight the program block', function(assert) {
+test('Should highlight repetition block', function(assert) {
     var highlighter = this.subject()
-    loadProgramAndSendSteps(highlighter, 1, linealProgram)
-    assert.equal(0, highlighter.blocks.length)
+    loadProgramAndSendSteps(highlighter, 3, repetitionProgram)
+    assertHighlight(assert, highlighter, ['repetir'])
 });
 
-
-test('On lineal program should highlight only the current block', function(assert) {
+test('When enter on repetition block should only highlight current block', function(assert) {
     var highlighter = this.subject()
-    loadProgramAndSendSteps(highlighter, 3, linealProgram)
-    assert.equal(1, highlighter.blocks.length)
-    assert.deepEqual(['MoverACasillaIzquierda'], highlighter.blocks.map((it) => it.type))
+    loadProgramAndSendSteps(highlighter, 5, repetitionProgram)
+    assertHighlight(assert, highlighter, ['MoverACasillaDerecha'])
+});
+
+test('When go out repetition block should only highlight next block', function(assert) {
+    var highlighter = this.subject()
+    loadProgramAndSendSteps(highlighter, 6, repetitionProgram)
+    assertHighlight(assert, highlighter, ['MoverACasillaAbajo'])
 });
 
 
 function loadProgramAndSendSteps(highlighter, steps, blocksAsText) {
-    let workspace = new Blockly.Workspace()
-    workspace.highlightBlock = function() { } //Mocking (?)
-    
     let dom = Blockly.Xml.textToDom(blocksAsText)
-    let mainBlock = Blockly.Xml.domToBlock(dom, workspace)
+    let mainBlock = Blockly.Xml.domToBlock(dom, highlighter.workspace)
 
-    highlighter.workspace = workspace
+    let ignoredBlockTypes = ["math_number"]
 
-    let currentBlock = mainBlock
-    while (steps != 0) {
-        highlighter.step(currentBlock.id)
-        currentBlock = currentBlock.getNextBlock() || currentBlock.getChildren()[0]
+    //Esta ejecución solamente RECORRE los bloques. ¡No tiene en cuenta la lógica!
+    function doStep(block) {
+        if (steps == 0 || ignoredBlockTypes.includes(block.type)) return
+        highlighter.step(block.id)
         steps--
+        block.getChildren().forEach(doStep)
     }
+    doStep(mainBlock)
+}
 
+//TODO: Config assert?
+function assertHighlight(assert, highlighter, expectedTypes) {
+    assertLength(assert, highlighter.blocks, expectedTypes.length)
+    assertTypes(assert, highlighter.blocks, expectedTypes)
+}
+
+function assertTypes(assert, blocks, expectedTypes) {
+    assert.deepEqual(blocks.map((it) => it.type), expectedTypes)
+}
+
+//TODO: Mover a un lugar más general
+function assertLength(assert, list, count) {
+    assert.deepEqual(list.length, count)
 }
