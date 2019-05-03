312211e3188242e638bd312eb3d911cce9704236
PalumboN <nahuel.palumbo@gmail.com>
2019-05-03 16:01:01 -0300

    Se acomoda el código para poder hacer unit testing

--- app/components/pilas-solution-file.js
+++ app/components/pilas-solution-file.js
@@ -1,5 +1,5 @@
-import Ember from 'ember';
-let VERSION_DEL_FORMATO_DE_ARCHIVO = 1;
+import Ember from 'ember'
+let VERSION_DEL_FORMATO_DE_ARCHIVO = 1
 
 export default Ember.Component.extend({
   tagName: 'span',
@@ -8,83 +8,88 @@ export default Ember.Component.extend({
   workspace: null,
   xml: null,
 
-  didInsertElement() {
-    this.fileInput().change((event) => {
-      let archivo = event.target.files[0];
+  leer(archivo) {
+    var reader = new FileReader()
 
-      if (archivo) {
-        var reader = new FileReader();
+    reader.onload = (e) => {
+      let contenido = e.target.result
+      this.cargarSolucion(contenido)
+    }
 
-        reader.onload = (e) => {
-      	  let contenido = e.target.result;
-          
-          this.cargarSolucion(contenido);
-        };
-
-        reader.readAsText(archivo);
-      }
-
-      // Fuerza a que se pueda cargar dos o más veces el mismo archivo.
-      this.limpiarInput();
-      return false;
-    });
-  },
-
-  limpiarInput() {
-    this.fileInput().value = null;
+    reader.readAsText(archivo)
   },
 
   descargar(text, name, type) {
-    var a = document.getElementById("placeholder");
-    var file = new Blob([text], {type: type});
-    a.href = URL.createObjectURL(file);
-    a.download = name;
+    var a = document.getElementById("placeholder")
+    var file = new Blob([text], {type: type})
+    a.href = URL.createObjectURL(file)
+    a.download = name
     a.type = type
     a.click()
   },
 
   cargarSolucion(contenido) {
-    // let regex_file = /\.spbq$/;
-    // let regex_version = /^\d+$/;
-    let data = null;
-    let solucion = null;
+    // let regex_file = /\.spbq$/
+    // let regex_version = /^\d+$/
+    let data = null
+    let solucion = null
 
     try {
-      data = JSON.parse(contenido);
-      solucion = atob(data.solucion);
+      data = JSON.parse(contenido)
+      solucion = atob(data.solucion)
     } catch (e) {
-      console.error(e);
-      alert("Lo siento, este archivo no tiene una solución de Pilas Bloques.");
-      return;
+      console.error(e)
+      throw "Lo siento, este archivo no tiene una solución de Pilas Bloques."
     }
+
+    this.set('workspace', solucion)
 
     if (this.get("actividad.nombre") !== data.actividad) {
-      alert(`Cuidado, el archivo indica que es para otra actividad (${data.actividad}). Se cargará de todas formas, pero puede fallar.`);
+      throw `Cuidado, el archivo indica que es para otra actividad (${data.actividad}). Se cargará de todas formas, pero puede fallar.`
     }
+  },
 
-    this.set('workspace', solucion);
+  didInsertElement() {
+    this.fileInput().change((event) => {
+      let archivo = event.target.files[0]
+
+      if (archivo) {
+        try {
+          this.leer(archivo)
+        } catch (error) {
+          alert(error) //TODO: Fix!! Las excepciones no llegan hasta acá, habría que trabajar mejor el async
+        }
+      }
+
+      this.limpiarInput() // Fuerza a que se pueda cargar dos o más veces el mismo archivo
+      return false
+    })
   },
 
   fileInput() {
     return this.$("#cargarActividadInput")
   },
 
+  limpiarInput() {
+    this.fileInput().value = null
+  },
+
   actions: {
     abrirSolucion() {
-      this.fileInput().click();
+      this.fileInput().click()
     },
 
     guardarSolucion() {
-      let activityName = this.get("actividad.nombre");
-      let fileName = `${activityName}.spbq`;
+      let activityName = this.get("actividad.nombre")
+      let fileName = `${activityName}.spbq`
 
       let contenido = {
         version: VERSION_DEL_FORMATO_DE_ARCHIVO,
         actividad: activityName,
         solucion: btoa(this.get('xml'))
-      };
+      }
 
-      this.descargar(JSON.stringify(contenido), fileName, 'application/octet-stream');
+      this.descargar(JSON.stringify(contenido), fileName, 'application/octet-stream')
     },
   }
-});
+})
