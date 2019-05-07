90f4c52c4d7a4b5ae94eefc97b818341ebeb56c0
Lucas Alan Silvestri <silvestrilucasalan@gmail.com>
2019-05-07 12:15:13 -0300

    ahora, se realizan de forma correcta los checkeos typescript a la hora de compilar

--- ejerciciosPilas/Gruntfile.js
+++ ejerciciosPilas/Gruntfile.js
@@ -1,4 +1,4 @@
-module.exports = function(grunt) {
+module.exports = function (grunt) {
 
   require('load-grunt-tasks')(grunt);
 
@@ -13,44 +13,28 @@ module.exports = function(grunt) {
       },
       dist: {
         src: [
-            'dependencias/helpers.js',
-            'dependencias/listHelper.js',
-            'node_modules/nearley/lib/nearley.js',
-            'compilados/gramaticaAleatoria.js',
-            'compilados/ejerciciosPilas.js',
+          'dependencias/helpers.js',
+          'dependencias/listHelper.js',
+          'node_modules/nearley/lib/nearley.js',
+          'compilados/gramaticaAleatoria.js',
+          'compilados/ejerciciosPilas.js',
         ],
         dest: 'compilados/ejerciciosPilas.js',
       },
     },
 
-    typescript: {
-      base: {
-        src: ['src/**/*.ts'],
-        dest: 'compilados/ejerciciosPilas.js',
-        options: {
-          module: 'commonjs',
-          target: 'es5',
-          rootDir: 'src',
-          experimentalDecorators: true,
-          emitDecoratorMetadata: true,
-          sourceMap: false,
-          fullSourceMapPath: false,
-          declaration: false,
-          comments: true,
-          }
-        }
-      },
-    watch: {
-      scripts: {
-        files: ['src/**'],
-        tasks: ['typescript', 'concat'],
+    ts: {
+      default: {
+        tsconfig: 'tsconfig.json'
       }
     },
+
     open: {
-        dev: {
-            path: 'src/visorEjercicios.html'
-        }
+      dev: {
+        path: 'src/visorEjercicios.html'
+      }
     },
+
     run: {
       compilarGramaticaAleatoria: {
         cmd: 'npm',
@@ -62,14 +46,14 @@ module.exports = function(grunt) {
     }
   });
 
-  grunt.loadNpmTasks('grunt-typescript');
+  grunt.loadNpmTasks('grunt-ts');
   grunt.loadNpmTasks('grunt-contrib-watch');
   grunt.loadNpmTasks('grunt-contrib-concat');
   grunt.loadNpmTasks('grunt-open');
   grunt.loadNpmTasks('grunt-contrib-qunit');
   grunt.loadNpmTasks('grunt-run');
 
-  grunt.registerTask('default', ['typescript', 'run:compilarGramaticaAleatoria', 'concat']);
+  grunt.registerTask('default', ['ts', 'run:compilarGramaticaAleatoria', 'concat']);
 
   grunt.registerTask('test', 'qunit');
 };
