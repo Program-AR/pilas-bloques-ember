0940ea10d0ab7c95be1cecc719faa9d2f435e6e0
Alfredo Sanzo <alfredo.sanzo@gmail.com>
2019-06-17 15:32:13 -0300

    agrego tests de proximidad para la interaccion

--- tests/integration/ejerciciosPilas/Interactuar-test.js
+++ tests/integration/ejerciciosPilas/Interactuar-test.js
@@ -1,55 +1,114 @@
-import { test } from 'ember-qunit';
-import { moduloEjerciciosPilas } from '../../helpers/ejerciciosPilasTest';
+import { test, setupApplicationTest } from 'ember-qunit';
+import { moduloEjerciciosPilas, hacerLuegoConCallback } from '../../helpers/ejerciciosPilasTest';
 import createPilasTest from '../../helpers/createPilasTest';
 import sinon from 'sinon';
 
 const nombre = 'Interactuar';
 
+let argumentosParaApretar = {
+    etiqueta: "BotonAnimado",
+    nombreAnimacion: "apretar",
+    animacionInteractuadoAlFinal: "prendida"
+}
+
+let argumentosParaPelear = {
+    etiqueta: "CaballeroAnimado",
+    animacionInteractuadoMientras: "defender",
+    nombreAnimacion: "atacar",
+}
+
+function setup(pilasService){
+    let Interactuar = pilasService.evaluar("Interactuar");
+    let alien = new (pilasService.evaluar("AlienAnimado"))(0,0);
+    pilasService.evaluar("pilas").escena_actual().automata = alien;
+    let boton = new (pilasService.evaluar("BotonAnimado"))(0,0);
+    let ActividadError = pilasService.evaluar("ActividadError");
+    let cuadricula = new (pilasService.evaluar("Cuadricula"))(0, 0, 1, 2,{},{grilla: 'invisible.png'});
+    return {Interactuar, alien, boton, ActividadError, cuadricula};
+}
+
 moduloEjerciciosPilas(nombre);
 
-test('Interactuar sanitiza correctamente argumentos correctos', function (assert) {
+test('Sanitización: Interactuar sanitiza correctamente argumentos correctos', function (assert) {
     return createPilasTest(this, 'EscenaTests', (pilas, resolve, pilasService) => {
         let Interactuar = pilasService.evaluar("Interactuar");
-        let argumentos = {
-            etiqueta: "etiqueta",
-            animacionInteractuadoMientras: "animacionInteractuadoMientras",
-            nombreAnimacion: "interactuar"
-        };
-        let instance = new Interactuar(argumentos);
+        let instance = new Interactuar(argumentosParaApretar);
+        
         instance.sanitizarArgumentos();
-
-        assert.ok(instance);
+        assert.ok(true); // la linea anterior no debe estallar.
         resolve();
     });
 });
 
-test('no se puede crear una instancia de Interactuar, falta la etiqueta', function (assert) {
+test('Sanitización: No se puede crear una instancia de Interactuar, falta la etiqueta', function (assert) {
     return createPilasTest(this, 'EscenaTests', (pilas, resolve, pilasService) => {
         let ArgumentError = pilasService.evaluar("ArgumentError");
         let Interactuar = pilasService.evaluar("Interactuar");
-        let argumentos = {};
 
-        assert.throws(() => {
-            new Interactuar(argumentos).sanitizarArgumentos();
-        }, new ArgumentError("Debe proveerse una etiqueta para verificar interacción"), 'Expect an error with this message');
+        assert.throws(
+            () => new Interactuar({}).sanitizarArgumentos(), 
+            new ArgumentError("Debe proveerse una etiqueta para verificar interacción"), 
+        );
         resolve();
     });
 });
 
-test('El mensaje interactuar llama al método alInteractuar', function (assert) {
+test('Sin cuadrícula: No se puede interactuar con un objeto si estoy lejos', function(assert){
     return createPilasTest(this, 'EscenaTests', (pilas, resolve, pilasService) => {
-        let Interactuar = pilasService.evaluar("Interactuar");
-        let argumentos = {
-            etiqueta: "etiqueta",
-            animacionInteractuadoMientras: "animacionInteractuadoMientras",
-            nombreAnimacion: "interactuar"
-        };
-        let instance = new Interactuar(argumentos);
-        let spy = sinon.spy(instance, 'alInteractuar')
-        instance.interactuar();
-        assert.ok(spy.calledOnce);
-        spy.restore();
+        let {Interactuar, alien, boton, ActividadError} = setup(pilasService);
+        boton.x += 300;
+
+        // Idealmente estas 3 líneas deberían ser un hacerLuegoConCallback, pero
+        // no hay hooks para esperar un error, así que por eso se usa el iniciar y configuracionInicial
+        elInteractuar = new Interactuar(argumentosParaApretar);
+        elInteractuar.iniciar(alien);
+        assert.throws(
+            () => elInteractuar.configuracionInicial(), 
+            new ActividadError("¡Acá no hay boton!")
+        );
         resolve();
+    });
+});
+
+test('Con cuadrícula: No se puede interactuar con un objeto si estoy en otra casilla', function(assert){
+    return createPilasTest(this, 'EscenaTests', (pilas, resolve, pilasService) => {
+        let {Interactuar, alien, boton, ActividadError,cuadricula} = setup(pilasService);
+        cuadricula.agregarActor(alien,0,0);
+        cuadricula.agregarActor(boton,0,1);
+
+        // Idealmente estas 3 líneas deberían ser un hacerLuegoConCallback, pero
+        // no hay hooks para esperar un error, así que por eso se usa el iniciar y configuracionInicial
+        elInteractuar = new Interactuar(argumentosParaApretar);
+        elInteractuar.iniciar(alien);
+        assert.throws(
+            () => elInteractuar.configuracionInicial(), 
+            new ActividadError("¡Acá no hay boton!")
+        );
+        resolve();
+    });
+});
+
+test('Sin cuadrícula: Interactúa cuando está el objeto', function(assert){
+    return createPilasTest(this, 'EscenaTests', (pilas, resolve, pilasService) => {
+        let {Interactuar, alien, boton} = setup(pilasService);
+
+        hacerLuegoConCallback(alien,Interactuar,argumentosParaApretar,() => {
+            assert.equal(argumentosParaApretar.animacionInteractuadoAlFinal, boton.nombreAnimacionActual());
+            resolve();
+        });
+    });
+});
+
+test('Con cuadrícula: Interactúa cuando está el objeto', function(assert){
+    return createPilasTest(this, 'EscenaTests', (pilas, resolve, pilasService) => {
+        let {Interactuar, alien, boton, cuadricula} = setup(pilasService);
+        cuadricula.agregarActor(alien,0,0);
+        cuadricula.agregarActor(boton,0,0);
+        
+        hacerLuegoConCallback(alien,Interactuar,argumentosParaApretar,() => {
+            assert.equal(argumentosParaApretar.animacionInteractuadoAlFinal, boton.nombreAnimacionActual());
+            resolve();
+        });
     });
 });
 
