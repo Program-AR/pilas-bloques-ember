f94640eb55a1146b88c73e94446a1fb71601f8ef
Lucas Alan Silvestri <silvestrilucasalan@gmail.com>
2019-06-21 14:46:06 -0300

    .eslintrc updated

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,7 +1,7 @@
 module.exports = {
   root: true,
   parserOptions: {
-    ecmaVersion: 2018,
+    ecmaVersion: 2015,
     sourceType: 'module'
   },
   plugins: [
@@ -12,39 +12,14 @@ module.exports = {
     'plugin:ember/recommended'
   ],
   env: {
-    browser: true
+    browser: false,
+    node: true
   },
-  rules: {
-  },
-  overrides: [
-    // node files
-    {
-      files: [
-        '.eslintrc.js',
-        '.template-lintrc.js',
-        'ember-cli-build.js',
-        'testem.js',
-        'blueprints/*/index.js',
-        'config/**/*.js',
-        'lib/*/index.js',
-        'server/**/*.js'
-      ],
-      parserOptions: {
-        sourceType: 'script',
-        ecmaVersion: 2015
-      },
-      env: {
-        browser: false,
-        node: true
-      },
-      plugins: ['node'],
-      rules: Object.assign({}, require('eslint-plugin-node').configs.recommended.rules, {
-        // add your custom rules and overrides for node files here
+  rules: Object.assign({}, require('eslint-plugin-node').configs.recommended.rules, {
+    // add your custom rules and overrides for node files here
 
-        // this can be removed once the following is fixed
-        // https://github.com/mysticatea/eslint-plugin-node/issues/77
-        'node/no-unpublished-require': 'off'
-      })
-    }
-  ]
+    // this can be removed once the following is fixed
+    // https://github.com/mysticatea/eslint-plugin-node/issues/77
+    'node/no-unpublished-require': 'off'
+  })
 };
