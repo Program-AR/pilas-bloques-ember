f7a8f5e43d01c28723a5bfe9992f14af0dc6988f
Lucas Alan Silvestri <silvestrilucasalan@gmail.com>
2019-07-05 15:45:38 -0300

    fix acceptance test

--- tests/acceptance/desafios-test.js
+++ tests/acceptance/desafios-test.js
@@ -1,22 +1,36 @@
-import { module, skip } from 'qunit';
+import { module, test } from 'qunit';
 import { setupApplicationTest } from 'ember-qunit';
 import setupMirage from "ember-cli-mirage/test-support/setup-mirage";
 
 import { create, visitable, count } from 'ember-cli-page-object';
 
 const page = create({
-  visit: visitable('/desafios'),
-  cantidadDeDesafiosDisponibles: count('.desafio a')
+  cantidadDeDesafiosDisponibles: count('.ember-view .desafio .desafio-img')
+});
+
+const page1 = create({
+  visit: visitable('/libros/1')
+});
+
+const page2 = create({
+  visit: visitable('/libros/2')
 });
 
 module('Acceptance | desafios', function (hooks) {
   setupApplicationTest(hooks);
   setupMirage(hooks);
 
-  skip('La cantidad de desafíos que se muestra es correcta', async function (assert) {
-    let cantidadDesafiosEsperada = 75;
-    await page.visit();
+  test('La cantidad de desafíos que se muestra en la pagina 1 es correcta', async function (assert) {
+    let cantidadDesafiosEsperada = 55;
+    await page1.visit();
     assert.equal(page.cantidadDeDesafiosDisponibles, cantidadDesafiosEsperada, `Hay exactamente ${cantidadDesafiosEsperada} desafios habilitados para utilizar.`);
   });
+
+  test('La cantidad de desafíos que se muestra en la pagina 2 es correcta', async function (assert) {
+    let cantidadDesafiosEsperada = 44;
+    await page2.visit();
+    assert.equal(page.cantidadDeDesafiosDisponibles, cantidadDesafiosEsperada, `Hay exactamente ${cantidadDesafiosEsperada} desafios habilitados para utilizar.`);
+  });
+
 
 });
