68fc7201e3b9e6279ae8cb75bfd64052e27a0262
PalumboN <nahuel.palumbo@gmail.com>
2019-08-05 20:06:24 -0300

    Testing required warnings

--- tests/unit/components/pilas-blockly-test.js
+++ tests/unit/components/pilas-blockly-test.js
@@ -1,100 +1,106 @@
-import { later } from '@ember/runloop';
-import { module, test } from 'qunit';
-import { setupTest } from 'ember-qunit';
+import { later } from '@ember/runloop'
+import { module, test } from 'qunit'
+import { setupTest } from 'ember-qunit'
 import {
   pilasMock,
   interpreterFactoryMock,
   interpreteMock,
   actividadMock,
   blocklyWorkspaceMock
-} from '../../helpers/mocks';
-import sinon from 'sinon';
+} from '../../helpers/mocks'
+import { findBlockByTypeIn, assertWarning, assertNotWarning } from '../../helpers/utils'
+import sinon from 'sinon'
 
 module('Unit | Components | pilas-blockly', function(hooks) {
-  setupTest(hooks);
+  setupTest(hooks)
 
   hooks.beforeEach(function() {
-      this.owner.register('service:interpreterFactory', interpreterFactoryMock);
+      this.owner.register('service:interpreterFactory', interpreterFactoryMock)
       this.owner.lookup('service:highlighter').workspace = blocklyWorkspaceMock()
       this.owner.lookup('service:blocksGallery').start()
 
-      this.ctrl = this.owner.factoryFor('component:pilas-blockly').create();
-      this.ctrl.pilas = pilasMock; //TODO: Injectar como service
-      this.ctrl.set('modelActividad', actividadMock);
-      sinon.resetHistory();
-  });
+      this.ctrl = this.owner.factoryFor('component:pilas-blockly').create()
+      this.ctrl.pilas = pilasMock //TODO: Injectar como service
+      this.ctrl.set('modelActividad', actividadMock)
+      sinon.resetHistory()
+  })
 
   //TODO: Ver de agrupar en modules
   test('Al ejecutar se encuentra ejecutando y ejecuta el intérprete', function(assert) {
-    this.ctrl.send('ejecutar');
+    this.ctrl.send('ejecutar')
 
-    assert.ok(this.ctrl.get('ejecutando'));
-    assert.notOk(this.ctrl.get('pausadoEnBreakpoint'));
-    assert.ok(interpreteMock.run.called);
-  });
+    assert.ok(this.ctrl.get('ejecutando'))
+    assert.notOk(this.ctrl.get('pausadoEnBreakpoint'))
+    assert.ok(interpreteMock.run.called)
+  })
 
   test('Ejecutar paso a paso bloquea la ejecución', function(assert) {
-    this.ctrl.send('ejecutar', true);
+    this.ctrl.send('ejecutar', true)
     
     later(() => {
-      assert.ok(interpreteMock.run.calledOnce);
-      assert.ok(this.ctrl.get('pausadoEnBreakpoint'));
-    });
-  });
+      assert.ok(interpreteMock.run.calledOnce)
+      assert.ok(this.ctrl.get('pausadoEnBreakpoint'))
+    })
+  })
 
   test('Step desbloquea el breakpoint', function(assert) {
-    this.ctrl.send('ejecutar', true);
+    this.ctrl.send('ejecutar', true)
     
     later(() => {
-      assert.ok(this.ctrl.get('pausadoEnBreakpoint'));
-      this.ctrl.send('step');
-      assert.notOk(this.ctrl.get('pausadoEnBreakpoint'));
-    });
-  });
+      assert.ok(this.ctrl.get('pausadoEnBreakpoint'))
+      this.ctrl.send('step')
+      assert.notOk(this.ctrl.get('pausadoEnBreakpoint'))
+    })
+  })
 
   test('Luego de ejecutar termina de ejecutar', function(assert) {
-    this.ctrl.send('ejecutar');
+    this.ctrl.send('ejecutar')
 
     later(() => {
-      assert.notOk(this.ctrl.get('ejecutando'));
-      assert.ok(this.ctrl.get('terminoDeEjecutar'));
-    });
-  });
+      assert.notOk(this.ctrl.get('ejecutando'))
+      assert.ok(this.ctrl.get('terminoDeEjecutar'))
+    })
+  })
 
   test('Al resolver el problema muestra el fin del desafío', function(assert) {
-    this.ctrl.set('debeMostrarFinDeDesafio', true);
-    this.ctrl.send('ejecutar');
+    this.ctrl.set('debeMostrarFinDeDesafio', true)
+    this.ctrl.send('ejecutar')
 
     later(() => {
-      assert.ok(this.ctrl.get('mostrarDialogoFinDesafio'));
-    });
-  });
+      assert.ok(this.ctrl.get('mostrarDialogoFinDesafio'))
+    })
+  })
 
   test('Al reiniciar settea flags y reinicia la escena de pilas', function(assert) {
-    this.ctrl.send('reiniciar');
+    this.ctrl.send('reiniciar')
 
-    assert.notOk(this.ctrl.get('ejecutando'));
-    assert.notOk(this.ctrl.get('terminoDeEjecutar'));
-    assert.notOk(this.ctrl.get('errorDeActividad'));
-    assert.ok(pilasMock.reiniciarEscenaCompleta.called);
-  });
+    assert.notOk(this.ctrl.get('ejecutando'))
+    assert.notOk(this.ctrl.get('terminoDeEjecutar'))
+    assert.notOk(this.ctrl.get('errorDeActividad'))
+    assert.ok(pilasMock.reiniciarEscenaCompleta.called)
+  })
 
 
-  let simpleProgram = `
+  let filledProgram = `
     <block type="al_empezar_a_ejecutar">
       <statement name="program">
         <block type="MoverACasillaDerecha"></block>
       </statement>
     </block>
   `
+  let emptyProcedure = `    
+  <block type="procedures_defnoreturn">
+    <field name="NAME">Hacer algo</field>
+  </block>
+  `
 
   test('Ejecuta cuando todos los bloques están completos', function(assert) {
-    Blockly.textToBlock(simpleProgram)
+    Blockly.textToBlock(filledProgram)
 
-    this.ctrl.send('ejecutar');
+    this.ctrl.send('ejecutar')
 
-    assert.ok(interpreteMock.run.called);
-  });
+    assert.ok(interpreteMock.run.called)
+  })
 
   test('No ejecuta cuando el programa tiene algún agujero', function(assert) {
     let program = `
@@ -112,10 +118,10 @@ module('Unit | Components | pilas-blockly', function(hooks) {
     `
     Blockly.textToBlock(program)
 
-    this.ctrl.send('ejecutar');
+    this.ctrl.send('ejecutar')
 
-    assert.notOk(interpreteMock.run.called);
-  });
+    assert.notOk(interpreteMock.run.called)
+  })
 
   test('Ejecuta cuando existe algún bloque con agujeros pero no se usa', function(assert) {
     let bloqueSuelto = `    
@@ -128,28 +134,33 @@ module('Unit | Components | pilas-blockly', function(hooks) {
     </block>
     `
 
-    Blockly.textToBlock(simpleProgram)
+    Blockly.textToBlock(filledProgram)
     Blockly.textToBlock(bloqueSuelto)
 
-    this.ctrl.send('ejecutar');
+    this.ctrl.send('ejecutar')
 
-    assert.ok(interpreteMock.run.called);
-  });
+    assert.ok(interpreteMock.run.called)
+  })
 
   test('No ejecuta cuando existe algún procedimiento vacío', function(assert) {
-    let emptyProcedure = `    
-    <block type="procedures_defnoreturn">
-      <field name="NAME">Hacer algo</field>
-    </block>
-    `
-
-    Blockly.textToBlock(simpleProgram)
+    Blockly.textToBlock(filledProgram)
     Blockly.textToBlock(emptyProcedure)
 
-    this.ctrl.send('ejecutar');
+    this.ctrl.send('ejecutar')
 
-    assert.notOk(interpreteMock.run.called);
-  });
+    assert.notOk(interpreteMock.run.called)
+  })
 
-});
+  test('Al ejecutar aparecen los warnings de bloques vacíos', function(assert) {
+    let procedure = Blockly.textToBlock(emptyProcedure)
+    let required = findBlockByTypeIn(procedure, "required_statement")
+    
+    assertNotWarning(assert, required)
+
+    this.ctrl.send('ejecutar')
+
+    later(() => assertWarning(assert, required, "Hay un espacio en blanco!"))
+  })
+
+})
 
