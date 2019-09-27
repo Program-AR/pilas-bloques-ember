8bd921ab1b6d59f3b89b0bc7bf50d274c02e0035
PalumboN <nahuel.palumbo@gmail.com>
2019-09-27 18:30:05 -0300

    Refactor pilas-mulang

--- app/services/pilas-mulang.js
+++ app/services/pilas-mulang.js
@@ -4,9 +4,9 @@ import Service from '@ember/service';
 
 export default Service.extend({
 
+  //TODO: Recibir el workspace??
   analyze(mainBlock, expectations = []) {
     let ast = this.parse(mainBlock)
-    // console.log({ast})
     
     let result = mulang.analyse({
       "sample" : {
@@ -32,169 +32,32 @@ export default Service.extend({
 });
 
 
-function createNode(tag, contents) {
-  if(tag === "Equation") return [contents];
-  return contents !== undefined ? {tag, contents} : {tag};
-}
-
-function nextBlockFor(block) {
-  return block.getNextBlock();
-}
-
-function getChild(block) {
-  return block.getChildren()[0]
-}
-
-function getBlock(blockId) {
-  return targetBlocks[blockId]
-}
-
-function parse(solution) {
-  let targetsContents = Object.values(solution.targets).map(target => buildTargetAst(target));
-  return createNode("Sequence", targetsContents);
-}
-
-function buildTargetVariables(target) {
-  let variables = buildVariables(target.variables, buildVariable);
-  let lists = buildVariables(target.lists, buildList);
-  return [...lists, ...variables];
-}
-
-function buildVariables(variables = {}, buildFunction) {
-  return Object.values(variables).map(variable => buildFunction(variable[0]));
-}
-
-function buildList(name) {
-  return buildVariable(name, createNode("MuList", []));
-}
-
-function buildVariable(name, contents) {
-  return createNode("Variable", [
-    name,
-    contents || buildEmptyNode()
-  ]);
-}
-
-function buildEmptyNode() {
-  return createNode("None", []);
-}
-
-function buildSequenceAst(topLevelBlock) {
-  if (!topLevelBlock) return buildEmptyNode();
-  let siblings = getBlockSiblings(topLevelBlock);
-  if (siblings.length) {
-    return createNode("Sequence", [topLevelBlock, ...siblings].map(b => buildBlockAst(b)))
-  } else {
-    return buildBlockAst(topLevelBlock);
-  }
-}
-
-function buildTargetAst(target) {
-  targetBlocks = target.blocks;
-  let topLevelBlocks = _.pickBy(targetBlocks, b => b.topLevel);
-  let topLevelSequence = [];
-  _.forOwn(topLevelBlocks, block => {
-    if(blocksStructures.validTopLevelBlock(block)){
-      topLevelSequence.push(buildSequenceAst(block))
-    }
-  });
-  topLevelSequence =  topLevelSequence.concat(buildTargetVariables(target));
-  return buildEntryPoint(target.name, createNode("Sequence", topLevelSequence));
-}
-
-function buildEntryPoint(name, contents) {
-  return createNode("EntryPoint", [name, contents]);
-}
-
-function hasSiblings(block) {
-  return block.getNextBlock() // && !hasImplicitSubstack(block.opcode);
-}
-
-/* Although it should be a substack, the procedure's body blocks  and listener
-   blocks are implemented as siblings, so this exception is needed; */
-
-function hasImplicitSubstack(opcode){
-  const withImplicitSubstack = ['procedures_definition', 'start_as_clone'];
-  return _.includes(withImplicitSubstack, opcode) || opcode.startsWith('event_when');
-}
-
-function getBlockSiblings(block) {
-  const siblings = [];
-  while (hasSiblings(block)){
-    block = nextBlockFor(block);
-    siblings.push(block);
-  }
-  return siblings;
-}
-
-
-
-
-
 function buildBlockAst(block) {
-  let {tag, parse} = parser(block);
-  return createNode(tag, parse(block));
+  let tag = mulangTag(block);
+  return createNode(tag, mulangParsers[tag](block));
 }
 
-function parser(block) {
-  var tag
-  var parse
-  switch (block.type) {
-    case "al_empezar_a_ejecutar":
-      tag = "EntryPoint"
-      parse = parseEntryPoint
-      break;
-  
-    case "repetir":
-      tag = "Repeat"
-      parse = parseRepeat
-      break;
-  
-    case "Si":
-      tag = "If"
-      parse = parseIf
-      break;
+function mulangTag(block) {
+  let tag = pilasToMulangTags[block.type]
+  if (tag) return tag
+  return block.categoria == "Valores" ? "Reference" : "Application"
+}
 
-    case "SiNo":
-      tag = "If"
-      parse = parseIfElse
-      break;
-  
-    case "Hasta":
-      tag = "While"
-      parse = parseWhile
-      break;
-  
-    case "math_number":
-      tag = "MuNumber"
-      parse = parseMuNumber
-      break;
-  
-    case "procedures_defnoreturn":
-      tag = "Procedure"
-      parse = parseProcedure
-      break;
-
-    case "variables_get":
-      tag = "Reference"
-      parse = parseVariable
-      break;
-
-    default:
-      if (block.categoria == "Valores") {
-        tag = "Reference"
-        parse = parseReference
-      } else {
-        tag = "Application"
-        parse = parseApplication
-      }
-      break;
-  }
-  return {tag, parse}
+let pilasToMulangTags = {
+  "al_empezar_a_ejecutar": "EntryPoint",
+  "repetir": "Repeat",
+  "Si": "If",
+  "SiNo": "If",
+  "Hasta": "While",
+  "math_number": "MuNumber",
+  "procedures_defnoreturn": "Procedure",
+  "variables_get": "Reference",
 }
 
 
-
+function parseMuNumber(block) {
+  return parseFloat(block.getFieldValue("NUM"));
+}
 
 function parseEntryPoint(block) {
   return [
@@ -210,15 +73,26 @@ function parseApplication(block) {
   ]
 }
 
+function parseReference(block) {
+  if (block.type == "variables_get") //TODO
+    return parseVariable(block)
+  else
+    return block.type
+}
+
+function parseVariable(block) {
+  return block.getFieldValue("VAR")
+}
+
 function parseArguments(block) {
   return block.inputList
     .filter(input => input.type == Blockly.INPUT_VALUE)
     .map(input => input.connection.targetBlock())    
     .map(b => buildBlockAst(b))
-    .concat(...getTextArguments(block))
+    .concat(...parseText(block))
 }
 
-function getTextArguments(block) {
+function parseText(block) {
   let text = block.getFieldValue("texto") // TODO: Mega-hard-coded
   if (!text) return []
   return [createNode("MuString", text)]
@@ -242,6 +116,13 @@ function parseWhile(block) {
   ]
 }
 
+function parseAbstractIf(block) {
+  if (block.type == "Si") //TODO
+    return parseIf(block)
+  else
+    return parseIfElse(block)
+}
+
 function parseIf(block) {
   let condition = block.getInputTargetBlock("condition")
   let statements = block.getInputTargetBlock("block")
@@ -262,28 +143,6 @@ function parseIfElse(block) {
     buildSequenceAst(statementsElse)
   ]
 }
-
-function parseMuNumber(block) {
-  return parseFloat(block.getFieldValue("NUM"));
-}
-
-function parseVariable(block) {
-  return block.getFieldValue("VAR")
-}
-
-function parseReference(block) {
-  return block.type
-}
-
-
-// TODO: Duplicate from blocks-gallery
-function getProcedureName(procedureBlock) { 
-  return procedureBlock.getProcedureDef()[0];
-}
-function getParams(procedureBlock) {
-  return procedureBlock.getProcedureDef()[1]
-}
-
 
 function parseProcedure(block) {
   return [
@@ -308,159 +167,75 @@ function parseEquationBody(block) {
   return createNode("UnguardedBody", bodyContents);
 }
 
+let mulangParsers = {
+  "Application": parseApplication,
+  "Reference": parseReference,
+  "Repeat": parseRepeat,
+  "While": parseWhile,
+  "If": parseAbstractIf,
+  "Procedure": parseProcedure,
+  "MuNumber": parseMuNumber,
+  "EntryPoint": parseEntryPoint
+}
 
 
 
 
+
+
+// TODO: Duplicate from blocks-gallery
+function getProcedureName(procedureBlock) { 
+  return procedureBlock.getProcedureDef()[0];
+}
+function getParams(procedureBlock) {
+  return procedureBlock.getProcedureDef()[1]
+}
+
+
+
+
+function createNode(tag, contents) {
+  if(tag === "Equation") return [contents];
+  return contents !== undefined ? {tag, contents} : {tag};
+}
 
 function createReference(block) {
   return createNode("Reference", block.type)
 }
 
-
-
-
-
-function parseRegularBlock(blockInfo) {
-  let mulangTag = blockInfo.blockStructure.mulangTag;
-  let parsingFunction = parseMulangTag(mulangTag);
-  return parsingFunction(blockInfo);
+function buildEmptyNode() {
+  return createNode("None", []);
 }
 
-function parseMulangTag(mulangTag) {
-  return mulangTags[mulangTag];
-}
-
-function parseField(input) {
-  let fieldValue = input.value;
-  return createNode("Reference", fieldValue);
-}
-
-function parseInput(input) {
-  let inputBlock = getInputBlock(input);
-  if (!inputBlock) return createNode("MuNil");
-  return buildSequenceAst(inputBlock);
-}
-
-function getInputBlock(input) {
-  let inputBlockId = input.block || input.shadow;
-  return targetBlocks[inputBlockId];
-}
-
-function parseInputBlock(blockInfo) {
-  if(blockInfo.blockStructure.mulangTag){
-    return parseRegularBlock(blockInfo);
-  }
-  else {
-    return parseAttributes(blockInfo);
-  }
-}
-
-function parseBlockInputs(attributes = [], block) {
-  return attributes.map(attr => doParseInput(attr, block))
-}
-
-function parseBlockFields(attributes = [], block) {
-  return attributes.map(attr => doParseField(attr, block))
-}
-
-function doParseInput(attribute, block) {
-  return parseAttribute(attribute, block, parseInput, 'inputs');
-}
-
-function doParseField(attribute, block) {
-  return parseAttribute(attribute, block, parseField, 'fields');
-}
-
-function parseAttribute(attr, block, parseFunction, type) {
-  let attribute = block[type][attr];
-  if (attribute) {
-    return parseFunction(attribute);
+function buildSequenceAst(topLevelBlock) {
+  if (!topLevelBlock) return buildEmptyNode();
+  let siblings = getBlockSiblings(topLevelBlock);
+  if (siblings.length) {
+    return createNode("Sequence", [topLevelBlock, ...siblings].map(b => buildBlockAst(b)))
   } else {
-    return buildEmptyNode();
+    return buildBlockAst(topLevelBlock);
   }
 }
 
-function getBlockInputs(blockStructure, block) {
-  if(blockStructure.unstructured) {
-    return Object.keys(block.inputs);
+function hasSiblings(block) {
+  return block.getNextBlock() 
+}
+
+function getBlockSiblings(block) {
+  const siblings = [];
+  while (hasSiblings(block)){
+    block = nextBlockFor(block);
+    siblings.push(block);
   }
-  else {
-    return blockStructure.inputs;
-  }
+  return siblings;
 }
 
-function getExpression(blockInfo) {
-  let attributes = parseAttributes(blockInfo);
-  return attributes[0];
+function nextBlockFor(block) {
+  return block.getNextBlock();
 }
 
-function getContents(blockInfo) {
-  return getExpression(blockInfo).contents;
+function getChild(block) {
+  return block.getChildren()[0]
 }
-
-function getWhileExpression(blockInfo) {
-  // Forever is parsed as while true as mulang doesn't support it natively
-  // Repeat_until is parsed as while(!condition)
-  if(blockInfo.normalizedOpcode === "forever"){
-    return createNode("MuBool", true);
-  } else {
-    return createNotApplication(getExpression(blockInfo));
-  }
-}
-
-function createNotApplication(contents) {
-  let negatedContents = [createNode("Reference", "not"), [contents]];
-  return createNode("Application", negatedContents)
-}
-
-function parseAssignment(blockInfo) {
-  return [
-    getContents(blockInfo),
-    doParseInput("VALUE", blockInfo.block)
-  ]
-
-}
-
-/* This function generates the name of a listener by replacing the $N tokens, with the
-   attributes that are located at the N position.
-   For example the name of when_greater_than block is defined as when_$0_greater_than_$1
-   so $0 is replaced by the first argument and $1 by the second one
-   If the argument is another block, it is parsed as 'custom_block' as it would be very complex
-   to create a consistent naming for each possible combination */
-
-function normalizeListenerName(listenerName, attributes) {
-  let normalizingRegex = new RegExp("\\$.", "g");
-  return listenerName.replace(normalizingRegex, (token) => {
-    let attributeIndex = token[1];
-    let attribute = attributes[attributeIndex];
-    return attribute instanceof Object ? "custom_block" : attribute.toString().toLowerCase();
-  });
-}
-
-function getListenerNormalizedName(blockInfo) {
-  let attributes = parseAttributes(blockInfo).map(attr => attr.contents);
-  let customNamePlaceholder = blockInfo.blockStructure.customName;
-  return normalizeListenerName(customNamePlaceholder, attributes);
-}
-
-let mulangTransform = {
-  "Repetir": "Repeat"
-}
-
-let mulangTags = {
-  "Application": parseApplication,
-  "Reference": parseReference,
-  "Repeat": parseRepeat,
-  "While": parseWhile,
-  "If": parseIf,
-  "Procedure": parseProcedure,
-  "Equation": parseEquation,
-  "MuNumber": parseMuNumber,
-  "MuString": getContents,
-  "Assignment": parseAssignment,
-  "EntryPoint": parseEntryPoint
-};
-
 // Code here will be ignored by JSHint.
 /* jshint ignore:end */
