1bd82c699fc47258ecbfd450d3224f7545d727c5
Lucas Alan Silvestri <silvestrilucasalan@gmail.com>
2019-12-03 14:48:17 -0300

    plias-header renamed to header

--- tests/integration/components/header-test.js
+++ tests/integration/components/header-test.js
@@ -10,7 +10,7 @@ module('Integration | Component | pilas header', function (hooks) {
     // Set any properties with this.set('myProperty', 'value');
     // Handle any actions with this.on('myAction', function(val) { ... });
 
-    await render(hbs`{{pilas-header}}`);
+    await render(hbs`{{header}}`);
 
     assert.dom().hasAnyText();
 
