3e1cbca888a423f683c2b0a4dbcda38cf9194824
Lucas Alan Silvestri <silvestrilucasalan@gmail.com>
2019-12-09 15:10:54 -0300

    bug-report-updated

--- app/components/bug-report-modal.js
+++ app/components/bug-report-modal.js
@@ -3,6 +3,7 @@ import Component from '@ember/component';
 export default Component.extend({
 
     actions: {
+        // Only used with alternative version with github integration.
         createIssue() {
             fetch('https://api.github.com/repos/Program-AR/pilas-bloques/issues', {
                 method: 'POST',
