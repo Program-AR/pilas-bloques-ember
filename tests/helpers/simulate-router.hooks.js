edf98c679e7a142207bd47955f8c81a2d0593e54
Lucas Alan Silvestri <silvestrilucasalan@gmail.com>
2020-06-20 20:09:33 -0300

    router model hooks simulation function comment fixed

--- tests/helpers/simulate-router.hooks.js
+++ tests/helpers/simulate-router.hooks.js
@@ -1,14 +1,7 @@
+/** 
+ * Simulate the model hook from router.
+ */
 export default async (store) => {
-    /** 
-     * Simulate the model hook from router.
-     * 
-     * TODO: replace the findAll("desafio") and findBy('nombre', nombre)
-     * function by a more specific ember-data query, like findRecord which 
-     * fetchs only one record.
-     * 
-     * (This only exist because mirage must be need fixed before).
-     */
-
     await store.findAll("libro");
     await store.findAll("capitulo");
     await store.findAll("grupo");
