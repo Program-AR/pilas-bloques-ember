e95645447955e29acb6c79aedb0a2dd9e3aac579
Lucas Alan Silvestri <silvestrilucasalan@gmail.com>
2020-07-23 03:37:35 -0300

    add test

--- tests/integration/desafios/FutbolDeRobots-test.js
+++ tests/integration/desafios/FutbolDeRobots-test.js
@@ -70,4 +70,79 @@ moduloActividad(nombre, () => {
 	</xml>`,
 	});
 
+	actividadTest(nombre, {
+		descripcionAdicional: 'No se puede mover a la derecha luego de haber pateado una pelota',
+		errorEsperado: 'No puedo ir para la derecha',
+		solucion: `<?xml version="1.0" encoding="UTF-8"?>
+			<xml xmlns="http://www.w3.org/1999/xhtml">
+			<variables />
+			<block type="al_empezar_a_ejecutar" id="R#u-G?}cX2zfH[M-UVuc" deletable="false" movable="false" editable="false" x="15" y="15">
+				<statement name="program">
+					<shadow type="required_statement" id="fF@TlUQgOpLWN_m1b*){" />
+					<block type="procedures_callnoreturn" id="lC,iTKKXrAH2};q3n*L2">
+						<mutation name="Patear todas las pelotas" />
+					</block>
+				</statement>
+			</block>
+			<block type="procedures_defnoreturn" id="pS^gxbVTfES(#_uh4u00" x="17" y="102">
+				<field name="NAME">Patear la pelota de una fila</field>
+				<statement name="STACK">
+					<block type="Hasta" id="5xjsZ0L9lRx]8Do/H!p@">
+						<value name="condition">
+						<shadow type="required_value" id="}j_8+/@XXv|8XRX!0*:p" />
+						<block type="TocandoPelota" id="Y;=e=uXBs[)XS9+GGGrh" />
+						</value>
+						<statement name="block">
+						<shadow type="required_statement" id="vRZIe-,nTB=o7EsXkpD;" />
+						<block type="MoverACasillaDerecha" id="2!~m@gW)0@Q*qNVkM3!|" />
+						</statement>
+						<next>
+						<block type="PatearPelota" id="R/25PmB,pTT*3_I)#.q_" />
+						</next>
+					</block>
+				</statement>
+			</block>
+			<block type="procedures_defnoreturn" id="S9Z1N#Aq8XJp9i_H_Gi?" x="376" y="96">
+				<field name="NAME">Patear todas las pelotas</field>
+				<statement name="STACK">
+					<block type="repetir" id="n8KTdgC{VMVZRy#H]%zx">
+						<value name="count">
+						<shadow type="required_value" id="m[3/=#Q*g5^C=;NM;GrY" />
+						<block type="math_number" id=",b{M=z$;nw7-})T.vn7c">
+							<field name="NUM">7</field>
+						</block>
+						</value>
+						<statement name="block">
+						<shadow type="required_statement" id="$;Lq[3i[6IzxUg!i,|bK" />
+						<block type="procedures_callnoreturn" id="46RBv#=^7BJYJ$F:+M:z">
+							<mutation name="Patear la pelota de una fila" />
+							<next>
+								<block type="procedures_callnoreturn" id="5Uw+9I[-A9Gb?8/Up]u#">
+									<mutation name="Pasar a la siguente fila" />
+								</block>
+							</next>
+						</block>
+						</statement>
+						<next>
+						<block type="procedures_callnoreturn" id="eUk(kjVaul6FOa)?S0pv">
+							<mutation name="Patear la pelota de una fila" />
+						</block>
+						</next>
+					</block>
+				</statement>
+			</block>
+			<block type="procedures_callnoreturn" id="Yo+%Hn_6#L+C2~q-z-8j" disabled="true" x="743" y="115">
+				<mutation name="Patear todas las pelotas" />
+			</block>
+			<block type="procedures_callnoreturn" id="[9rV,3DyLt+K!09lam3)" disabled="true" x="743" y="115">
+				<mutation name="Patear todas las pelotas" />
+			</block>
+			<block type="procedures_defnoreturn" id="V=A)VCb:?l-?L[_0WP!R" x="20" y="275">
+				<field name="NAME">Pasar a la siguente fila</field>
+			</block>
+			</xml>
+		`,
+	});
+
+
 });
