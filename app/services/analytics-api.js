4c43e1e846da782e2f2307d943f65ad30c46620f
PalumboN <nahuel.palumbo@gmail.com>
2020-09-16 13:48:28 -0300

    Refactor analyticsApi

--- app/services/analytics-api.js
+++ app/services/analytics-api.js
@@ -8,40 +8,32 @@ export default Service.extend({
   platform: service(),
 
   openChallenge(challengeId) {
-    const url = `${baseURL}/challenges`
-    const body = this.buildBody(challengeId)
-
-    fetch(url, {
-      method: "POST",
-      body: JSON.stringify(body),
-      headers: { 'Content-Type': 'application/json' }
-    })
-      .then((res) => { if (res.status > 200) return res.text() })
-      .then((pbAnalyticsError) => { if (pbAnalyticsError) console.log({ pbAnalyticsError }) }) // Api error
-      .catch((pbAnalyticsError) => console.log({ pbAnalyticsError })) // Connection error
-
+    const body = this._buildBody(challengeId)
+    this._send('POST', 'challenges', body)
   },
 
   runProgram(challengeId, program, staticAnalysis) {
-    const url = `${baseURL}/solutions`
     const solutionId = uuidv4()
     const body = {
       solutionId,
       program,
       staticAnalysis,
-      ...this.buildBody(challengeId)
+      ...this._buildBody(challengeId)
     }
 
-    fetch(url, {
-      method: "POST",
-      body: JSON.stringify(body),
-      headers: { 'Content-Type': 'application/json' }
-    })
+    this._send('POST', 'solutions', body)
 
     return solutionId
   },
 
-  buildBody(challengeId) {
+  checkSessionId() {
+    let session = JSON.parse(localStorage.getItem(this.ANALYTICS_KEY))
+    const isOld = () => (new Date() - new Date(session.timestamp)) / 1000 / 60 > sessionExpire // Minutes
+    if (!session || isOld()) return this._updateSession().id
+    return session.id
+  },
+
+  _buildBody(challengeId) {
     const online = this.platform.online()
     const fingerprint = new ClientJS().getFingerprint()
     const sessionId = this.checkSessionId()
@@ -55,19 +47,25 @@ export default Service.extend({
     }
   },
 
-  checkSessionId() {
-    let session = JSON.parse(localStorage.getItem(this.ANALYTICS_KEY))
-    const isOld = () => (new Date() - new Date(session.timestamp)) / 1000 / 60 > sessionExpire // Minutes
-    if (!session || isOld()) return this.updateSession().id
-    return session.id
-  },
-
-  updateSession() {
+  _updateSession() {
     const newSession = {
       id: uuidv4(),
       timestamp: new Date()
     }
     localStorage.setItem(this.ANALYTICS_KEY, JSON.stringify(newSession))
     return newSession
-  }
+  },
+
+  _send(method, resource, body) {
+    const url = `${baseURL}/${resource}`
+
+    return fetch(url, {
+      method,
+      body: JSON.stringify(body),
+      headers: { 'Content-Type': 'application/json' }
+    })
+      .then((res) => { if (res.status > 200) return res.text() })
+      .then((pbAnalyticsError) => { if (pbAnalyticsError) console.log({ pbAnalyticsError }) }) // Api error
+      .catch((pbAnalyticsError) => console.log({ pbAnalyticsError })) // Connection error
+  },
 })
