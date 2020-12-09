6cefc910fd897c5880aecd9d0f276faa34a4c04f
Lucas Alan Silvestri <silvestrilucasalan@gmail.com>
2020-12-09 10:53:16 -0300

    bye makefile and unnecessary scripts

--- scripts/ember.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-# The ember executable path.
-EMBER=./node_modules/.bin/ember
-
-echo $EMBER
--- scripts/test.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-EMBER=$(sh scripts/ember.sh)
-
-case "$1" in
-    (-live) $EMBER test --serve ;;
-    (-ci) $EMBER test           ;;
-    (*) $EMBER test --serve     ;;
-esac 
