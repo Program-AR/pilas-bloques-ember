1b6870ecf167bb560b83e8f309611999575e80fc
PalumboN <nahuel.palumbo@gmail.com>
2021-03-29 19:09:28 -0300

    Using User.id in session / context

--- app/services/pilas-bloques-api.js
+++ app/services/pilas-bloques-api.js
@@ -85,7 +85,7 @@ export default Service.extend({
       return; 
     }
     const user = this.getUser()
-    if (body) { body.session = this.pilasBloquesAnalytics.buildSession(user && user.nickName) } //TODO: Move user to Analytics / use id instead of nickname / rename Analytics to session related approach
+    if (body) { body.session = this.pilasBloquesAnalytics.buildSession() }
 
     const url = `${baseURL}/${resource}`
     const flag = `loading.${resource.split('?')[0].replace('/', '-')}`
--- app/services/storage.js
+++ app/services/storage.js
@@ -4,6 +4,11 @@ export default Ember.Service.extend({
   USER_KEY: 'PB_USER',
   ANALYTICS_KEY: 'PB_ANALYTICS_SESSION',
 
+  getUserId() { 
+    const user = this.getUser()
+    return user && user.id
+  },
+
   getUser()       { return this._get(this.USER_KEY) },
   saveUser(user)  { this._save(this.USER_KEY, user) },
 
