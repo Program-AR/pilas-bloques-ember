411bedf92a6cc107eeb36f0df4fc47f184602b76
tfloxolodeiro <tfloxolodeiro@est.frba.utn.edu.ar>
2021-04-14 16:02:36 -0300

    moving most register validations to controller

--- app/controllers/register.js
+++ app/controllers/register.js
@@ -18,6 +18,32 @@ export default Controller.extend({
     return this.avatardb.allAvatars()
   }),
 
+  mailValidation:[{
+    message: 'Este no es un email válido',
+    validate: (mail) => {
+      // https://stackoverflow.com/questions/46155/how-to-validate-an-email-address-in-javascript
+      const re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+      return re.test(mail)
+    }
+  }],
+
+  parentNameValidation:[{
+    message: 'Se requiere nombre y apellido',
+    validate: (parentName) => {
+      const re = /.+ .+/
+      return re.test(parentName)
+    }
+  }],
+
+  DNIValidation:[{
+    message: 'El DNI es invalido',
+    validate: (DNI) =>{
+      // any string with at least 6 numbers
+      const re = new RegExp('(\\D*\\d\\D*){6,}')
+      return re.test(DNI)
+    }
+  }],
+
   actions: {
     doRegister() {
       this.pilasBloquesApi.register(this.registerData)
@@ -28,22 +54,5 @@ export default Controller.extend({
       this.pilasBloquesApi.userExists(this.registerData.username)
         .then(exist => this.set("validUsername", !exist))
     },
-
-    checkEmail() {
-      // https://stackoverflow.com/questions/46155/how-to-validate-an-email-address-in-javascript
-      const re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
-      this.set("validEmail", re.test(this.registerData.email))
-    },
-
-    checkParentName() {
-      const re = /.+ .+/
-      this.set("validParentName", re.test(this.registerData.parentName))
-    },
-
-    checkDNI() {
-      // any string with at least 6 numbers
-      const re = new RegExp('(\\D*\\d\\D*){6,}')
-      this.set("validDNI", re.test(this.registerData.parentDNI))
-    }
   },
 })
