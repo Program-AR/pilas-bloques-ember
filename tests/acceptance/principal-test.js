8c5d23b98b490070f4d59d61bb26895c8f8a3463
tfloxolodeiro <tfloxolodeiro@est.frba.utn.edu.ar>
2021-05-06 22:09:34 -0300

    Test fix

--- tests/acceptance/principal-test.js
+++ tests/acceptance/principal-test.js
@@ -17,6 +17,6 @@ module('Acceptance | principal', function (hooks) {
 
   test('visiting /', async function (assert) {
     await page.visit();
-    assert.equal(page.cantidadDeLinks, 5, "La cantidad de links en pantalla renderiza bien.");
+    assert.equal(page.cantidadDeLinks, 6, "La cantidad de links en pantalla renderiza bien.");
   });
 });
