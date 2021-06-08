bec015ad5628b0503116bde7fb5cf15966c325c0
Nahuel Palumbo <nahuel.palumbo@gmail.com>
2021-06-08 13:36:43 -0300

    Update tests/integration/components/scene-details-test.js
    Co-authored-by: asanzo <alfredo.sanzo@gmail.com>

--- tests/integration/components/scene-details-test.js
+++ tests/integration/components/scene-details-test.js
@@ -4,7 +4,7 @@ import { setupRenderingTest } from 'ember-qunit';
 import { render } from '@ember/test-helpers';
 import hbs from 'htmlbars-inline-precompile';
 
-module('integration | component | scene details', function (hooks) {
+module('Integration | Component | Scene details', function (hooks) {
   setupRenderingTest(hooks);
 
   test('it renders markdown', async function (assert) {
