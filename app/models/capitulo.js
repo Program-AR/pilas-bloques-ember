9232cafc5b37628999a30ed36452851dbb1485eb
giordanolucas <lucas.giordano@mulesoft.com>
2021-06-10 17:15:26 -0300

    Fix jshint

--- app/models/capitulo.js
+++ app/models/capitulo.js
@@ -1,5 +1,4 @@
 import Model from 'ember-data/model';
-import attr from 'ember-data/attr';
 import { hasMany, belongsTo } from 'ember-data/relationships';
 import { computed } from '@ember/object';
 import Ember from 'ember';
