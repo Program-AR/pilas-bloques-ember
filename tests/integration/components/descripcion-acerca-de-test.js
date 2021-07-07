04aec9660fa13d190eabcd99ee18bdbc6d6f2375
Alfredo Sanzo <alfredo.sanzo@gmail.com>
2021-07-07 03:29:12 -0300

    fixing test

--- tests/integration/components/descripcion-acerca-de-test.js
+++ tests/integration/components/descripcion-acerca-de-test.js
@@ -1,13 +1,14 @@
-import { module, test } from 'qunit';
-import { setupRenderingTest } from 'ember-qunit';
-import { render } from '@ember/test-helpers';
-import { hbs } from 'ember-cli-htmlbars';
+import { module, test } from 'qunit'
+import { setupRenderingTest } from 'ember-qunit'
+import { render } from '@ember/test-helpers'
+import { hbs } from 'ember-cli-htmlbars'
 
 module('Integration | Component | descripcion-acerca-de', function(hooks) {
-  setupRenderingTest(hooks);
+  setupRenderingTest(hooks)
 
   test('it renders', async function(assert) {
+    this.owner.lookup('service:intl').setLocale('es-ar')
     await render(hbs`<DescripcionAcercaDe />`);
-    assert.ok(this.element.textContent.includes('Acerca de'));
-  });
-});
+    assert.ok(this.element.textContent.includes('Acerca de'))
+  })
+})
