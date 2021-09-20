8439b91dce4ca56b0e54b47896d5d7ccf3bcc1be
PalumboN <nahuel.palumbo@gmail.com>
2021-09-20 16:40:24 -0300

    Hacks for mulang

--- app/routes/application.js
+++ app/routes/application.js
@@ -4,5 +4,14 @@ export default Route.extend({
   intl: service(),
   beforeModel() {
     this.get('intl').setLocale(['es-ar']);
+  },
+
+  afterModel() {
+    // Load Mulang lazy
+    const _script = document.createElement('script')
+    _script.type = 'text/javascript'
+    _script.src = 'assets/mulang.js'
+    const _head = document.getElementsByTagName("head")[0]
+    _head.appendChild(_script)
   }
 });
