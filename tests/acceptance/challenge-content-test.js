2cd58f01fea5123d23fc18d009c1e0ab97d0dea7
tfloxolodeiro <tfloxolodeiro@est.frba.utn.edu.ar>
2021-09-29 12:50:40 -0300

    fixing challenge content tests

--- tests/acceptance/challenge-content-test.js
+++ tests/acceptance/challenge-content-test.js
@@ -14,7 +14,7 @@ module('Acceptance | challenge content internationalization test', function (hoo
    * Checks title, description and clue of the challenge are translated.
    */
 
-  function testCanVisit(challengeId) {
+  function testCanVisit(challengeId, expectedTitle, expectedescription, expectedClue) {
     test(`Challenge ${challengeId} is internationalized`, async function (assert) {
       // The visit helper has a known bug, so we need this try/catch
       // https://github.com/emberjs/ember-test-helpers/issues/332 (still open)
@@ -30,36 +30,40 @@ module('Acceptance | challenge content internationalization test', function (hoo
         }
       }
 
-      var intl = this.owner.lookup('service:intl')
-
-      var expectedTitle = intl.t("model.challenges." + challengeId + ".title").string.trim()
       const challengeTitle = $(".challenge-title").text().trim()
       assert.equal(challengeTitle, expectedTitle)
 
       await awaitChallengeLoading()
 
-      var expectedescription = intl.t("model.challenges." + challengeId + ".description").string.trim().replace(/\s+/g, " ")
-      assert.dom("[data-test-challenge-description]").containsText(expectedescription)
+      assert.dom("[data-test-challenge-description]").matchesText(expectedescription)
 
-      var expectedClue = intl.t("model.challenges." + challengeId + ".clue").string.trim()
-      assert.dom("[data-test-challenge-clue]").containsText(expectedClue)
+      assert.dom("[data-test-challenge-clue]").matchesText(expectedClue)
     })
   }
 
-// Some second book's challenges:
-  testCanVisit(1)
-  testCanVisit(2)
-  testCanVisit(3)
+  // Second book challenge:
+  testCanVisit(1,
+    'El alien toca el botón',
+    'Ayudá a nuestro Alien a presionar el botón de su laboratorio. Pistas: mirá las primitivas disponibles. ¡Vas a tener que avanzar varias veces! 😉.',
+    'Los bloques te permiten formar secuencias de acciones para resolver los desafíos que te proponemos en Pilas Bloques.')
 
-// Conditional Alternative:
-  testCanVisit(13)
-  testCanVisit(14)
+  // Conditional Alternative:
+  // Because markdown is being used, the clue of this challenge uses the caracter '…', which is different from '...' (three dot characters).
+  testCanVisit(13,
+    'El mono y las bananas',
+    '¿Podés hacer que el mono avance al casillero de enfrente? Si hay una banana debe comérsela. Si no, es feliz con sólo llegar. Ejecutá el programa varias veces para asegurarte que siempre funciona. Pista: mirá la categoría \"Sensores\" y la categoría \"Alternativas\".',
+    'El bloque \"Si… Entonces\" ejecuta una secuencia de instrucciones solamente cuando la condición es verdadera. Esto se llama \"alternativa condicional\".')
 
-// Challenge with no clues:
-  testCanVisit(15)
+  // Challenge with no clues:
+  testCanVisit(15,
+    'Laberinto corto',
+    'Guiá al ratón para llegar a la meta. Para lograrlo, debe avanzar una casilla en la dirección que indica la flecha. Pista: mirá en la categoría \"Sensores\" qué preguntas podés hacer.',
+    '')
 
-// Some first book's challenges:
-  testCanVisit(230)
-  testCanVisit(201)
+  // First book challenge:
+  testCanVisit(230,
+    'Desafío 1',
+    '¿Puede la puma llegar al churrasco usando solo una vez el bloque mover?',
+    'Podés usar el nuevo bloque “Repetir”')
 
 })
