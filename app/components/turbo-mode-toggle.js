94d2e7f82bf5f680e2632170061e27712bd60270
Lucio Francioni <franciolucio@gmail.com>
2021-11-25 21:37:11 -0300

    Se agrega en modo turbo para que quede fijo

--- app/components/turbo-mode-toggle.js
+++ app/components/turbo-mode-toggle.js
@@ -1,26 +1,34 @@
 import Component from '@ember/component';
 import Ember from 'ember';
+import { inject as service } from '@ember/service';
 
 export default Component.extend({
+    tagName: 'div',
+    classNames: [],
     turboMode: false,
+    storage: service(),
 
+    didRender() {
+        this.set('turboMode', this.storage.getUseTurboMode());
+        if (this.turboMode) {
+            this.pilas.habilitarModoTurbo();
+        }
+    },
+    
     actions: {
         updateTurboMode() {
-
             if (this.turboMode) {
+                this.storage.toggleTurboMode();
                 this.pilas.deshabilitarModoTurbo();
-                this.set("turboMode", false);
             }
-
             else {
+                this.storage.toggleTurboMode()
                 this.pilas.habilitarModoTurbo();
-                this.set("turboMode", true);
             }
 
             if (!Ember.testing) {
                 this.set("needShowTurboModeIndicator", true);
             }
-
         },
     }
 });
