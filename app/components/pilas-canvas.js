eb9c4be9f1f2d02c0441c1aae77f757c289209ee
tfloxolodeiro <tfloxolodeiro@est.frba.utn.edu.ar>
2021-12-07 15:02:58 -0300

    Starting to refactor pilas-canvas

--- app/components/pilas-canvas.js
+++ app/components/pilas-canvas.js
@@ -1,4 +1,4 @@
-import { scheduleOnce } from '@ember/runloop';
+import { inject as service } from '@ember/service';
 import Component from '@ember/component';
 
 export default Component.extend({
@@ -6,64 +6,55 @@ export default Component.extend({
   classNameBindings: ['media.isMobile:media-mobile'],
   iframeElement: null,
   challenge: null,
-  pilas: null,       /* Se espera que este atributo se defina al
-                      * llamar al componente y es obligatorio. */
+  pilas: service(),
 
-  didRender() {
-    this.initElement()
+  didInsertElement() {
+    this.set("iframeElement", this.element.querySelector('#innerIframe'))
+    this.iframeElement.onload = () => { this.loadScene() }
+  },
+
+  didUpdate() {
+    this.pilas.evaluar('pilas.reiniciar();')
+    this.pilas.inicializarEscena(this.get('iframeElement'), this.sceneConstructor());
   },
 
   willDestroyElement() {
-    if (this.pilas) {
-      this.pilas.liberarRecursos();
-    }
+    this.pilas.liberarRecursos();
   },
 
   sceneConstructor() {
     return this.challenge && this.challenge.escena;
   },
 
-  initElement() {
+  loadScene() {
+    const iframeElement = this.get('iframeElement')
+    this.pilas.inicializarPilas(iframeElement, { width: 420, height: 480 }, this.challenge).
+      then(() => { this.initializeScene() });
 
-    let iframeElement = this.element.querySelector('#innerIframe');
+    // onLoad solo se utiliza dentro de la batería de tests. Este
+    // componente se tendría que usar mediante el servicio "pilas"
+    // en cualquier otro lugar.
+    if (this.onLoad)
+      this.onLoad({ iframeElement });
+  },
 
-    this.set("iframeElement", iframeElement);
+  initializeScene() {
+    if (this.sceneConstructor()) {
+      this.pilas.inicializarEscena(iframeElement, this.sceneConstructor());
+    } else {
+      console.warn("No especificó una escena para cargar en pilas-canvas.");
+    }
 
-    this.iframeElement.onload = () => {
-
-      if (this.pilas) {
-        this.pilas.inicializarPilas(iframeElement, { width: 420, height: 480 }, this.challenge).
-          then((pilas) => {
-
-            if (this.sceneConstructor()) {
-              this.pilas.inicializarEscena(iframeElement, this.sceneConstructor());
-            } else {
-              console.warn("No especificó una escena para cargar en pilas-canvas.");
-            }
-
-            /*
-             * Invoca a la acción "onReady" que envía el objeto pilas listo
-             * para ser utilizado.
-             *
-             */
-            if (this.onReady) {
-              this.onReady(pilas);
-            } else {
-              //console.warn("Se a iniciado el componente pilas-canvas sin referencia a la acción onLoad.");
-            }
-          });
-      } else {
-        console.warn("No has enviado el objeto pilas.");
-      }
-
-      // onLoad solo se utiliza dentro de la batería de tests. Este
-      // componente se tendría que usar mediante el servicio "pilas"
-      // en cualquier otro lugar.
-      if (this.onLoad)
-        this.onLoad({ iframeElement });
-
-    };
-
+    /*
+     * Invoca a la acción "onReady" que envía el objeto pilas listo
+     * para ser utilizado.
+     *
+     */
+    if (this.onReady) {
+      this.onReady(pilas);
+    } else {
+      //console.warn("Se a iniciado el componente pilas-canvas sin referencia a la acción onLoad.");
+    }
   },
   actions: {
     quitFullscreen() {
--- tests/integration/components/pilas-canvas-test.js
+++ tests/integration/components/pilas-canvas-test.js
@@ -9,13 +9,11 @@ module('Integration | Component | pilas canvas', function (hooks) {
   test('it renders', async function (assert) {
     // Set any properties with this.set('myProperty', 'value');
     // Handle any actions with this.on('myAction', function(val) { ... });
-
-    this.set('pilas', this.owner.lookup('service:pilas'));
     this.set('cargando', true);
     this.set('escena', null);
     this.set('onReady', () => { });
 
-    await render(hbs`<PilasCanvas @pilas={{pilas}} @onReady={{onReady}} @escena={{escena}} @cargando={{cargando}} />`);
+    await render(hbs`<PilasCanvas @onReady={{onReady}} @escena={{escena}} @cargando={{cargando}} />`);
 
     assert.dom().hasText('');
 
