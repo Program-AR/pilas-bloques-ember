56989822efb05b6908921f35e3a8168998127c2b
tfloxolodeiro <tfloxolodeiro@est.frba.utn.edu.ar>
2022-03-16 14:24:07 -0300

    First language change tests

--- tests/integration/components/language-menu-test.js
+++ tests/integration/components/language-menu-test.js
@@ -1,26 +1,14 @@
-import { module, skip } from 'qunit';
+import { module, test } from 'qunit';
 import { setupRenderingTest } from 'ember-qunit';
-import { render } from '@ember/test-helpers';
-import { hbs } from 'ember-cli-htmlbars';
+import { setUpTestLocale } from '../../helpers/utils';
 
-module('Integration | Component | language-menu', function(hooks) {
+module('Integration | Component | language-menu', function (hooks) {
   setupRenderingTest(hooks);
+  setUpTestLocale(hooks)
 
-  skip('it renders', async function(assert) {
-    // Set any properties with this.set('myProperty', 'value');
-    // Handle any actions with this.set('myAction', function(val) { ... });
-
-    await render(hbs`<LanguageMenu />`);
-
-    assert.equal(this.element.textContent.trim(), '');
-
-    // Template block usage:
-    await render(hbs`
-      <LanguageMenu>
-        template block text
-      </LanguageMenu>
-    `);
-
-    assert.equal(this.element.textContent.trim(), 'template block text');
+  test('Should change intl locale on language selection', async function (assert) {
+    const intl = this.owner.lookup('service:intl')
+    this.owner.factoryFor('component:language-menu').create().send('setLanguage', 'en-us')
+    assert.equal(intl.locale, 'en-us')
   });
 });
