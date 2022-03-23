6563b5d17b453c97301396ae5cdc277d32740ca8
Diana Lopez Alvas <dlopezalvas@gmail.com>
2022-03-23 11:12:14 -0300

    disble languages

--- app/components/language-menu.js
+++ app/components/language-menu.js
@@ -5,9 +5,14 @@ import { computed } from '@ember/object';
 export default Component.extend({
 
     intl: service(),
+    disabledLanguages: ['pt'], //disables portuguese
     localeCodes: computed("intl", function () {
         return this.get('intl').get('locales')
     }),
+
+    shouldBeEnabled(localeCode){
+        return !this.disabledLanguages.includes(localeCode)
+    },
 
     languageName(localeCode) {
         return this.get('intl').t('localeName', { locale: localeCode })
