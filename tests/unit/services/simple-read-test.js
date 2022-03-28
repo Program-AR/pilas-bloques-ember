fe988aaf432dc3d633733c96697327f95bc862b1
ezequielPereyra <31800576+ezequielPereyra@users.noreply.github.com>
2022-03-28 11:57:59 -0300

    Test: SimpleRead:shouldShowSimpleRead

--- tests/unit/services/simple-read-test.js
+++ tests/unit/services/simple-read-test.js
@@ -2,11 +2,29 @@ import { module, test } from 'qunit';
 import { setupTest } from 'ember-qunit';
 
 module('Unit | Service | simple-read', function(hooks) {
+
+  let service
+
+  const storageMock = {
+    getUseSimpleRead() {
+      return true
+    }
+  }
+
   setupTest(hooks);
 
-  // TODO: Replace this with your real tests.
-  test('it exists', function(assert) {
-    let service = this.owner.lookup('service:simple-read');
-    assert.ok(service);
-  });
+  hooks.beforeEach(function() {
+    service = this.owner.lookup('service:simple-read')
+    service.storage = storageMock
+  })
+
+  test('If default value is true, it should show simple read', function (assert) {
+    assert.ok(service.shouldShowSimpleRead(true))
+  })
+
+  test('If default value is false, it should use the storage value', function (assert) {
+    assert.ok(service.shouldShowSimpleRead(false))
+  })
+
+
 });
