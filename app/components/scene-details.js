62b4b7240bbb4ac4ffb7ea80365889dc644e421d
Alfredo Sanzo <alfredo.sanzo@gmail.com>
2022-03-28 18:38:49 -0300

    Refactor setTab

--- app/components/scene-details.js
+++ app/components/scene-details.js
@@ -8,39 +8,12 @@ export default Component.extend({
         return this.model.challengeCover || `imagenes/desafios/${this.model.nombreImagen}`
     }),
 
-
-    didRender() {
-
-        // Show the default tab element.
-        if (!this.get('initialized')) {
-            this.send("setTab", "statement");
-        }
-
-    },
-
     actions: {
 
-        setTab(tab) {
-            var i, tabcontent, tablinks;
-            tabcontent = document.getElementsByClassName("tabcontent");
-
-            if (tabcontent.length > 1) {
-
-                this.set('initialized', true);
-
-                for (i = 0; i < tabcontent.length; i++) {
-                    tabcontent[i].style.display = "none";
-                }
-
-                tablinks = document.getElementsByClassName("tablinks");
-
-                for (i = 0; i < tablinks.length; i++) {
-                    tablinks[i].className = tablinks[i].className.replace(" active", "");
-                }
-
-                document.getElementById(tab).style.display = "block";
-                document.getElementById(tab).style.width = "100%";
-            }
+        setTab(tabId) {
+            Array.from(document.getElementsByClassName("tabcontent"))
+                .forEach(e => e.classList.remove("active"))
+            document.getElementById(tabId).classList.add("active")
         },
 
     },
