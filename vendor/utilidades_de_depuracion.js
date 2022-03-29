f15b609d16f1c3c6bce24d5f53fe6461ca85040a
ezequielPereyra <31800576+ezequielPereyra@users.noreply.github.com>
2022-03-29 10:49:37 -0300

    css aplicar-modo-lectura-simple rename

--- vendor/utilidades_de_depuracion.js
+++ vendor/utilidades_de_depuracion.js
@@ -52,7 +52,7 @@ var pbUtils = {
     }
 
     aleph.querySelectorAll(".blocklyPathLight").forEach(function (elem) { elem.setAttribute("fill", "none") });
-    if (document.getElementsByClassName("aplicar-modo-lectura-simple").length > 0) {
+    if (document.getElementsByClassName("simple-read").length > 0) {
       aleph.querySelectorAll(".blocklyText").forEach(function (elem) { elem.textContent = elem.textContent.toUpperCase() });
     }
 
