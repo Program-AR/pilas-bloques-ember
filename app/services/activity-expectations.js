91488f4e85a2528871bd7cc88ea9e4f17ad86883
Diana Lopez Alvas <dlopezalvas@gmail.com>
2022-04-07 19:37:55 -0300

    adding activity expectations

--- app/services/activity-expectations.js
+++ app/services/activity-expectations.js
@@ -1,6 +1,6 @@
 import Service from '@ember/service'
 import { entryPointType } from '../utils/blocks'
-import { allProceduresShould, declaresAnyProcedure, doSomething, isUsed, isUsedFromMain, multiExpect, notTooLong, usesConditionalAlternative } from '../utils/expectations'
+import { allProceduresShould, declaresAnyProcedure, doSomething, isUsed, isUsedFromMain, multiExpect, notTooLong, usesConditionalAlternative, usesConditionalRepetition } from '../utils/expectations'
 
 const activityExpectations = {
   // La gran aventura del mar encantado
@@ -21,18 +21,39 @@ const activityExpectations = {
   // La elección del mono
   14: usesConditionalAlternative,
 
-  //Laberinto corto 
+  // Laberinto corto 
   15: usesConditionalAlternative,
-  
-  //Solo en ciertas ocasiones
 
-  //Desafio 1 
+  // Súper Tito 1
+  19: usesConditionalRepetition,
+  
+  // Súper Tito 2
+  20: usesConditionalRepetition,
+  
+  // Laberinto con queso
+  21: usesConditionalRepetition,
+
+  //El Detective chaparro
+  22: usesConditionalRepetition,
+  
+  //Fútbol para robots
+  23: usesConditionalRepetition,
+  
+  //Prendiendo las compus
+  24: usesConditionalRepetition,
+  
+  //El mono que sabe contar
+  25: usesConditionalRepetition,
+  
+  // Solo en ciertas ocasiones
+
+  // Desafio 1 
   242: usesConditionalAlternative,
 
-  //Desafio 2 
+  // Desafio 2 
   243: usesConditionalAlternative,
   
-  //Desafio 3 
+  // Desafio 3 
   244: usesConditionalAlternative
 }
 
