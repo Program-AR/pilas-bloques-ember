34350a40256fb292e8c0da9e17c065197235fdc8
ezequielPereyra <31800576+ezequielPereyra@users.noreply.github.com>
2022-04-07 15:40:01 -0300

    Added within

--- app/utils/expectations.js
+++ app/utils/expectations.js
@@ -12,7 +12,7 @@ export const multiExpect = (...expectations) => (element) =>
 
 // DECLARATION EXPECTATIONS
 export const doSomething = (declaration) =>
-  newExpectation(`within ${toEDLString(declaration)} ${countWithRecursiveCalling(declaration)} >= 1`, 'do_something', { declaration })
+  newExpectation(`${countWithRecursiveCalling(declaration)} >= 1`, 'do_something', { declaration })
 
 export const isUsed = (declaration) =>
   newExpectation(`calls ${toEDLString(declaration)}`, 'is_used', { declaration })
@@ -21,7 +21,7 @@ export const isUsedFromMain = (declaration) =>
   newExpectation(`through ${toEDLString(entryPointType)} calls ${toEDLString(declaration)}`, 'is_used_from_main', { declaration })
 
 export const notTooLong = (limit = 7) => (declaration) =>
-  newExpectation(`within ${toEDLString(declaration)} ${countWithRecursiveCalling(declaration)} <= ${limit - 1}`, 'too_long', { declaration, limit })
+  newExpectation(`${countWithRecursiveCalling(declaration)} <= ${limit - 1}`, 'too_long', { declaration, limit })
 
 export const doesNotUseRecursion = (declaration) =>
   newExpectation(`not (through ${toEDLString(declaration)} calls ${toEDLString(declaration)})`, doesNotUseRecursionId, { declaration })
@@ -31,7 +31,7 @@ const newExpectation = (expect, id, opts = {}) =>
   `expectation "${stringify(id, opts)}": ${expect};`
 
 const countWithRecursiveCalling = (declaration) =>
-  `count(calls) + count(calls ${toEDLString(declaration)})`
+  `within ${toEDLString(declaration)} count(calls) + count(calls ${toEDLString(declaration)})`
 
 export const stringify = (id, opts) =>
   `${expectationName(id)}|${Object.entries(opts).map(([key, value]) => `${key}=${value}`).join(';')}`
