bd6419ab9ffa1a233119afa5b5c7e0c50af56f5a
Diana Lopez Alvas <dlopezalvas@gmail.com>
2022-05-09 17:15:44 -0300

    adding some tests

--- tests/integration/components/multiple-scenarios-arrow-test.js
+++ tests/integration/components/multiple-scenarios-arrow-test.js
@@ -3,24 +3,39 @@ import { setupRenderingTest } from 'ember-qunit';
 import { render } from '@ember/test-helpers';
 import { hbs } from 'ember-cli-htmlbars';
 
-module('Integration | Component | multiple-scenarios-arrow', function(hooks) {
+module('Integration | Component | multiple-scenarios-arrow', function (hooks) {
   setupRenderingTest(hooks);
   const arrow = hbs`{{multiple-scenarios-arrow shouldShowMultipleScenarioHelp=shouldShowMultipleScenarioHelp shouldUseFloatingMode=shouldUseFloatingMode}}`
 
-  hooks.beforeEach(function() {
-    this.set('shouldUseFloatingMode', true);
-  });
-
-  test('Multiple Scenarios arrow shows in challenges with shouldShowMultipleScenarioHelp', async function (assert) {
-    this.set('shouldShowMultipleScenarioHelp', true);
-    await render(arrow);
+  test('It shows in challenges with shouldShowMultipleScenarioHelp', async function (assert) {
+    await renderArrow(this, true);
     assert.dom('div.multiple-scenarios-arrow').exists()
   });
-
-  test('Multiple Scenarios arrow does NOT show for challenges with no shouldShowMultipleScenarioHelp', async function (assert) {
-    this.set('shouldShowMultipleScenarioHelp', false);
+  
+  test('By default, it does NOT show', async function (assert) {
     await render(arrow);
     assert.dom('div.multiple-scenarios-arrow').doesNotExist()
   });
+
+  test('It does NOT show for challenges with no shouldShowMultipleScenarioHelp', async function (assert) {
+    await renderArrow(this, false);
+    assert.dom('div.multiple-scenarios-arrow').doesNotExist()
+  });
+
+  test('It shows in full screen mode using floating mode', async function (assert) {
+    await renderArrow(this, true, true);
+    assert.dom('div.full-screen-arrow').exists()
+  });
+
+  test('It shows in normal screen mode when NOT using floating mode', async function (assert) {
+    await renderArrow(this, true, false);
+    assert.dom('div.normal-screen-arrow').exists()
+  });
+
+  async function renderArrow(testContext, shouldshouldShowMultipleScenarioHelp, shouldUseFloatingMode = true) {
+    testContext.set('shouldShowMultipleScenarioHelp', shouldshouldShowMultipleScenarioHelp);
+    testContext.set('shouldUseFloatingMode', shouldUseFloatingMode);
+    await render(arrow);
+  }
   
 });
