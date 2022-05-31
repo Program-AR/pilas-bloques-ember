d797de3e6c3f67e63c403b3ba68fd27e590d7517
Diana Lopez Alvas <dlopezalvas@gmail.com>
2022-05-31 14:31:31 -0300

    change version function

--- scripts/change-pilas-versions.sh
+++ scripts/change-pilas-versions.sh
@@ -1,8 +1,15 @@
 echo Getting Pilasweb and pilas-bloques-exercises versions
-PILAS_BLOQUES_EXERCISES_VERSION="$(awk '$1 ~ /"pilas-bloques-exercises"/ {print $2}' ../package.json | sed 's/[",^]//g')"
 
-PILASWEB_VERSION=$(awk '$1 ~ /"pilasweb"/ {print $2}' ../package.json | sed 's/[",^]//g')
+PILAS_BLOQUES_EXERCISES_VERSION=$(awk '$1 ~ /"pilas-bloques-exercises"/ {print $2}' package.json)
+
+PILASWEB_VERSION=$(awk '$1 ~ /"pilasweb"/ {print $2}' package.json)
+
+change_version(){
+    local version=$(echo "$2" | sed 's/[",^]//g')
+    sed -i "s/${1}.js?v=[0-9].[0-9].[0-9]/${1}.js?v=${version}/g" public/pilas.html
+}
 
 echo Changing pilas.html versions 
-sed "s/pilasweb.js?v=[0-9].[0-9].[0-9]/pilasweb.js?v=${PILASWEB_VERSION}/g" ../public/pilas.html
-sed "s/pilas-bloques-exercises.js?v=[0-9].[0-9].[0-9]/pilas-bloques-exercises.js?v=${PILAS_BLOQUES_EXERCISES_VERSION}/g" ../public/pilas.html
+
+change_version "pilas-bloques-exercises" $PILAS_BLOQUES_EXERCISES_VERSION
+change_version "pilasweb" $PILASWEB_VERSION
