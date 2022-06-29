fa81cdfb7499fa022f7922a533e0687c493bb773
Diana Lopez Alvas <dlopezalvas@gmail.com>
2022-06-29 18:36:37 -0300

    fix pilas-blockly tests

--- tests/helpers/utils.js
+++ tests/helpers/utils.js
@@ -88,6 +88,12 @@ export function resetFetch() {
     mockApi(`solutions`, 200)
     mockApi(`ping`, 200)
     mockApi(`error`, { throws: 'ERROR' })
+    fetchMock.mock(`https://api64.ipify.org?format=json`, userIpResponse(""))
+}
+
+function userIpResponse(_ip) {
+    const json = JSON.stringify({ip: _ip})
+    return new Response(json)
 }
 
 export function mockApi(path, response, options) {
