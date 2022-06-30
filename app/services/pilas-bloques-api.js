b808298119779b12c14e5b1d18da92b78f548f51
Diana Lopez Alvas <dlopezalvas@gmail.com>
2022-06-30 18:01:49 -0300

    really really fixing tests

--- app/services/pilas-bloques-api.js
+++ app/services/pilas-bloques-api.js
@@ -92,11 +92,6 @@ export default Service.extend({
 
   async _send(method, resource, body, critical = true) {
     const user = this.getUser()
-    if (body) {
-      body.context = await this.pilasBloquesAnalytics.context()
-      body.timestamp = new Date()
-    } //TODO: Move user to Analytics / use id instead of nickname / rename Analytics to session related approach
-
     const url = `${baseURL}/${resource}`
     const flag = `loading.${resource.split('?')[0].replace('/', '-')}`
     const headers = {
@@ -104,7 +99,12 @@ export default Service.extend({
       'Authorization': user ? `Bearer ${user.token}` : null
     }
 
-    this.set(flag, true)
+    this.set(flag, true) 
+
+    if (body) {
+      body.context = await this.pilasBloquesAnalytics.context()
+      body.timestamp = new Date()
+    } //TODO: Move user to Analytics / use id instead of nickname / rename Analytics to session related approach
 
     return this._doFetch(url, {
       method,
