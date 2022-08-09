6f1a9be6063d305d68e48849d809ffee639a9477
Diana Lopez Alvas <dlopezalvas@gmail.com>
2022-08-09 13:27:17 -0300

    fixing test

--- tests/acceptance/desafios-test.js
+++ tests/acceptance/desafios-test.js
@@ -27,7 +27,7 @@ module('Acceptance | desafios', function (hooks) {
   });
 
   test('La cantidad de desafíos que se muestra en la pagina 2 es correcta', async function (assert) {
-    let cantidadDesafiosEsperada = 46;
+    let cantidadDesafiosEsperada = 47;
     await page2.visit();
     assert.equal(page.cantidadDeDesafiosDisponibles, cantidadDesafiosEsperada, `Hay exactamente ${cantidadDesafiosEsperada} desafios habilitados para utilizar.`);
   });
