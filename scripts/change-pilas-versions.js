fdf6c8d56c09164bc4a047edfae63be6cca5baff
Diana Lopez Alvas <dlopezalvas@gmail.com>
2022-09-01 17:49:11 -0300

    using new scripts

--- scripts/change-pilas-versions.js
+++ scripts/change-pilas-versions.js
@@ -3,12 +3,11 @@ const pilasBloquesExercises = require('pilas-bloques-exercises/package.json');
 const shell = require('shelljs');
 
 function changeVersion(dependency, version){
+  console.log(`Changing ${dependency} version in pilas.html`)
   const regex = new RegExp(`${dependency}.js\\?v=[0-9]*.[0-9]*.[0-9]*`)
   const replacement = `${dependency}.js?v=${version}`
   shell.sed('-i', regex, replacement, 'public/pilas.html')
 }
 
-console.log('Changing pilasweb in pilas.html')
 changeVersion('pilasweb', pilasweb.version)
-console.log('Changing pilas-bloques-exercises in pilas.html')
 changeVersion('pilas-bloques-exercises', pilasBloquesExercises.version)
--- scripts/change-sass-division.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-declare -a FoldersToMigrate=("font-awesome/scss" "angular-material-styles/src/core" "angular-material-styles/src/components" )
-
-for folder in "${FoldersToMigrate[@]}"; do
-   echo "Changing deprecated division - ${folder}";
-   sass-migrator division node_modules/$folder/**/*.scss node_modules/$folder/*.scss;
-done
-
