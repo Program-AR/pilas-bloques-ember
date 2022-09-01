d1d1676d7ac4468545308920b2d4a4ea55cb6480
Diana Lopez Alvas <dlopezalvas@gmail.com>
2022-09-01 17:38:42 -0300

    remove mulang required + sass migrator scrips to js

---
+++ scripts/change-sass-division.js
@@ -0,0 +1,11 @@
+const { forEach } = require('ramda')
+const shell = require('shelljs')
+
+let foldersToMigrate=["font-awesome/scss", "angular-material-styles/src/core","angular-material-styles/src/components"]
+
+foldersToMigrate.forEach( folder => {
+   console.log(`Changing deprecated division - ${folder}`)
+   shell.exec(`sass-migrator division node_modules/${folder}/**/*.scss node_modules/${folder}/*.scss`)
+})
+
+
