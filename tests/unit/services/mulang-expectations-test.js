99899ee27e9242d53552c168615ca3f13e43503a
Diana Lopez Alvas <dlopezalvas@gmail.com>
2022-10-03 21:36:00 -0300

    fix jshint

--- tests/unit/services/mulang-expectations-test.js
+++ tests/unit/services/mulang-expectations-test.js
@@ -324,7 +324,7 @@ module('Unit | Service | Mulang | Expectations', function (hooks) {
       const fullId = mulang
         .astCode(rawSequence([]))
         .customExpect(edl)
-        .map(([name]) => parseExpect(atob(name))) // jshint ignore:line
+        .map(([name]) => parseExpect(window.atob(name)))
 
       assert.deepEqual(fullId, expectedIds)
     })
