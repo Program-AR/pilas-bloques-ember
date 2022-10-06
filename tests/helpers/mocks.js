234769ea3eb41087820d0928864e70033082134a
tfloxolodeiro <tfloxolodeiro@est.frba.utn.edu.ar>
2022-10-06 15:38:23 -0300

    Fixing tests again (again)

--- tests/helpers/mocks.js
+++ tests/helpers/mocks.js
@@ -29,7 +29,8 @@ export const interpreterFactoryMock = Service.extend({
 export const challengeExpectationsMock = Service.extend({
     expectations: () => '',
     expectationFor(/* challenge */) { return this.expectations },
-    totalScoreOf(/* challenge */) { return 0 }
+    totalScoreOf(/* challenge */) { return 0 },
+    allExpectIdsIn(/* challenge */) { return [] }
 });
 
 export const createActividadMock = (fields) => EmberObject.extend({
