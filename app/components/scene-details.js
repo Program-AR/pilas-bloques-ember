151189b94c5aa47a640bee5816869372b0839aac
Alfredo Sanzo <alfredo.sanzo@gmail.com>
2022-10-06 20:12:11 -0300

    refactor exercise cover

--- app/components/scene-details.js
+++ app/components/scene-details.js
@@ -4,10 +4,6 @@ import { computed } from '@ember/object';
 export default Component.extend({
 
     initialized: false,
-    exerciseCover: computed('model', function () {
-        return this.model.challengeCover || `imagenes/desafios/${this.model.nombreImagen}`
-    }),
-
     actions: {
 
         setTab(tabId) {
