31e26454fba9a14fd556e91cf555800db843e642
tfloxolodeiro <tfloxolodeiro@est.frba.utn.edu.ar>
2022-10-06 14:22:23 -0300

    total score tests

--- tests/unit/services/expects-scoring-test.js
+++ tests/unit/services/expects-scoring-test.js
@@ -2,7 +2,7 @@ import { module, test } from 'qunit';
 import { setupTest } from 'ember-qunit';
 import { setUpTestLocale } from '../../helpers/utils';
 import { createComponentMock, expectationsConfigMock } from '../../helpers/mocks';
-import { mainTooLongID, doesNotNestControlStructuresId, simpleRepetitionId } from '../../../utils/expectations'
+import { mainTooLongID, doesNotNestControlStructuresId, simpleRepetitionId, doSomethingId, tooLongId, nameWasChangedId } from '../../../utils/expectations'
 
 const solutionPassMock = {
   id: 'solution_works',
@@ -83,4 +83,33 @@ module('Unit | Service | expects-scoring', function (hooks) {
     assert.propEqual(results, [solutionPassMock, e2, e3])
   })
 
+  const assertFloatEqual = (assert, n1, n2) => { assert.equal(Math.floor(n1), Math.floor(n2)) }
+
+  test('Total score when using all expects of configuration', function (assert) {
+    const e1 = expectation(doSomethingId, true)
+    const e2 = expectation(tooLongId, false)
+    const e3 = expectation(mainTooLongID, true)
+    const e4 = expectation(nameWasChangedId, true)
+    const e5 = expectation(doesNotNestControlStructuresId, false)
+    const expects = [e1, e2, e3, e4, e5]
+    //The "+1" here refers to the solution passed expectation that is added to the expects.
+    const passingExpects = 3 + 1
+    const totalExpects = 5 + 1
+
+
+    assertFloatEqual(assert, expectsScoring.totalScore(expects, challengeMock), 100 * (passingExpects / totalExpects))
+  })
+
+  test('Calculating total score when not using all expects of the challenge configuration should add the unused expects as passing expects', function (assert) {
+    const e1 = expectation(doSomethingId, true)
+    const e2 = expectation(tooLongId, false)
+    const e3 = expectation(mainTooLongID, false)
+    const e4 = expectation(nameWasChangedId, false)
+    //Does not nest control structures from the decomposition configuration is not used
+    const expects = [e1, e2, e3, e4]
+    const passingExpects = 1 + 2 //The solution passed expectation and the does not nest control structures expectation are added
+    const totalExpects = 5 + 1 //The solution passed expectation is added
+
+    assertFloatEqual(assert, expectsScoring.totalScore(expects, challengeMock), 100 * (passingExpects / totalExpects))
+  })
 });
