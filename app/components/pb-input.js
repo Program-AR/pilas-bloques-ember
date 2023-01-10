91e80633fd348336241f2d0802b2a6e1cde45b54
ezequielPereyra <31800576+ezequielPereyra@users.noreply.github.com>
2023-01-09 22:22:24 -0300

    Changing required field default error message

--- app/components/pb-input.js
+++ app/components/pb-input.js
@@ -2,8 +2,11 @@ import Component from '@ember/component';
 import ChildMixin from '../mixins/child-mixin';
 import FocusableMixin from '../mixins/focusable-mixin';
 import { computed } from '@ember/object'
+import { inject as service } from '@ember/service';
 
 export default Component.extend(FocusableMixin, ChildMixin, {
+
+    intl: service(),
 
     isInvalid: computed('error', function (){
         return this.error
@@ -15,5 +18,15 @@ export default Component.extend(FocusableMixin, ChildMixin, {
     stringLabel: computed('label', function () {
         return this.label.toString()
     }),
+
+    actions : {
+        onInvalid(e) {
+            e.target.setCustomValidity(this.intl.t("templates.pb-input.requiredField"))
+        },
+
+        onInput(e) {
+            e.target.setCustomValidity('')
+        }
+    }
 
 });
