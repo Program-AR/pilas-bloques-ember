7c38f060fe234d86b72d3c83c33d38e06c143b31
Tomas Floxo <tomasfloxolodeiro@gmail.com>
2023-01-16 18:33:20 -0300

    trying tests CI with hash

--- config/environment.js
+++ config/environment.js
@@ -66,7 +66,7 @@ module.exports = function (environment) {
   if (environment === 'test') {
     // Testem prefers this...
     ENV.rootURL = '/'
-    ENV.locationType = 'none'
+    ENV.locationType = 'hash'
     ENV.APP.autoboot = false // ember-qunit needs it to be false.
 
     // keep test console output quieter
