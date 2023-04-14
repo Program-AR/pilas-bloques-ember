1b04859411f04a321b7b2435399e3b8f6fd0c1b4
Daniel Ferro <danieloscarferro@gmail.com>
2023-04-14 11:38:07 -0300

    Firefox x Chrome en testem

--- testem.js
+++ testem.js
@@ -2,23 +2,8 @@
 module.exports = {
   test_page: 'tests/index.html?hidepassed&nocontainer',
   disable_watching: true,
-  launch_in_ci: [
-  'Chrome'
-  ],
-  launch_in_dev: [
-    'Chrome'
-  ],
-  browser_args: {
-    Chrome: {
-      mode: 'ci',
-      args: [
-        '--disable-gpu',
-        '--headless',
-        '--remote-debugging-port=0',
-        '--window-size=1440,900'
-      ]
-    }
-  }
-  };
+  launch_in_ci: ['Firefox'],
+  launch_in_dev: ['Firefox']
+};
 
 
