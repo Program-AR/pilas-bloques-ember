3569d5fe0b519d7a599d9754c5af2215c6aa8e88
Daniel Ferro <danieloscarferro@gmail.com>
2023-04-14 15:36:21 -0300

    Chrome in testem

--- testem.js
+++ testem.js
@@ -2,8 +2,23 @@
 module.exports = {
   test_page: 'tests/index.html?hidepassed&nocontainer',
   disable_watching: true,
-  launch_in_ci: ['Firefox'],
-  launch_in_dev: ['Firefox']
-};
+  launch_in_ci: [
+    'Chrome'
+  ],
+  launch_in_dev: [
+    'Chrome'
+  ],
+  browser_args: {
+    Chrome: {
+      mode: 'ci',
+      args: [
+        '--disable-gpu',
+        '--headless',
+        '--remote-debugging-port=0',
+        '--window-size=1440,900'
+      ]
+    }
+  }
+};  
 
 
