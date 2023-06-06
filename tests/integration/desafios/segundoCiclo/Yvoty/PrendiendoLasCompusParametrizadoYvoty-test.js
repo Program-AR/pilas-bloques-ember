043b92e637a0da92fb6da4be735225658b598219
Diana López Alvas <dlopezalvas@gmail.com>
2023-06-06 18:54:16 -0300

    fixing tests ?

--- tests/integration/desafios/segundoCiclo/Yvoty/PrendiendoLasCompusParametrizadoYvoty-test.js
+++ tests/integration/desafios/segundoCiclo/Yvoty/PrendiendoLasCompusParametrizadoYvoty-test.js
@@ -5,87 +5,99 @@ const nombre = 'PrendiendoLasCompusParametrizadoYvoty';
 moduloActividad(nombre, () => {
 
 	actividadTest(nombre, {
-		solucion: `
-		<xml xmlns="http://www.w3.org/1999/xhtml">
-		  <block type="al_empezar_a_ejecutar" id="1" deletable="false" movable="false" editable="false" x="0" y="0">
-		    <statement name="program">
-		      <block type="procedures_callnoreturn" id="46">
-		        <mutation name="Prender compus hacia">
-		          <arg name="direccion"></arg>
-		        </mutation>
-		        <value name="ARG0">
-		          <block type="ParaLaDerecha" id="61"></block>
-		        </value>
-		        <next>
-		          <block type="procedures_callnoreturn" id="64">
-		            <mutation name="Prender compus hacia">
-		              <arg name="direccion"></arg>
-		            </mutation>
-		            <value name="ARG0">
-		              <block type="ParaAbajo" id="72"></block>
-		            </value>
-		            <next>
-		              <block type="procedures_callnoreturn" id="77">
-		                <mutation name="Prender compus hacia">
-		                  <arg name="direccion"></arg>
-		                </mutation>
-		                <value name="ARG0">
-		                  <block type="ParaLaIzquierda" id="83"></block>
-		                </value>
-		                <next>
-		                  <block type="procedures_callnoreturn" id="92">
-		                    <mutation name="Prender compus hacia">
-		                      <arg name="direccion"></arg>
-		                    </mutation>
-		                    <value name="ARG0">
-		                      <block type="ParaArriba" id="89"></block>
-		                    </value>
-		                  </block>
-		                </next>
-		              </block>
-		            </next>
-		          </block>
-		        </next>
-		      </block>
-		    </statement>
-		  </block>
-		  <block type="procedures_defnoreturn" id="18" x="7" y="207">
-		    <mutation>
-		      <arg name="direccion"></arg>
-		    </mutation>
-		    <field name="NAME">Prender compus hacia</field>
-		    <comment pinned="false" h="80" w="160">Describe esta función...</comment>
-		    <statement name="STACK">
-		      <block type="MoverA" id="95">
-		        <value name="direccion">
-		          <block type="param_get" id="99">
-		            <field name="VAR">direccion</field>
-		          </block>
-		        </value>
-		        <next>
-		          <block type="hasta" id="29">
-		            <value name="condition">
-		              <block type="EstoyEnEsquina" id="31"></block>
-		            </value>
-		            <statement name="block">
-		              <block type="PrenderComputadora" id="41">
-		                <next>
-		                  <block type="MoverA" id="34">
-		                    <value name="direccion">
-		                      <block type="param_get" id="38">
-		                        <field name="VAR">direccion</field>
-		                      </block>
-		                    </value>
-		                  </block>
-		                </next>
-		              </block>
-		            </statement>
-		          </block>
-		        </next>
-		      </block>
-		    </statement>
-		  </block>
-		</xml>
+		solucion: `<xml xmlns="http://www.w3.org/1999/xhtml">
+		<variables>
+		  <variable type="" id="i0)F?:LYp*zutb,AI.@E">direccion</variable>
+		</variables>
+		<block type="al_empezar_a_ejecutar" id="1" deletable="false" movable="false" editable="false" x="0" y="0">
+		  <statement name="program">
+			<shadow type="required_statement" id="C3M/%+fO0x9_T47!*H7-"></shadow>
+			<block type="procedures_callnoreturn" id="46">
+			  <mutation name="Prender compus hacia">
+				<arg name="direccion"></arg>
+			  </mutation>
+			  <value name="ARG0">
+				<shadow xmlns="" type="required_value"/>
+				<block type="ParaLaDerecha" id="61"></block>
+			  </value>
+			  <next>
+				<block type="procedures_callnoreturn" id="64">
+				  <mutation name="Prender compus hacia">
+					<arg name="direccion"></arg>
+				  </mutation>
+				  <value name="ARG0">
+					<shadow xmlns="" type="required_value"/>
+					<block type="ParaAbajo" id="72"></block>
+				  </value>
+				  <next>
+					<block type="procedures_callnoreturn" id="77">
+					  <mutation name="Prender compus hacia">
+						<arg name="direccion"></arg>
+					  </mutation>
+					  <value name="ARG0">
+						<shadow xmlns="" type="required_value"/>
+						<block type="ParaLaIzquierda" id="83"></block>
+					  </value>
+					  <next>
+						<block type="procedures_callnoreturn" id="92">
+						  <mutation name="Prender compus hacia">
+							<arg name="direccion"></arg>
+						  </mutation>
+						  <value name="ARG0">
+							<shadow xmlns="" type="required_value"/>
+							<block type="ParaArriba" id="89"></block>
+						  </value>
+						</block>
+					  </next>
+					</block>
+				  </next>
+				</block>
+			  </next>
+			</block>
+		  </statement>
+		</block>
+		<block type="procedures_defnoreturn" id="18" x="7" y="207">
+		  <mutation>
+			<arg name="direccion"></arg>
+		  </mutation>
+		  <field name="NAME">Prender compus hacia</field>
+		  <field name="ARG0">direccion</field>
+		  <comment pinned="true" h="80" w="160">Describe esta función...</comment>
+		  <statement name="STACK">
+			<block type="MoverA" id="95">
+			  <value name="direccion">
+				<shadow type="required_value" id="DJGZT7CJms.URyD+_|Z\`"></shadow>
+				<block type="variables_get" id="/][:vGe_g7k=RqDI-cdO">
+				  <mutation var="direccion" parent="18"></mutation>
+				</block>
+			  </value>
+			  <next>
+				<block type="hasta" id="29">
+				  <value name="condition">
+					<shadow type="required_value" id="LJWbvl!?gaTI4@@ymkZK"></shadow>
+					<block type="EstoyEnEsquina" id="31"></block>
+				  </value>
+				  <statement name="block">
+					<shadow type="required_statement" id="-.Je5-:.=G1ADwqV``pA"></shadow>
+					<block type="PrenderComputadora" id="41">
+					  <next>
+						<block type="MoverA" id="34">
+						  <value name="direccion">
+							<shadow type="required_value" id="q_!(oY|Q\`Qj*#-#-AUC5"></shadow>
+							<block type="variables_get" id="*u9QTaxAdQxHEGsDuN|N">
+							  <mutation var="direccion" parent="18"></mutation>
+							</block>
+						  </value>
+						</block>
+					  </next>
+					</block>
+				  </statement>
+				</block>
+			  </next>
+			</block>
+		  </statement>
+		</block>
+	  </xml> 
 	`
 	});
 
