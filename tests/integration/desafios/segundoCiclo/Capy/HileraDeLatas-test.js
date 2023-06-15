8ebc838d120054cda41d071571eac4d7bb4187b9
Alfredo Sanzo <alfredo.sanzo@gmail.com>
2023-06-15 19:46:01 -0300

    Fixing tests

--- tests/integration/desafios/segundoCiclo/Capy/HileraDeLatas-test.js
+++ tests/integration/desafios/segundoCiclo/Capy/HileraDeLatas-test.js
@@ -11,12 +11,12 @@ moduloActividad(nombre, () => {
 	actividadTest(nombre, {
 		descripcionAdicional: 'Da error al querer avanzar hacia una casilla con obstaculos',
 		solucion: '<xml xmlns=\"http://www.w3.org/1999/xhtml\"><variables></variables><block type=\"al_empezar_a_ejecutar\" id=\"NM_JO$8pGwA`|zV5gLT4\" deletable=\"false\" movable=\"false\" editable=\"false\" x=\"15\" y=\"15\"><statement name=\"program\"><shadow type=\"required_statement\" id=\"?,iVo5gL9O{y48Joxs]Z\"></shadow><block type=\"MoverACasillaAbajo\" id=\"u2Icda]N(xCSyjOa7Q:I\"></block></statement></block></xml>',
-		errorEsperado: 'No puedo ir para abajo',
+		errorEsperado: '¡Hay un obstáculo!',
 	});
 
 	actividadTest(nombre, {
 		descripcionAdicional: 'Da error al querer recoger una lata donde no hay',
-		solucion: '<xml xmlns=\"http://www.w3.org/1999/xhtml\"><variables></variables><block type=\"al_empezar_a_ejecutar\" id=\"3etCqS}C/PQNXxLJF9F(\" deletable=\"false\" movable=\"false\" editable=\"false\" x=\"15\" y=\"15\"><statement name=\"program\"><shadow type=\"required_statement\" id=\"`:U)KB4!86=SoJ5B{wE}\"></shadow><block type=\"ExplotarGlobo\" id=\"ttyewTQUyBGZ.sBV7*aV\"></block></statement></block></xml>',
+		solucion: '<xml xmlns="http://www.w3.org/1999/xhtml">\n  <variables></variables>\n  <block type="al_empezar_a_ejecutar" deletable="false" movable="false" editable="false" x="15" y="15">\n    <statement name="program">\n      <shadow type="required_statement"></shadow>\n      <block type="RecogerLata"></block>\n    </statement>\n  </block>\n</xml>',
 		errorEsperado: 'Acá no hay una lata',
 	});
 
